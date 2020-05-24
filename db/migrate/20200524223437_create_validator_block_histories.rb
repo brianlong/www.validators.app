43ef0dd9d00dd166864ba6dfd64550e61088cfd8
Brian Long <brian.long@fmadata.com>
2020-05-24 17:12:44 -0600

    Create ValidatorBlockHistory

---
+++ db/migrate/20200524223437_create_validator_block_histories.rb
@@ -0,0 +1,18 @@
+# frozen_string_literal: true
+
+# CreateValidatorBlockHistories
+class CreateValidatorBlockHistories < ActiveRecord::Migration[6.0]
+  def change
+    create_table :validator_block_histories do |t|
+      t.references :validator, null: false, foreign_key: true
+      t.integer :epoch
+      t.integer :leader_slots
+      t.integer :blocks_produced
+      t.integer :skipped_slots
+      t.decimal :skipped_slot_percent, precision: 10, scale: 4
+      t.timestamps
+    end
+    add_index :validator_block_histories, %i[validator_id epoch]
+    add_index :validator_block_histories, %i[validator_id created_at]
+  end
+end
