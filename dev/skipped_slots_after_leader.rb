0ffc78816e92ebc407213b1dc3fdbd8b8a75275e
Brian Long <brian.long@firstmoversadvantage.com>
2020-06-03 18:01:10 -0600

    Adds a dev script

---
+++ dev/skipped_slots_after_leader.rb
@@ -0,0 +1,96 @@
+# frozen_string_literal: true
+
+require File.expand_path('../config/environment', __dir__)
+batch_id = SecureRandom.uuid
+
+# block_history_json = `solana block-production \
+#                       --output json-compact \
+#                       --url 'http://testnet.solana.com:8899'`
+block_history_json = File.read('block_production.json')
+block_history = JSON.parse(block_history_json)
+
+epoch = block_history['epoch']
+total_slots = block_history['total_slots']
+total_slots_skipped = block_history['total_slots_skipped']
+total_slots_skipped_percent = (total_slots_skipped / total_slots.to_f) * 100.0
+
+leaders_block = block_history['leaders']
+validator_block_history = {}
+leaders_block.each do |v|
+  validator_block_history[v['identityPubkey']] = {
+    'batch_id' => batch_id,
+    'epoch' => epoch,
+    'leader_slots' => v['leaderSlots'],
+    'blocks_produced' => v['blocksProduced'],
+    'skipped_slots' => v['skippedSlots'],
+    'skipped_slot_percent' => v['skippedSlots'] / v['leaderSlots'].to_f,
+    'skipped_slots_after_me' => 0,
+    'skipped_slots_after_me_percent' => 0.0
+  }
+end
+
+# puts block_history['individual_slot_status'][0..4].inspect
+# Sample block history individual_slot_status
+# [{"leader"=>"8NndwQsrH4f6xF6DW1tt7ESMEJpKz346AGqURKMXcNhT", "skipped"=>false, "slot"=>17372256}, {"leader"=>"8NndwQsrH4f6xF6DW1tt7ESMEJpKz346AGqURKMXcNhT", "skipped"=>false, "slot"=>17372257}, {"leader"=>"8NndwQsrH4f6xF6DW1tt7ESMEJpKz346AGqURKMXcNhT", "skipped"=>false, "slot"=>17372258}, {"leader"=>"8NndwQsrH4f6xF6DW1tt7ESMEJpKz346AGqURKMXcNhT", "skipped"=>false, "slot"=>17372259}, {"leader"=>"3FhfNWGiqDsy4xAXiS74WUb5GLfK7FVnn6kxt3CYLgvr", "skipped"=>false, "slot"=>17372260}]
+
+# Look through the slots to accumulate stats
+prior_leader = nil
+current_leader = nil
+i = 0
+block_history['individual_slot_status'].each do |h|
+  this_leader = h['leader']
+  # puts "#{h['slot']} => #{this_leader}"
+  if this_leader == current_leader
+    # We have the same leader
+  else
+    # We have a new leader
+    prior_leader = current_leader
+    current_leader = this_leader
+    # calculate the stats for the prior leader
+    # The prior leaders slots ended at i-1
+    # if /26AT/.match prior_leader
+    #   puts "  PRIOR LEADER: #{prior_leader}"
+    #   puts "  NEW LEADER: #{current_leader}"
+    #   puts "  #{block_history['individual_slot_status'][i + 1..i + 4].map { |s| s['skipped'] }}"
+    # end
+    skipped_slots_after_leader = \
+      block_history['individual_slot_status'][i + 1..i + 4].count do |s|
+        s['skipped'] == true
+      end
+    # puts "  #{skipped_slots_after_leader}"
+
+    next if prior_leader.nil?
+
+    validator_block_history[prior_leader]['skipped_slots_after_me'] += \
+      skipped_slots_after_leader
+  end
+
+  i += 1
+end
+
+# Put some data to the console for logging
+puts "EPOCH: #{epoch}"
+puts "TOTAL SLOTS: #{total_slots}"
+puts "TOTAL SKIPPED: #{total_slots_skipped}"
+puts "TOTAL SKIPPED %: #{total_slots_skipped_percent}"
+puts ''
+puts [
+  'Public ID'.ljust(11),
+  'Slots'.ljust(9),
+  'Skipped'.ljust(9),
+  'Skipped %'.ljust(9),
+  'Skipped After'.ljust(13),
+  'Skipped After %'.ljust(15)
+].join(' | ')
+puts ['-' * 11, '-' * 9, '-' * 9, '-' * 9, '-' * 13, '-' * 15].join('-|-')
+
+validator_block_history.each do |k, v|
+  puts [
+    "#{k[0..3]}...#{k[-4..-1]}".ljust(11),
+    v['leader_slots'].to_s.rjust(9),
+    v['skipped_slots'].to_s.rjust(9),
+    format('%.2f', v['skipped_slot_percent'] * 100.0).rjust(9),
+    v['skipped_slots_after_me'].to_s.rjust(13),
+    format('%.2f', (v['skipped_slots_after_me'] / v['leader_slots'].to_f) * 100.0).rjust(15)
+  ].join(' | ')
+end
