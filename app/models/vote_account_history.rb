00ef514fc70a2ae7c14f48915198115d26359ca4
Brian Long <brian.long@fmadata.com>
2020-06-28 19:23:35 -0600

    Adds comments

--- app/models/vote_account_history.rb
+++ app/models/vote_account_history.rb
@@ -1,5 +1,7 @@
 # frozen_string_literal: true
 
+# Holds the history of getVoteAccounts RPC requests. See
+# https://docs.solana.com/apps/jsonrpc-api#getvoteaccounts
 class VoteAccountHistory < ApplicationRecord
   belongs_to :vote_account
 end
