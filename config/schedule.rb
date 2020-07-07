9f38fee290a550d114b23cc412ab70d80e19b014
Brian Long <brian.long@firstmoversadvantage.com>
2020-07-07 07:31:17 -0600

    Update testnet URL & schedule.rb

--- config/schedule.rb
+++ config/schedule.rb
@@ -37,6 +37,6 @@ set :output, File.join(Whenever.path, 'log', 'whenever.log')
 job_type :ruby_script, 'cd :path && RAILS_ENV=:environment :bundle_bin exec :ruby_bin script/:task >> :whenever_path/log/:task.log 2>&1'
 
 every 1.minutes do
-  # ruby_script 'gather_rpc_data.rb'
+  ruby_script 'gather_rpc_data.rb'
   ruby_script 'gather_rpc_data_mainnet.rb'
 end
