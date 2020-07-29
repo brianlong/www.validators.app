1b18f2db53c0955ff898c03e823af75d1eacd294
Brian Long <brian.long@firstmoversadvantage.com>
2020-07-29 16:48:11 -0600

    Change indexes

---
+++ db/migrate/20200729192118_update_network_indexes.rb
@@ -0,0 +1,8 @@
+# frozen_string_literal: true
+
+class UpdateNetworkIndexes < ActiveRecord::Migration[6.0]
+  def change
+    add_index :batches, %i[network created_at]
+    remove_index :batches, :created_at
+  end
+end
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2020_07_29_033136) do
+ActiveRecord::Schema.define(version: 2020_07_29_192118) do
 
   create_table "active_storage_attachments", options: "ENGINE=InnoDB DEFAULT CHARSET=utf8", force: :cascade do |t|
     t.string "name", null: false
@@ -38,7 +38,7 @@ ActiveRecord::Schema.define(version: 2020_07_29_033136) do
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
     t.string "network"
-    t.index ["created_at"], name: "index_batches_on_created_at"
+    t.index ["network", "created_at"], name: "index_batches_on_network_and_created_at"
     t.index ["network", "uuid"], name: "index_batches_on_network_and_uuid"
   end
 
