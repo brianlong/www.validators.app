f7c73fee3e17cacda32994c0a3fea234f36059d0
Brian Long <brian.long@fmadata.com>
2020-07-28 21:56:21 -0600

    Formatting & remove un-needed comments

--- script/validators_get_info.rb
+++ script/validators_get_info.rb
@@ -6,28 +6,14 @@ require 'solana_logic'
 
 include SolanaLogic
 
-# {
-#   "identityPubkey": "71bhKKL89U3dNHzuZVZ7KarqV6XtHEgjXjvJTsguD11B",
-#   "infoPubkey": "FRZR9KVav9brvchLUjBZm6Y4Ur9Nv4T1ckGY4fqk7y2z",
-#   "info": {
-#     "keybaseUsername": "brianlong",
-#     "name": "BL",
-#     "website": "https://www.fmadata.com"
-#   }
-# }
-# mainnet
-
 %w[testnet mainnet].each do |network|
-  puts network
   payload = {
     config_urls: Rails.application.credentials.solana["#{network}_urls".to_sym],
     network: network
   }
 
-  p = Pipeline.new(200, payload)
-              .then(&validator_info_get_and_save)
-              .then(&log_errors)
-              .then(&batch_touch)
-
-  # puts p.inspect
+  _p = Pipeline.new(200, payload)
+               .then(&validator_info_get_and_save)
+               .then(&log_errors)
+               .then(&batch_touch)
 end
