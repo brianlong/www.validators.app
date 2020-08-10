19cb911b3e18e4e117022026d699b92dc511c085
Brian Long <brian.long@firstmoversadvantage.com>
2020-08-10 16:56:38 -0600

    WIP

--- app/models/validator_score_v1.rb
+++ app/models/validator_score_v1.rb
@@ -1,6 +1,21 @@
 # frozen_string_literal: true
 
-# This is the model for version 1 of our ValidatorScore.
+# This is the model for version 1 of our ValidatorScore. This model will
+# maintain scores for each validator and also maintain a recent history of
+# events that can be used for charting or quick analysis. Factors that go into
+# building the score are (representative values are shown. Subject to change):
+#
+# - root_distance: [0, 1, 2] based on cluster median & average
+# - vote_distance: [0, 1, 2] based on cluster median & average
+# - skipped_slot_percent: [0, 1, 2] based on cluster median & average
+# - skipped_after_percent: [0, 1, 2] based on cluster median & average
+# - software_version: 2 = current patch, 1 = current minor, 0 = major or N/A
+#
+# Factors that will be deducted from score above:
+# - High percent of total stake. We want to encourage decentralization.
+#   Delegated stake > 3% = -2
+# - Located in high-concentration data center. Located with 3% stake of other
+#   stakeholders = -1, 6% = -2
 class ValidatorScoreV1 < ApplicationRecord
   belongs_to :validator
 end
--- db/migrate/20200810200009_create_validator_score_v1s.rb
+++ db/migrate/20200810200009_create_validator_score_v1s.rb
@@ -4,7 +4,20 @@ class CreateValidatorScoreV1s < ActiveRecord::Migration[6.0]
   def change
     create_table :validator_score_v1s do |t|
       t.references :validator
-
+      t.text :root_distance_history
+      t.integer :root_distance_score
+      t.text :vote_distance_history
+      t.integer :vote_distance_score
+      t.text :skipped_slot_history
+      t.integer :skipped_slot_score
+      t.text :skipped_after_history
+      t.integer :skipped_after_score
+      t.string :software_version
+      t.integer :software_version_score
+      t.bigint :active_stake
+      t.integer :active_stake_score
+      t.integer :data_center_concentration
+      t.integer :data_center_score
       t.timestamps
     end
   end
