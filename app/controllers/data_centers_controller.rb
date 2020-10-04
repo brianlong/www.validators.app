7b21801472d657be2607f9914683ee4efa79f7e6
Brian Long <brian.long@fmadata.com>
2020-10-03 20:01:14 -0600

    Limit results to just the active network

--- app/controllers/data_centers_controller.rb
+++ app/controllers/data_centers_controller.rb
@@ -7,6 +7,12 @@ class DataCentersController < ApplicationController
              IF(ISNULL(city_name), location_time_zone, city_name) as location,
              count(*) as count
       FROM ips
+      WHERE ips.address IN (
+        SELECT vip.address
+        FROM validator_ips vip
+        INNER JOIN validators val ON val.id = vip.validator_id
+        WHERE val.network = '#{params[:network]}'
+      )
       GROUP BY data_center_key,
                country_iso_code,
                location,
