e996314537b88e6493eaa357fa12fa196dacbd2a
Brian Long <brian.long@fmadata.com>
2020-10-04 20:46:53 -0600

    Show data center occupants

--- app/controllers/data_centers_controller.rb
+++ app/controllers/data_centers_controller.rb
@@ -1,4 +1,5 @@
 class DataCentersController < ApplicationController
+  # params[:network]
   def index
     sql = "
       SELECT data_center_key,
@@ -20,5 +21,22 @@ class DataCentersController < ApplicationController
       ORDER BY count desc
     "
     @data_centers = Ip.connection.execute(sql)
+  end
+
+  # params[:network]
+  # params[:key]
+  def data_center
+    sql = "
+      SELECT *
+      FROM validators val
+      INNER JOIN validator_ips vip ON val.id = vip.validator_id
+      WHERE val.network = '#{params[:network]}'
+      AND vip.address IN (
+        SELECT address from ips
+        WHERE data_center_key = '#{params[:key]}'
+      )
+      ORDER BY val.updated_at desc
+    "
+    @validators = Validator.connection.execute(sql)
   end
 end
