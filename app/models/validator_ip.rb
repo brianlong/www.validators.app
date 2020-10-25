158abe39c583102c215c13806eecddb69ccbd192
Brian Long <brian.long@fmadata.com>
2020-10-25 10:04:40 -0600

    Copy the address from validator_ip to the score record.

--- app/models/validator_ip.rb
+++ app/models/validator_ip.rb
@@ -6,9 +6,9 @@ class ValidatorIp < ApplicationRecord
   after_save :copy_data_to_score
 
   def copy_data_to_score
-    if validator.validator_score_v1
-      validator.validator_score_v1.ip_address = ip_address
-      validator.validator_score_v1.save
-    end
+    return if validator.validator_score_v1
+
+    validator.validator_score_v1.ip_address = address
+    validator.validator_score_v1.save
   end
 end
