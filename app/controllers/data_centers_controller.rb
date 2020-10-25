08edcb01a763250d51a5843c7d630a7239479211
Brian Long <brian.long@fmadata.com>
2020-10-25 10:47:44 -0600

    Suppress validators with 0 stake

--- app/controllers/data_centers_controller.rb
+++ app/controllers/data_centers_controller.rb
@@ -16,14 +16,15 @@ class DataCentersController < ApplicationController
     "
     @dc_sql = Ip.connection.execute(sql)
     @scores = ValidatorScoreV1.where(network: params[:network])
+                              .where('active_stake > 0')
     @data_centers = {}
     @dc_sql.each do |dc|
       population = @scores.where(data_center_key: dc[0]).count || 0
-      Rails.logger.info "#{dc.inspect} => #{population}"
-      Rails.logger.info dc[0]
-      Rails.logger.info "@data_centers is a #{@data_centers.class}"
-      Rails.logger.info @data_centers[dc[0]]
-      Rails.logger.info (@data_centers[dc[0]]).to_s
+      # Rails.logger.info "#{dc.inspect} => #{population}"
+      # Rails.logger.info dc[0]
+      # Rails.logger.info "@data_centers is a #{@data_centers.class}"
+      # Rails.logger.info @data_centers[dc[0]]
+      # Rails.logger.info (@data_centers[dc[0]]).to_s
       # byebug
       @data_centers[dc[0]] = {
         aso: dc[1],
@@ -31,30 +32,31 @@ class DataCentersController < ApplicationController
         location: dc[3],
         count: population
       }
-      Rails.logger.info @data_centers.inspect
+      # Rails.logger.info @data_centers.inspect
     end
     @data_centers = @data_centers.sort_by { |_k, v| -v[:count] }
-    Rails.logger.info @data_centers
+    # Rails.logger.info @data_centers
   end
 
   # params[:network]
   # params[:key]
   def data_center
     key = params[:key].gsub('-slash-', '/')
-    sql = "
-      SELECT *
-      FROM validators val
-      INNER JOIN validator_score_v1s score ON val.id = score.validator_id
-      WHERE score.network = '#{params[:network]}'
-      AND score.active_stake > 0
-      AND score.ip_address IN (
-        SELECT ip_address from ips
-        WHERE data_center_key = '#{key}'
-      )
-      ORDER BY val.account
-    "
-    @validators = Validator.connection.execute(sql)
+    # sql = "
+    #   SELECT *
+    #   FROM validators val
+    #   INNER JOIN validator_score_v1s score ON val.id = score.validator_id
+    #   WHERE score.network = '#{params[:network]}'
+    #   AND score.active_stake > 0
+    #   AND score.ip_address IN (
+    #     SELECT ip_address from ips
+    #     WHERE data_center_key = '#{key}'
+    #   )
+    #   ORDER BY val.account
+    # "
+    # @validators = Validator.connection.execute(sql)
     @scores = ValidatorScoreV1.where(network: params[:network])
                               .where(data_center_key: key)
+                              .where('active_stake > 0')
   end
 end
