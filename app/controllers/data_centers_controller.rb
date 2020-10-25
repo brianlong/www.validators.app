8dc6d45ceae42c713517b264ec916222ac72d995
Brian Long <brian.long@fmadata.com>
2020-10-24 18:42:44 -0600

    WIP to de-deduplicate data center rosters

--- app/controllers/data_centers_controller.rb
+++ app/controllers/data_centers_controller.rb
@@ -2,11 +2,10 @@ class DataCentersController < ApplicationController
   # params[:network]
   def index
     sql = "
-      SELECT data_center_key,
+      SELECT distinct data_center_key,
              traits_autonomous_system_organization,
              country_iso_code,
-             IF(ISNULL(city_name), location_time_zone, city_name) as location,
-             count(*) as count
+             IF(ISNULL(city_name), location_time_zone, city_name) as location
       FROM ips
       WHERE ips.address IN (
         SELECT vip.address
@@ -14,14 +13,28 @@ class DataCentersController < ApplicationController
         INNER JOIN validators val ON val.id = vip.validator_id
         WHERE val.network = '#{params[:network]}'
       )
-      GROUP BY data_center_key,
-               country_iso_code,
-               location,
-               traits_autonomous_system_organization
-      ORDER BY count desc
     "
-    @data_centers = Ip.connection.execute(sql)
+    @dc_sql = Ip.connection.execute(sql)
     @scores = ValidatorScoreV1.where(network: params[:network])
+    @data_centers = {}
+    @dc_sql.each do |dc|
+      population = @scores.where(data_center_key: dc[0]).count || 0
+      Rails.logger.info "#{dc.inspect} => #{population}"
+      Rails.logger.info dc[0]
+      Rails.logger.info "@data_centers is a #{@data_centers.class}"
+      Rails.logger.info @data_centers[dc[0]]
+      Rails.logger.info (@data_centers[dc[0]]).to_s
+      # byebug
+      @data_centers[dc[0]] = {
+        aso: dc[1],
+        country: dc[2],
+        location: dc[3],
+        count: population
+      }
+      Rails.logger.info @data_centers.inspect
+    end
+    @data_centers = @data_centers.sort_by { |_k, v| -v[:count] }
+    Rails.logger.info @data_centers
   end
 
   # params[:network]
