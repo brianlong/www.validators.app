257afa1d5a6be2265474f1cbe15188e2ae4521e2
Brian Long <brian.long@fmadata.com>
2021-01-05 16:55:04 -0700

    Adds a temporary message

--- app/controllers/validators_controller.rb
+++ app/controllers/validators_controller.rb
@@ -62,6 +62,9 @@ class ValidatorsController < ApplicationController
     ping_batch = PingTime.where(network: params[:network])&.last&.batch_uuid
     ping_time_stat = PingTimeStat.where(batch_uuid: ping_batch)&.last
     @ping_time_avg = ping_time_stat&.overall_average_time
+
+    flash[:error] = 'Due to a problem with our RPC server pool, the Skipped Slot % data is inaccurate. I am aware of the problem and working on a better solution. Thanks, Brian Long'
+
   end
 
   # GET /validators/1
@@ -92,6 +95,9 @@ class ValidatorsController < ApplicationController
             ) * 100.0
       }
     end
+    
+    flash[:error] = 'Due to a problem with our RPC server pool, the Skipped Slot % data is inaccurate. I am aware of the problem and working on a better solution. Thanks, Brian Long'
+
   end
 
   private
