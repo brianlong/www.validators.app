c6df2708ae62a48e805c671d26c33da4d0d9dec9
Brian Long <brian.long@fmadata.com>
2021-02-02 09:51:58 -0700

    Extend RPC_TIMEOUT

--- script/fix_ip_hetzner.rb
+++ script/fix_ip_hetzner.rb
@@ -113,11 +113,11 @@ puts 'End of Script'
 # SQL to use when manually creating IP ip_overrides
 #
 # Helsinki
-# insert into ip_overrides (address, traits_autonomous_system_number, country_iso_code, country_name, city_name, data_center_key, data_center_host, created_at, updated_at) VALUES ('135.181.181.119 ', 24940, 'FI', 'Finland', 'Helsinki', '24940-FI-Helsinki', 'core32.hel1.hetzner.com', NOW(), NOW());
+# insert into ip_overrides (address, traits_autonomous_system_number, country_iso_code, country_name, city_name, data_center_key, data_center_host, created_at, updated_at) VALUES ('135.181.141.90', 24940, 'FI', 'Finland', 'Helsinki', '24940-FI-Helsinki', 'ex9k1.dc3.hel1.hetzner.com', NOW(), NOW());
 #
 # Falkenstein
 # insert into ip_overrides (address, traits_autonomous_system_number, country_iso_code, country_name, city_name, data_center_key, data_center_host, created_at, updated_at) VALUES ('168.119.213.165 ', 24940, 'DE', 'Germany', 'Falkenstein', '24940-DE-Falkenstein', 'core24.fsn1.hetzner.com', NOW(), NOW());
 #
 # UPDATE ips ip INNER JOIN ip_overrides ipor ON ip.address = ipor.address SET ip.traits_autonomous_system_number = ipor.traits_autonomous_system_number, ip.country_iso_code = ipor.country_iso_code, ip.country_name = ipor.country_name, ip.city_name = ipor.city_name, ip.data_center_key = ipor.data_center_key, ip.data_center_host = ipor.data_center_host, ip.traits_autonomous_system_organization = 'Hetzner Online GmbH', ip.updated_at = NOW();
 #
-# UPDATE validator_score_v1s sc INNER JOIN ips ip ON sc.ip_address = ip.address SET sc.data_center_key = ip.data_center_key, sc.updated_at = NOW();
+# UPDATE validator_score_v1s sc INNER JOIN ips ip ON sc.ip_address = ip.address SET sc.data_center_key = ip.data_center_key, sc.data_center_host = ip.data_center_host, sc.updated_at = NOW();
