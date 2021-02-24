3887ccf0c7169a481b52445a1090891de4142423
Tyler VanNurden <tyler.vannurden@colorado.edu>
2021-02-23 22:35:17 -0700

    Allow testnet version number comparison

--- app/services/validator_software_version.rb
+++ app/services/validator_software_version.rb
@@ -1,10 +1,15 @@
 class ValidatorSoftwareVersion < ::Gem::Version
+  def initialize(number, network = 'mainnet')
+    @current_version_number = network == 'mainnet' ? MAINNET_CLUSTER_VERSION : TESTNET_CLUSTER_VERSION
+    super(number)
+  end
+
   def running_latest_or_edge?
-    self >= Gem::Version.new(CURRENT_VERSION)
+    self >= Gem::Version.new(@current_version_number)
   end
 
   def running_latest_minor?
-    current_major_minor_version = CURRENT_VERSION.split('.').first(2).join('.')
+    current_major_minor_version = @current_version_number.split('.').first(2).join('.')
     self >= Gem::Version.new(current_major_minor_version)
   end
 end
