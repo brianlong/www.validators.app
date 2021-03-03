fe14277e21ba7ee1af6784580434ab9820f38a19
Brian Long <brian.long@fmadata.com>
2021-03-02 17:30:25 -0700

    Add & Update ad-hoc scripts to manage data center IPs

--- script/fix_misc_ip.rb
+++ script/fix_misc_ip.rb
@@ -1,5 +1,9 @@
-# Marbis Moscow
-# insert into ip_overrides (address, traits_autonomous_system_number, country_iso_code, country_name, city_name, data_center_key, data_center_host, created_at, updated_at) VALUES ('5.83.175.225', 199610, 'RU', 'Russia', 'Moscow', '199610-RU-Moscow', 'FRNKT-NYC-PE-MX960-1.megafon.ru', NOW(), NOW());
+# Psychz Networks
+# insert into ip_overrides (address, traits_autonomous_system_number, country_iso_code, country_name, city_name, data_center_key, data_center_host, created_at, updated_at) VALUES ('45.34.1.46', 40676, 'US', 'United States', 'Los Angeles', '40676-US-Los Angeles', '', NOW(), NOW());
+
+# Update ips set traits_autonomous_system_organization = 'Psychz Networks', location_time_zone = 'America/Los Angeles' where address = '45.34.1.46';
+
+
 #
 # UPDATE ips ip INNER JOIN ip_overrides ipor ON ip.address = ipor.address SET ip.traits_autonomous_system_number = ipor.traits_autonomous_system_number, ip.country_iso_code = ipor.country_iso_code, ip.country_name = ipor.country_name, ip.city_name = ipor.city_name, ip.data_center_key = ipor.data_center_key, ip.data_center_host = ipor.data_center_host, ip.traits_autonomous_system_organization = 'Hetzner Online GmbH', ip.updated_at = NOW();
 #
