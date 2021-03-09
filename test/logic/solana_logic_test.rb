994c876cafff53f5fa6fce34e375cad10cfc4f99
Tyler VanNurden <tyler.vannurden@colorado.edu>
2021-03-08 20:31:47 -0700

    VCR: Ignore requests to localhost

--- test/logic/solana_logic_test.rb
+++ test/logic/solana_logic_test.rb
@@ -82,31 +82,36 @@ class SolanaLogicTest < ActiveSupport::TestCase
   end
 
   test 'epoch_get with fail_over' do
-    VCR.use_cassette('epoch_get_with_fail_over') do
-      fail_over_payload = {
-        # This assumes that there is no RPC server running on the localhost!
-        config_urls: [
-          'http://127.0.0.1:8899',
-          'http://testnet.solana.com:8899'
-        ],
-        network: 'testnet'
-      }
+    # We need to stub both the cli call (using minitest stub)
+    # plus the POST http://165.227.100.142:8899/ call (using VCR)
+    json_data = File.read("#{Rails.root}/test/json/validators.json")
+    SolanaCliService.stub(:request, json_data, ['validators', 'http://165.227.100.142:8899']) do
+      VCR.use_cassette('epoch_get_with_fail_over') do
+        fail_over_payload = {
+          # This assumes that there is no RPC server running on the localhost!
+          config_urls: [
+            'http://127.0.0.1:8899',
+            'http://testnet.solana.com:8899'
+          ],
+          network: 'testnet'
+        }
 
-      p = Pipeline.new(200, fail_over_payload)
-                  .then(&batch_set)
-                  .then(&epoch_get)
+        p = Pipeline.new(200, fail_over_payload)
+                    .then(&batch_set)
+                    .then(&epoch_get)
 
-      assert_equal 200, p.code
-      assert_not_nil p.payload[:epoch]
-      assert_not_nil p.payload[:batch_uuid]
+        assert_equal 200, p.code
+        assert_not_nil p.payload[:epoch]
+        assert_not_nil p.payload[:batch_uuid]
 
-      # Find the EpochHistory record and show that the values match
-      epoch = EpochHistory.where(batch_uuid: p.payload[:batch_uuid]).first
-      assert_equal p.payload[:epoch], epoch.epoch
-      assert_equal p.payload[:batch_uuid], epoch.batch_uuid
-      assert_not_nil epoch.current_slot
-      assert_not_nil epoch.slot_index
-      assert_not_nil epoch.slots_in_epoch
+        # Find the EpochHistory record and show that the values match
+        epoch = EpochHistory.where(batch_uuid: p.payload[:batch_uuid]).first
+        assert_equal p.payload[:epoch], epoch.epoch
+        assert_equal p.payload[:batch_uuid], epoch.batch_uuid
+        assert_not_nil epoch.current_slot
+        assert_not_nil epoch.slot_index
+        assert_not_nil epoch.slots_in_epoch
+      end
     end
   end
 
@@ -131,7 +136,8 @@ class SolanaLogicTest < ActiveSupport::TestCase
     ValidatorHistory.delete_all
     assert_equal 0, ValidatorHistory.count
 
-    # We need ot stub both the cli call in plus the POST http://165.227.100.142:8899/ call
+    # We need to stub both the cli call (using minitest stub)
+    # plus the POST http://165.227.100.142:8899/ call (using VCR)
     json_data = File.read("#{Rails.root}/test/json/validators.json")
     SolanaCliService.stub(:request, json_data, ['validators', 'http://165.227.100.142:8899']) do
       VCR.use_cassette('validators_cli') do
