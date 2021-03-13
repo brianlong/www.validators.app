d756de481e16e66cad8e3750c3dd90cfd1e86f4f
Tyler VanNurden <tyler.vannurden@colorado.edu>
2021-03-12 17:56:32 -0700

    Rename skipped slot column name

--- db/migrate/20210311150854_add_last_24_hours_moving_average_to_validator_block_histories.rb
+++
@@ -1,5 +0,0 @@
-class AddLast24HoursMovingAverageToValidatorBlockHistories < ActiveRecord::Migration[6.0]
-  def change
-    add_column :validator_block_histories, :last_24_hours_skipped_slot_percent_moving_average, :decimal, precision: 10, scale: 4
-  end
-end
