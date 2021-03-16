150ae98513c2b5670c88166faf2354297ab8e773
Klaudia Biała <klaudia.biala@polcode.net>
2021-03-16 12:52:29 +0100

    Move logic out of the views

--- app/helpers/validators_helper.rb
+++ app/helpers/validators_helper.rb
@@ -1,2 +1,27 @@
 module ValidatorsHelper
+  def set_chart_line_color(score)
+    if score == 2
+      GREEN
+    elsif score == 1
+      BLUE
+    else
+      LIGHT_GREY
+    end
+  end
+
+  def set_score_class(score)
+    if score == -2
+      'text-danger'
+    else
+      'text-warning'
+    end
+  end
+
+  def set_chart_y_scale(count)
+    if count < 60
+      count
+    else
+      60
+    end
+  end
 end
