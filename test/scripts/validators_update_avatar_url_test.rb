eb6dc90819d5102d48b476d33d54de82c7062493
maciejkocylapc <maciej.kocyla@polcode.net>
2021-03-22 12:09:24 +0100

    removed other validators from fixtures

--- test/scripts/validators_update_avatar_url_test.rb
+++ test/scripts/validators_update_avatar_url_test.rb
@@ -2,8 +2,26 @@ require 'test_helper'
 
 class ValidatorUpdateAvatarUrlTest < ActiveSupport::TestCase
 
+  def setup
+    Validator.create([{
+      keybase_id: 'stakingfund',
+      network: 'testnet',
+      account: '2345',
+      avatar_url: "https://s3.amazonaws.com/keybase_processed_uploads/d48739023a250815c4ac564c9870ec05_360_360.jpg"
+    },{
+      keybase_id: 'nikitainbar',
+      network: 'testnet',
+      account: '3456',
+      avatar_url: "https://s3.amazonaws.com/keybase_processed_uploads/3efdd9c766508d123c15677e075e5d05_360_360.jpg"
+    },{
+      keybase_id: 'zhanglianghui',
+      network: 'testnet',
+      account: '4567',
+      avatar_url: "https://s3.amazonaws.com/keybase_processed_uploads/5a7a92ba3326454e654e76eaeb9c3d05_360_360.jpg"
+    }])
+  end
+
   test "validators with correct urls" do
-    validators(:staking_fund, :medusa, :hashquark)
     urls = Validator.all.pluck(:avatar_url)
     VCR.use_cassette('validators_update_avatar_url/validators_with_correct_urls', record: :new_episodes) do
       load(Rails.root.join('script', 'validators_update_avatar_url.rb') )
@@ -16,7 +34,7 @@ class ValidatorUpdateAvatarUrlTest < ActiveSupport::TestCase
     broken_url = 'https://keybase.io/images/no-photo/placeholder-avatar-180-x-181@2x.png'
     Validator.all.each{ |v| v.update(avatar_url: broken_url) }
     VCR.use_cassette('validators_update_avatar_url/validators_with_broken_urls', record: :new_episodes) do
-      assert_changes 'Validator.find(2).avatar_url == broken_url' do
+      assert_changes 'Validator.last.avatar_url == broken_url' do
         load(Rails.root.join('script', 'validators_update_avatar_url.rb') )
       end
     end
@@ -26,7 +44,7 @@ class ValidatorUpdateAvatarUrlTest < ActiveSupport::TestCase
     default_url = 'https://keybase.io/images/no-photo/placeholder-avatar-180-x-180@2x.png'
     Validator.all.each{ |v| v.update(avatar_url: default_url) }
     VCR.use_cassette('validators_update_avatar_url/validators_with_default_urls', record: :new_episodes) do
-      assert_changes 'Validator.find(2).avatar_url == default_url' do
+      assert_changes 'Validator.last.avatar_url == default_url' do
         load(Rails.root.join('script', 'validators_update_avatar_url.rb') )
       end
     end
