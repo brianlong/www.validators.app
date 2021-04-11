55989e6c0c069807bbbf92b89cb02a9a13c8def2
Brian Long <brian.long@fmadata.com>
2021-04-11 13:09:48 -0600

    Adds controller & views for Log Deep Dive articles

---
+++ test/controllers/log_deep_dives_controller_test.rb
@@ -0,0 +1,13 @@
+require "test_helper"
+
+class LogDeepDivesControllerTest < ActionDispatch::IntegrationTest
+  test "should get index" do
+    get log_deep_dives_index_url
+    assert_response :success
+  end
+
+  test "should get slot_72677728" do
+    get log_deep_dives_slot_72677728_url
+    assert_response :success
+  end
+end
