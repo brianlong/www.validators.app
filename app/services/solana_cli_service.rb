78933b3e81cb49d5531280c6695763ce85d5547f
Brian Long <brian.long@fmadata.com>
2021-04-17 16:09:00 -0600

    Adds a dev script for account transactions + some supporting changes in other modules

--- app/services/solana_cli_service.rb
+++ app/services/solana_cli_service.rb
@@ -10,4 +10,8 @@ class SolanaCliService
   def self.request(cli_method, rpc_url)
     `#{solana_path}solana #{cli_method} --output json-compact --url #{rpc_url}`
   end
+
+  def self.request_with_params(cli_method, rpc_url, params)
+    `#{solana_path}solana #{cli_method} --output json-compact --url #{rpc_url} #{params}`    
+  end
 end
--- dev/show_acount_history.rb
+++
@@ -1,5 +0,0 @@
-# frozen_string_literal: true
-
-# Accept an account or token address on the command line and show the history
-
-require File.expand_path('../config/environment', __dir__)
