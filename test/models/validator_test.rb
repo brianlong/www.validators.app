63b84ff5ebbeb09d16cbe4c66558a3fb70997cd2
Tyler VanNurden <tyler.vannurden@colorado.edu>
2021-05-04 19:00:59 -0600

    Add helper method for private validator

--- test/models/validator_test.rb
+++ test/models/validator_test.rb
@@ -10,4 +10,9 @@ class ValidatorTest < ActiveSupport::TestCase
   test 'ping_times_to_avg' do
     assert_equal 75.0, validators(:one).ping_times_to_avg
   end
+
+  test '#private_validator? returns true if validator\'s commission is 100%' do
+    assert create(:validator_score_v1, commission: 100).validator.private_validator?
+    refute create(:validator_score_v1, commission: 99).validator.private_validator?
+  end
 end
