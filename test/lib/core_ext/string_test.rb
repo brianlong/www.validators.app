1c6aba4afbded1a7dca4b1009146e078fea48df2
Radosław Stolarski <radoslaw.stolarski@polcode.net>
2021-05-12 12:38:33 +0200

    Remove comment

--- test/lib/core_ext/string_test.rb
+++ test/lib/core_ext/string_test.rb
@@ -2,7 +2,6 @@
 
 require 'test_helper'
 
-# SolanaLogicTest
 class StringTest < ActiveSupport::TestCase
   test 'encode_utf_8 properly encode string with unusual characters' do
     assert_equal 'Staking-P◎◎l FREE Validati◎n',
