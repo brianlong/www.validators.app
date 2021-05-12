015702e32dd56955cb42f1e6e992332e2778a1f7
Radosław Stolarski <radoslaw.stolarski@polcode.net>
2021-05-12 12:17:10 +0200

    Extend string class with encode_to_utf_8 method + test

---
+++ test/lib/core_ext/string_test.rb
@@ -0,0 +1,11 @@
+# frozen_string_literal: true
+
+require 'test_helper'
+
+# SolanaLogicTest
+class StringTest < ActiveSupport::TestCase
+  test 'encode_utf_8 properly encode string with unusual characters' do
+    assert_equal 'Staking-P◎◎l FREE Validati◎n',
+                 'Staking-P◎◎l FREE Validati◎n'.encode_utf_8
+  end
+end
