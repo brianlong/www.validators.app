00a3fca1dce30fe51551ac1a1e5a4425ddda35d0
maciejkocylapc <maciej.kocyla@polcode.net>
2021-05-18 15:16:20 +0200

    pagination tests + docs

--- test/controllers/api/v1/epochs_controller_test.rb
+++ test/controllers/api/v1/epochs_controller_test.rb
@@ -40,4 +40,30 @@ class EpochsControllerTest < ActionDispatch::IntegrationTest
       created_at
     ].sort, resp[0].keys.sort
   end
+
+  test 'epoch with default pagination returns correct number of records' do
+    100.times do |i|
+      create(:epoch_wall_clock, epoch: i)
+    end
+
+    get api_v1_epoch_index_url(network: 'testnet'), headers: { 'Token' => @user.api_token }
+    resp = response_to_json(@response.body)
+
+    assert_response 200
+    assert_equal 50, resp.count
+  end
+
+  test 'epoch with custom pagination returns correct number of records' do
+    100.times do |i|
+      create(:epoch_wall_clock, epoch: i)
+    end
+
+    get api_v1_epoch_index_url(network: 'testnet', per: 20, page: 2), headers: { 'Token' => @user.api_token }
+    resp = response_to_json(@response.body)
+
+    assert_response 200
+    assert_equal 20, resp.count
+    assert_equal 82, resp.first['epoch']
+  end
+
 end
