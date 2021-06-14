b665f2742667ddae27401326b95d120084d62c0a
maciejkocylapc <maciej.kocyla@polcode.net>
2021-06-14 12:18:51 +0200

    refactor + tests

--- script/add_current_epoch.rb
+++ script/add_current_epoch.rb
@@ -3,75 +3,87 @@
 require 'uri'
 require 'net/http'
 
+@common_params = {
+  "jsonrpc" => "2.0", 
+  "id" => 1
+}
+
 def get_last_epoch(http, uri)
-  params = {
-    "jsonrpc" => "2.0", 
-    "id" => 1, 
+  params = @common_params.merge( {
     "method" => "getEpochInfo"
-  }
+  } )
   resp, _ = http.post(uri, params.to_json, { 'Content-Type' => 'application/json' } )
   JSON.parse(resp.body)['result']
 end
 
 def get_block_time(http, uri, block)
-  params = {
-    "jsonrpc" => "2.0", 
-    "id" => 1, 
+  params = @common_params.merge( {
     "method" => "getBlockTime",
     "params" => [block]
-  }
+  } )
   resp, _ = http.post(uri, params.to_json, {'Content-Type' => 'application/json'})
   JSON.parse(resp.body)['result']
 end
 
 def get_confirmed_block(http, uri, slot)
-  params = {
-    "jsonrpc" => "2.0", 
-    "id" => 1, 
+  params = @common_params.merge( {
     "method" => "getConfirmedBlock", #TODO: This method is deprecated from solana 1.7
     "params" => [slot]
-  }
+  } )
   resp, _ = http.post(uri, params.to_json, {'Content-Type' => 'application/json'})
   JSON.parse(resp.body)['result'] ? slot : nil
 end
 
 %w[mainnet testnet].each do |network|
-  url = network == 'mainnet' ? 'https://api.mainnet-beta.solana.com' : 'https://api.testnet.solana.com'
+  if Rails.env.test?
+    url = 'https://api.testnet.solana.com'
+  else
+    url = if network == 'mainnet'
+      Rails.application.credentials.solana[:mainnet_urls][0]
+    else
+      Rails.application.credentials.solana[:testnet_urls][0]
+    end
+  end
+
   uri = URI(url)
   http = Net::HTTP.new(uri.host, uri.port)
   http.use_ssl = true
 
   slots_in_epoch = 432000
 
+  # number of slots to search for a confirmed block
+  block_search_count = 100
+
   last_epoch = get_last_epoch(http, uri)
   last_epoch_start_slot = last_epoch['absoluteSlot'] - last_epoch['slotIndex']
   last_epoch_start_datetime = DateTime.strptime(get_block_time(http, uri, last_epoch_start_slot).to_s, '%s')
 
-  unless EpochWallClock.where(network: network).find_by(epoch: last_epoch['epoch'])
-    confirmed_start_block = nil
-    100.times do |b_diff|
-      confirmed_start_block = get_confirmed_block(http, uri, last_epoch_start_slot + b_diff)
-      break if confirmed_start_block
-    end
-    break unless confirmed_start_block
+  next if EpochWallClock.where(network: network).find_by(epoch: last_epoch['epoch'])
 
-    EpochWallClock.create(
-      epoch: last_epoch['epoch'], 
-      network: network, 
-      starting_slot: confirmed_start_block, 
-      slots_in_epoch: slots_in_epoch, 
-      created_at: last_epoch_start_datetime,
-      ending_slot: nil
-    )
-
-    confirmed_start_block = nil
-    100.times do |b_diff|
-      confirmed_end_block = get_confirmed_block(http, uri, last_epoch_start_slot - 1 - b_diff)
-      break if confirmed_end_block
-    end
-    break unless confirmed_end_block
-    EpochWallClock.where(network: network)
-                  .find_by(epoch: last_epoch['epoch'].to_i - 1)
-                  .update(ending_slot: confirmed_end_block)
+  confirmed_start_block = nil
+  block_search_count.times do |b_diff|
+    confirmed_start_block = get_confirmed_block(http, uri, last_epoch_start_slot + b_diff)
+    break if confirmed_start_block
   end
+
+  break unless confirmed_start_block
+
+  EpochWallClock.create(
+    epoch: last_epoch['epoch'],
+    network: network,
+    starting_slot: confirmed_start_block,
+    slots_in_epoch: slots_in_epoch,
+    created_at: last_epoch_start_datetime,
+    ending_slot: nil
+  )
+
+  confirmed_end_block = nil
+  block_search_count.times do |b_diff|
+    confirmed_end_block = get_confirmed_block(http, uri, last_epoch_start_slot - 1 - b_diff)
+    break if confirmed_end_block
+  end
+  break unless confirmed_end_block
+  EpochWallClock.where(network: network)
+                .find_by(epoch: last_epoch['epoch'].to_i - 1)
+                &.update(ending_slot: confirmed_end_block)
 end
--- script/back_fill_epoch_data.rb
+++ script/back_fill_epoch_data.rb
@@ -1,38 +1,17 @@
 # frozen_string_literal: true
 
-# %w[mainnet testnet].each do |network|
-#   epochs = EpochHistory.where(network: network)
-#                        .where('created_at >= ?', Date.today.beginning_of_year)
-#                        .pluck(:epoch).uniq
-
-#   epochs.each do |epoch|
-#     first_in_history = EpochHistory.where(epoch: epoch, network: network)
-#                                    .order(created_at: :desc)
-#                                    .last
-
-#     EpochWallClock.find_or_create_by(epoch: epoch, network: network) do |e|
-#       e.network = network
-#       e.starting_slot = first_in_history.current_slot
-#       e.slots_in_epoch = first_in_history.slots_in_epoch
-#       e.ending_slot = first_in_history.current_slot - first_in_history.slots_in_epoch
-#       e.created_at = first_in_history.created_at
-#       puts 'created new EWC'
-#     end
-
-#     puts first_in_history.inspect
-#   end
-# end
-
-
 require 'uri'
 require 'net/http'
 
 EpochWallClock.delete_all
 
+@common_params = {
+  "jsonrpc" => "2.0", 
+  "id" => 1
+}
+
 def get_last_epoch(http, uri)
-  params = {
-    "jsonrpc" => "2.0", 
-    "id" => 1, 
+  params = @common_params.merge {
     "method" => "getEpochInfo"
   }
   resp, _ = http.post(uri, params.to_json, { 'Content-Type' => 'application/json' } )
@@ -40,9 +19,7 @@ def get_last_epoch(http, uri)
 end
 
 def get_block_time(http, uri, block)
-  params = {
-    "jsonrpc" => "2.0", 
-    "id" => 1, 
+  params = @common_params.merge {
     "method" => "getBlockTime",
     "params" => [block]
   }
@@ -51,9 +28,7 @@ def get_block_time(http, uri, block)
 end
 
 def get_confirmed_block(http, uri, slot)
-  params = {
-    "jsonrpc" => "2.0", 
-    "id" => 1, 
+  params = @common_params.merge {
     "method" => "getConfirmedBlock", #TODO: This method is deprecated from solana 1.7
     "params" => [slot]
   }
@@ -62,7 +37,12 @@ def get_confirmed_block(http, uri, slot)
 end
 
 %w[testnet mainnet].each do |network|
-  url = network == 'mainnet' ? 'https://api.mainnet-beta.solana.com' : 'https://api.testnet.solana.com'
+  url = if network == 'mainnet'
+    Rails.application.credentials.solana[:mainnet_urls][0]
+  else
+    Rails.application.credentials.solana[:testnet_urls][0]
+  end
+
   uri = URI(url)
   http = Net::HTTP.new(uri.host, uri.port)
   http.use_ssl = true
@@ -76,13 +56,13 @@ end
   slot_set = last_epoch_start_slot.to_i
   current_epoch = last_epoch['epoch']
 
-  last_epoch['epoch'].times do |i|
-    slot_set = slot_set - slots_in_epoch
-    current_epoch = current_epoch - 1
+  last_epoch['epoch'].times do
+    slot_set -= slots_in_epoch
+    current_epoch -= 1
     confirmed_start_block = nil
-    100.times do |b_diff|
-      puts b_diff
-      confirmed_start_block = get_confirmed_block(http, uri, slot_set + b_diff)
+    100.times do |block_offset|
+      puts block_offset
+      confirmed_start_block = get_confirmed_block(http, uri, slot_set + block_offset)
       break if confirmed_start_block
       sleep(0.1)
     end
@@ -90,15 +70,17 @@ end
     break unless confirmed_start_block
 
     confirmed_end_block = nil
-    100.times do |b_diff|
-      confirmed_end_block = get_confirmed_block(http, uri, slot_set + slots_in_epoch - b_diff)
+    100.times do |block_offset|
+      confirmed_end_block = get_confirmed_block(http, uri, slot_set + slots_in_epoch - block_offset)
+
       break if confirmed_end_block
     end
 
     break unless confirmed_end_block
 
-    current_epoch_start_unix =  get_block_time(http, uri, confirmed_start_block).to_s
+    current_epoch_start_unix = get_block_time(http, uri, confirmed_start_block).to_s
     current_epoch_created_at = DateTime.strptime(current_epoch_start_unix, '%s')
+
     ewc = EpochWallClock.create(
       epoch: current_epoch, 
       network: network, 
