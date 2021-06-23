ca59ccfa8931094f436787721227dc4b3236d5e5
maciejkocylapc <maciej.kocyla@polcode.net>
2021-06-23 13:07:03 +0200

    sidekiq job in schedule

--- config/schedule.rb
+++ config/schedule.rb
@@ -36,6 +36,8 @@ set :whenever_path, Whenever.path
 set :output, File.join(Whenever.path, 'log', 'whenever.log')
 job_type :ruby_script,
          'cd :path && RAILS_ENV=:environment :bundle_bin exec :ruby_bin script/:task >> :whenever_path/log/:task.log 2>&1'
+job_type :sidekiq,
+         "cd :path && :environment_variable=:environment bundle exec sidekiq-client push :task :output"
 
 every 1.hour do
   ruby_script 'validators_get_info.rb'
@@ -51,7 +53,7 @@ every 1.day do
 end
 
 every 10.minutes do
-  ValidatorCheckActiveWorker.perform_async
+  sidekiq 'ValidatorCheckActiveWorker'
 end
 
 every 1.minute do
