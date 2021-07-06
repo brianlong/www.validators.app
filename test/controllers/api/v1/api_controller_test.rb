e9227705447e3b6ac5ca8e9d159144a4256e15d7
maciejkocylapc <80820353+maciejkocylapc@users.noreply.github.com>
2021-07-06 15:46:48 +0200

    178718094-api-access-using-origin (#137)
    * origin for configured pages
    
    * whitelist for stage

--- test/controllers/api/v1/api_controller_test.rb
+++ test/controllers/api/v1/api_controller_test.rb
@@ -1,6 +1,8 @@
 # frozen_string_literal: true
 
 require 'test_helper'
+require 'rack/cors'
+require 'rack/test'
 
 # ApiControllerTest
 class ApiControllerTest < ActionDispatch::IntegrationTest
@@ -16,6 +18,43 @@ class ApiControllerTest < ActionDispatch::IntegrationTest
     @user = User.create(@user_params)
 
     Validator.destroy_all
+  end
+
+  test 'When reaching the api \
+        from origin listed in whitelist \
+        should return 200' do
+    get api_v1_ping_url, headers: {
+      'Origin' => 'http://example.com' # this origin is in whitelist
+    }
+
+    assert_response 200
+    json = response_to_json(@response.body)
+    assert_equal 'pong', json['answer']
+  end
+
+  test 'When reaching the api \
+        from foreign origin \
+        should return Unauthorized' do
+    get api_v1_ping_url, headers: {
+      'Origin' => 'http://foreign.com'
+    }
+
+    assert_response 401
+    json = response_to_json(@response.body)
+    assert_equal 'Unauthorized', json['error']
+  end
+
+  test 'When reaching the api \
+  from foreign origin but with correct token \
+  should return 200' do
+    get api_v1_ping_url, headers: {
+      'Token' => @user.api_token,
+      'Origin' => 'http://foreign.com'
+    }
+
+    assert_response 200
+    json = response_to_json(@response.body)
+    assert_equal 'pong', json['answer']
   end
 
   test 'GET api_v1_ping without token should get error' do
