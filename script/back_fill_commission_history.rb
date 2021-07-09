d99a42e9859995083a72b768829437d58d3a2698
maciejkocylapc <maciej.kocyla@polcode.net>
2021-07-09 14:04:41 +0200

    find or create by

--- script/back_fill_commission_history.rb
+++ script/back_fill_commission_history.rb
@@ -1,14 +1,34 @@
 # frozen_string_literal: true
 
-%[mainnet testnet].each do |network|
-  Validator.scorable.where(network: network).each do |validator|
+%w[mainnet testnet].each do |network|
+  Validator.where(network: network).each do |validator|
     last_history = ValidatorHistory.where(account: validator.account)
                                    .order(created_at: :desc)
                                    .first
     loop do
-      ValidatorHistory.where(account: validator.account)
-                      .order(created_at: :desc)
-                      .first
+      changed_history = ValidatorHistory.where(account: validator.account)
+                                        .where.not(commission: last_history.commission)
+                                        .where('created_at < ?', last_history.created_at)
+                                        .order(created_at: :desc)
+                                        .first
+      break unless changed_history
+
+      epoch_info = EpochHistory.where('created_at < ?', changed_history.created_at)
+                               .where(network: network)
+                               .order(created_at: :desc)
+                               .first
+
+      new_h = validator.commission_histories.find_or_create_by(
+        commission_before: changed_history.commission,
+        commission_after: last_history.commission,
+        batch_uuid: changed_history.batch_uuid,
+        epoch: epoch_info.epoch,
+        epoch_completion: (epoch_info.slots_in_epoch / epoch_info.slot_index.to_f).round(2)
+      )
+
+      last_history = changed_history
+
+      puts new_h.inspect
     end
 
   end
