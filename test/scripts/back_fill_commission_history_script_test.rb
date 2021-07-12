1fdbc6f27632c29b1e65fdce077968e0e7ac2a2e
maciejkocylapc <maciej.kocyla@polcode.net>
2021-07-12 11:00:28 +0200

    178811031-old-commission-changes

---
+++ test/scripts/back_fill_commission_history_script_test.rb
@@ -0,0 +1,30 @@
+# frozen_string_literal: true
+
+require 'test_helper'
+
+class AddCurrentEpochScriptTest < ActiveSupport::TestCase
+  setup do
+    v = create(:validator, network: 'testnet')
+    commission = 10
+    create(:batch, network: 'testnet')
+    create(:epoch_history, network: 'testnet', created_at: 10.minutes.ago)
+
+    10.times do |i|
+      this_commission = i % 3 == 0 ? commission + i : commission
+      ValidatorHistory.create(
+        account: v.account,
+        commission: this_commission,
+        network: v.network,
+        created_at: i.minutes.ago
+      )
+    end
+  end
+
+  test 'commission histories are added correctly' do
+    refute CommissionHistory.exists?
+    load(Rails.root.join('script', 'back_fill_commission_history.rb'))
+    assert_equal 5, CommissionHistory.count
+    assert_equal [10, 13, 10, 16, 10], CommissionHistory.pluck(:commission_after)
+    assert_equal [13, 10, 16, 10, 19], CommissionHistory.pluck(:commission_before)
+  end
+end
