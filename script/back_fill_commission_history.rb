0dc1420a86f9c688230d94e0126d7657a6f8a032
maciejkocylapc <maciej.kocyla@polcode.net>
2021-07-12 14:48:24 +0200

    reverse the script

--- script/back_fill_commission_history.rb
+++ script/back_fill_commission_history.rb
@@ -2,28 +2,28 @@
 
 %w[mainnet testnet].each do |network|
   Validator.where(network: network).each do |validator|
-    last_history = ValidatorHistory.where(account: validator.account)
+    first_history = ValidatorHistory.where(account: validator.account)
                                    .order(created_at: :desc)
-                                   .first
+                                   .last
     next unless last_history
 
     loop do
       changed_history = ValidatorHistory.where(account: validator.account)
                                         .where(network: network)
-                                        .where.not(commission: last_history.commission)
-                                        .where('created_at < ?', last_history.created_at)
+                                        .where.not(commission: first_history.commission)
+                                        .where('created_at > ?', first_history.created_at)
                                         .order(created_at: :desc)
-                                        .first
+                                        .last
       break unless changed_history
 
-      epoch_info = EpochHistory.where('created_at < ?', changed_history.created_at)
+      epoch_info = EpochHistory.where('created_at <= ?', changed_history.created_at)
                                .where(network: network)
                                .order(created_at: :desc)
                                .first
 
       new_h = validator.commission_histories.find_or_create_by(
-        commission_before: changed_history.commission,
-        commission_after: last_history.commission,
+        commission_before: first_history.commission,
+        commission_after: changed_history.commission,
         batch_uuid: changed_history.batch_uuid,
         epoch: epoch_info.epoch,
         network: network,
@@ -31,7 +31,7 @@
         created_at: changed_history.created_at
       )
 
-      last_history = changed_history
+      first_history = changed_history
     end
 
   end
