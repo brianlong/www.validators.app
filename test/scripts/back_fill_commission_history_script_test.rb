4bba78894f4092eb44aed6b43a1c1ec970f79611
maciejkocylapc <80820353+maciejkocylapc@users.noreply.github.com>
2021-07-14 12:14:37 +0200

    178811031 old commission changes (#149)
    * create commisssion history
    
    * validator history tests
    
    * epochs info + callback in score
    
    * minor refactor
    
    * back_fill_commission_history_script WIP
    
    * find or create by
    
    * check if history exists
    
    * fix epoch info
    
    * remove previous from validator_history
    
    * 178811031-old-commission-changes
    
    * add network to where
    
    * add created_at
    
    * reverse the script
    
    * pass tests
    
    * limit the history creation to last deploy time
    
    * create commisssion history
    
    * validator history tests
    
    * epochs info + callback in score
    
    * back_fill_commission_history_script WIP
    
    * find or create by
    
    * check if history exists
    
    * fix epoch info
    
    * remove previous from validator_history
    
    * 178811031-old-commission-changes
    
    * add network to where
    
    * add created_at
    
    * reverse the script
    
    * pass tests
    
    * limit the history creation to last deploy time
    
    * schema information fixed

---
+++ test/scripts/back_fill_commission_history_script_test.rb
@@ -0,0 +1,30 @@
+# frozen_string_literal: true
+
+require 'test_helper'
+
+class AddCurrentEpochScriptTest < ActiveSupport::TestCase
+  setup do
+    v = create(:validator, network: 'testnet')
+    commission = 10
+    create(:batch, network: 'testnet')
+    create(:epoch_history, network: 'testnet', created_at: DateTime.new(2021, 7, 9) - 10.minutes)
+
+    10.times do |i|
+      this_commission = i % 3 == 0 ? commission + i : commission
+      ValidatorHistory.create(
+        account: v.account,
+        commission: this_commission,
+        network: v.network,
+        created_at: DateTime.new(2021, 7, 9) - i.minutes
+      )
+    end
+  end
+
+  test 'commission histories are added correctly' do
+    refute CommissionHistory.exists?
+    load(Rails.root.join('script', 'back_fill_commission_history.rb'))
+    assert_equal 5, CommissionHistory.count
+    assert_equal [10.0, 16.0, 10.0, 13.0, 10.0], CommissionHistory.pluck(:commission_after)
+    assert_equal [19.0, 10.0, 16.0, 10.0, 13.0], CommissionHistory.pluck(:commission_before)
+  end
+end
