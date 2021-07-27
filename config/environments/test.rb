c617aa141119b38ee6be0d2d2e249779b688a716
Radosław Stolarski <radoslaw.stolarski@polcode.net>
2021-07-27 16:03:35 +0200

    178985735_switch_from_using_partial_in_api (#171)
    * Load all needed ips in controller
    
    * Fix indentation
    
    * Add has_one relationship between validator_score_v1 and ip
    
    * Revert "Load all needed ips in controller"
    
    This reverts commit 78d3fa10641b1fff70200fa6939945ab289a76f4.
    
    * Change relationship name, select only needed fields
    
    * Includes ip_for_api to query
    
    * Use constant, remove method
    
    * Move array rendering to index
    
    * Add to_builder methods to models
    
    * Add default_url option to configs + add #api_url method to validator model + test
    
    * Update host for stage environment
    
    * Return hash instead of rendering jbuilder views
    
    * Change syntax
    
    * Change use_ssl to protocol in config
    
    * remove no longer needed jbuidler views

--- config/environments/test.rb
+++ config/environments/test.rb
@@ -53,3 +53,8 @@ Rails.application.configure do
   # config.action_view.raise_on_missing_translations = true
   config.action_mailer.default_url_options = { host: 'localhost', port: 3000 }
 end
+
+Rails.application.routes.default_url_options = {
+  host: 'localhost',
+  port: 3000
+}
