f8e2b7506b5293b3f8f752b8241165bf96075a7a
Radosław Stolarski <radoslaw.stolarski@polcode.net>
2021-07-27 15:57:54 +0200

    179000784_re_fix_action_view_template_error (#173)
    * Prevent malformed version from being saved
    
    * Add test for ValidatorsHelper

---
+++ test/helpers/validators_helper_test.rb
@@ -0,0 +1,44 @@
+# frozen_string_literal: true
+
+require 'test_helper'
+
+class ValidatorsHelperTest < ActiveSupport::TestCase
+  include ValidatorsHelper
+  def setup
+    @versions_array = [
+      {"1.6.16"=>{"count"=>2, "stake_percent"=>0.0}},
+      {"1.6.18"=>{"count"=>3, "stake_percent"=>0.0}},
+      {"1.6.9"=>{"count"=>4, "stake_percent"=>0.0}},
+      {"1.7.0"=>{"count"=>8, "stake_percent"=>0.0}},
+      {"1.7.1"=>{"count"=>9, "stake_percent"=>0.0}},
+      {"1.7.2"=>{"count"=>1, "stake_percent"=>0.0}},
+      {"1.7.3"=>{"count"=>36, "stake_percent"=>1.05}},
+      {"1.7.4"=>{"count"=>129, "stake_percent"=>7.39}},
+      {"1.7.5"=>{"count"=>84, "stake_percent"=>4.2}},
+      {"1.7.6"=>{"count"=>1697, "stake_percent"=>86.25}},
+      {"1.7.7"=>{"count"=>5, "stake_percent"=>0.81}},
+      {"1.8.0"=>{"count"=>2, "stake_percent"=>0.0}}
+    ]
+  end
+
+  test '#sort_software_versions sort versions in correct order' do
+
+    expected_order = [
+      "1.8.0",
+      "1.7.7",
+      "1.7.6",
+      "1.7.5",
+      "1.7.4",
+      "1.7.3",
+      "1.7.2",
+      "1.7.1",
+      "1.7.0",
+      "1.6.18",
+      "1.6.16",
+      "1.6.9"
+    ]
+
+    sorted_versions = sort_software_versions(@versions_array)
+    assert_equal expected_order, sorted_versions.map{ |e| e.keys }.flatten
+  end
+end
