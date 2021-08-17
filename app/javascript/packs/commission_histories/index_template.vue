5ba3d873f3c0100ce502cb5c83bf369851ab0e6b
Klaudia Biala <klaudia.biala@polcode.net>
2021-08-17 15:47:08 +0200

    Style pagination

--- app/javascript/packs/commission_histories/index_template.vue
+++ app/javascript/packs/commission_histories/index_template.vue
@@ -4,16 +4,16 @@
       <table class='table mb-0'>
         <thead>
           <tr>
-            <th>
+            <th class="narrower-column">
               <a href="#" @click.prevent="sort_by_validator">Validator</a>
             </th>
-            <th class="narrow-column">
+            <th class="narrower-column">
               <a href="#" @click.prevent="sort_by_epoch">Epoch</a><br />
               <small>(completion %)</small>
             </th>
             <th class="wider-column">Batch</th>
             <th class="wider-column text-center">Before<i class="fas fa-long-arrow-alt-right px-2"></i>After</th>
-            <th class="narrow-column">
+            <th class="narrower-column">
               <a href="#" @click.prevent="sort_by_timestamp">Timestamp</a>
             </th>
           </tr>
@@ -23,11 +23,15 @@
           </commission-history-row>
         </tbody>
       </table>
+      <div class="pt-2 px-3">
+        <b-pagination
+            v-model="page"
+            :total-rows="total_count"
+            :per-page="25"
+            first-text="« First"
+            last-text="Last »" />
+      </div>
     </div>
-    <b-pagination 
-      v-model="page" 
-      :total-rows="total_count" 
-      :per-page="25" />
   </div>
 </template>
 
