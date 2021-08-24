a7e1cb7cce7196ec9673b9c0ba75e4853cbcbcce
Klaudia Biala <klaudia.biala@polcode.net>
2021-08-24 16:55:08 +0200

    Add tests

---
+++ test/factories/opt_out_request.rb
@@ -0,0 +1,11 @@
+FactoryBot.define do
+  factory :opt_out_request do
+    name { Faker::Name.name[0..15] }
+    request_type { 'opt_out' }
+    street_address { Faker::Address.street_address }
+    city { Faker::Address.city }
+    postal_code { Faker::Address.postcode }
+    state { Faker::Address.state_abbr }
+    meta_data { { ip: '127.0.0.1' } }
+  end
+end
