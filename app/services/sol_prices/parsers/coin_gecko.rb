a90f519e8c77cd4048a10e1fb2b566791ea8f95c
Radosław Stolarski <radoslaw.stolarski@polcode.net>
2021-09-08 15:50:25 +0200

    Add parsers for data from coingecko

---
+++ app/services/sol_prices/parsers/coin_gecko.rb
@@ -0,0 +1,42 @@
+module SolPrices
+  module Parsers
+    module CoinGecko
+      def prices_from_ohlc(response)
+        response.map do |row|
+          datetime = convert_to_datetime_utc(row[0])
+          open_price = row[1]
+          high_price = row[2]
+          low_price = row[3]
+          close_price = row[4]
+
+          {
+            exchange: SolPrice.exchanges[:coingecko],
+            currency: SolPrice.currencies[:usd],
+            open: open_price,
+            high: high_price,
+            low: low_price,
+            close: close_price,
+            datetime_from_exchange: datetime
+          }
+        end
+      end
+
+      # For getting volume from coingecko.
+      def volume_from_daily_historical_price(response)
+        volumes = response['total_volumes']
+
+        volumes.map do |volume|
+          datetime = convert_to_datetime_utc(volume[0])
+
+          { datetime: datetime, volume: volume[1] }
+        end
+      end
+
+      # Convert timestamp to datetime
+      def convert_to_datetime_utc(timestamp)
+        timestamp = timestamp / 1000 # removes miliseconds 
+        Time.at(timestamp).utc.to_datetime
+      end
+    end
+  end
+end
---
+++ test/services/sol_prices/parsers/coin_gecko_test.rb
@@ -0,0 +1,52 @@
+require 'test_helper'
+
+module SolPrices
+  module Parsers
+    class CoinGeckoTest < ActiveSupport::TestCase
+      include VcrHelper
+      include Parsers::CoinGecko
+
+      setup do
+        @namespace = File.join('services', 'sol_prices', 'parsers', 'coin_gecko')
+        @wrapper = ApiWrappers::CoinGecko.new
+      end 
+
+      test '#prices_from_ohlc' do
+        vcr_cassette(@namespace, __method__) do
+          response = @wrapper.ohlc(days: 'max')
+
+          sol_prices_array = prices_from_ohlc(response)
+
+          assert_equal 136, sol_prices_array.size
+
+          assert_difference 'SolPrice.count', 136 do
+            SolPrice.create(sol_prices_array)
+          end
+        end
+      end
+
+      test '#prices_from_ohlc 1 day' do
+        vcr_cassette(@namespace, __method__) do
+          response = @wrapper.ohlc(days: '1')        
+          sol_prices_array = prices_from_ohlc(response)
+          assert_equal 49, sol_prices_array.size
+        end
+      end
+
+      test '#volume_from_daily_historical_price' do
+        vcr_cassette(@namespace, __method__) do
+          expected = [
+            5245858966.571948,
+            16149087501.300968,
+            13505195046.862394
+          ]
+          
+          response = @wrapper.daily_historical_price(days: '2')
+          parsed = volume_from_daily_historical_price(response)
+          
+          assert_equal expected, parsed.map { |e| e[:volume] }
+        end
+      end
+    end
+  end
+end
