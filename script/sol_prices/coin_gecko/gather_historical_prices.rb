6d03a8440906e3b59e06fddad680d5ed2347999d
Radosław Stolarski <radoslaw.stolarski@polcode.net>
2021-09-10 15:01:34 +0200

    Add ability to pass argument to script

--- script/sol_prices/coin_gecko/gather_historical_prices.rb
+++ script/sol_prices/coin_gecko/gather_historical_prices.rb
@@ -1,9 +1,10 @@
 # frozen_string_literal: true
 
-# RAILS_ENV=production bundle exec rails r script/gather_yesterday_sol_prices_from_coin_gecko.rb
+# Argument is optional, when is omitted max will be used.
+# Valid values: 1/7/14/30/90/180/365/'max'. Defaults to 7 days.
+# 
+# RAILS_ENV=production bundle exec rails r script/gather_historical_prices_from_coin_gecko.rb 365 
 
-# Script that runs 1-time per day, just after midnight UTC, 
-# to the get the price data for the previous day.
 require_relative '../../../config/environment'
 require 'coin_gecko_logic'
 
@@ -13,7 +14,7 @@ include CoinGeckoLogic
 initial_payload = {
   client: SolPrices::ApiWrappers::CoinGecko.new,
   datetime: Time.now.utc.beginning_of_day.to_datetime,
-  days: 'max'
+  days: ARGV[0] || 'max'
 }
 
 p = Pipeline.new(200, initial_payload)
