d49db9cbbbf678619f45472fc5c36e1ff0e0aebf
Radosław Stolarski <radoslaw.stolarski@polcode.net>
2021-09-20 19:03:07 +0200

    Update script to get correct prices

--- script/sol_prices/coin_gecko_gather_historical_prices.rb
+++ script/sol_prices/coin_gecko_gather_historical_prices.rb
@@ -5,22 +5,32 @@
 # 
 # RAILS_ENV=production bundle exec rails r script/gather_historical_prices_from_coin_gecko.rb 365 
 
-require_relative '../../../config/environment'
+require_relative '../../config/environment'
 
 include SolPrices::CoinGeckoLogic
 include SolPrices::SharedLogic
 
 # Create our initial payload with the input values
+datetime = DateTime.new(2020,7,27)
+
 initial_payload = {
   exchange: SolPrice.exchanges[:coin_gecko],
-  client: SolPrices::ApiWrappers::CoinGecko.new,
-  datetime: DateTime.current.beginning_of_day,
-  days: ARGV[0] || 'max'
+  client: SolPrices::ApiWrappers::CoinGecko.new
 }
 
-p = Pipeline.new(200, initial_payload)
-            .then(&get_ohlc_prices)
-            .then(&assign_epochs)
-            .then(&save_sol_prices)
-            .then(&log_info)
+begin
+  until datetime > DateTime.current
+    initial_payload[:datetime] = datetime
 
+    p = Pipeline.new(200, initial_payload)
+                .then(&get_historical_average_price)
+                .then(&assign_epochs)
+                .then(&save_sol_prices)
+                .then(&log_info)
+
+    datetime = datetime + 1.day
+  end
+
+rescue StandardError => e
+  p e
+end
