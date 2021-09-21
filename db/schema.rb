bcf85512d1e7b480144cd98a70c06d719264bbbb
Radosław Stolarski <radoslaw.stolarski@polcode.net>
2021-09-21 14:23:33 +0200

    Fix db timestampt

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2021_09_07_110222) do
+ActiveRecord::Schema.define(version: 2021_09_20_133806) do
 
   create_table "active_storage_attachments", charset: "utf8mb4", collation: "utf8mb4_0900_ai_ci", force: :cascade do |t|
     t.string "name", null: false
