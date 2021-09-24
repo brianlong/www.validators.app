c0a56e80b4a5b819480b398151c95d609bda0d7f
Radoslaw Stolarski <radoslaw.stolarski@polcode.net>
2021-09-24 16:40:12 +0200

    Add basic table view for sol_prices

---
+++ test/controllers/sol_prices_controller_test.rb
@@ -0,0 +1,8 @@
+require "test_helper"
+
+class SolPricesControllerTest < ActionDispatch::IntegrationTest
+  test "should get index" do
+    get sol_prices_index_url
+    assert_response :success
+  end
+end
