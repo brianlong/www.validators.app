b9f6653b1ee83149c7c9f9a9550de3dbdbf05a9c
Radoslaw Stolarski <radoslaw.stolarski@polcode.net>
2021-09-24 19:05:58 +0200

    Add candlestick chart for ftx data

--- app/javascript/packs/application.js
+++ app/javascript/packs/application.js
@@ -8,6 +8,9 @@ require("turbolinks").start()
 require("@rails/activestorage").start()
 require("channels")
 import Chart from 'chart.js/auto';
+import { DateTime } from "luxon";
+import 'chartjs-adapter-luxon';
+import './chart-financial';
 
 // Uncomment to copy all static images under ../images to the output folder and reference
 // them with the image_pack_tag helper in views (e.g <%= image_pack_tag 'rails.png' %>)
@@ -45,6 +48,7 @@ document.addEventListener("turbolinks:load", function() {
 
 document.addEventListener('turbolinks:load', () => {
     var ctx = document.getElementById('myChart').getContext('2d');
+    var ctx2 = document.getElementById('myChart2').getContext('2d');
     var myChart = new Chart(ctx, {
     type: 'line',
     data: {
@@ -52,6 +56,23 @@ document.addEventListener('turbolinks:load', () => {
       datasets: [{
         label: 'SOL Token Price',
         data: JSON.parse(ctx.canvas.dataset.data),
+        fill: false,
+        borderColor: 'rgb(75, 192, 192)',
+        tension: 0.1
+      }]
+    },
+  });
+
+  const data2 = JSON.parse(ctx2.canvas.dataset.data)
+  // Converts string timestamp to Int for luxon
+  data2.forEach(el => el['x'] = parseInt(el['x']));
+
+  var myChart2 = new Chart(ctx2, {
+    type: 'candlestick',
+    data: {
+      datasets: [{
+        label: 'SOL Token Price',
+        data: data2,
         fill: false,
         borderColor: 'rgb(75, 192, 192)',
         tension: 0.1
