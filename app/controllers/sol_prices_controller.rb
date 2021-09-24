85e5cea2486eb53b4ce863520f5441ce721c4d09
Radoslaw Stolarski <radoslaw.stolarski@polcode.net>
2021-09-24 19:05:58 +0200

    Add candlestick chart for ftx data

--- app/controllers/sol_prices_controller.rb
+++ app/controllers/sol_prices_controller.rb
@@ -7,5 +7,15 @@ class SolPricesController < ApplicationController
       datetime.to_date.to_s
     end
     @coin_gecko_data = @coin_gecko_prices.pluck(:average_price)
+
+    @ftx_data = @ftx_prices.map do |ftx_price|
+      {
+        x: ftx_price.datetime_from_exchange.to_datetime.strftime('%Q'),
+        o: ftx_price.open,
+        h: ftx_price.high,
+        l: ftx_price.low,
+        c: ftx_price.close
+      }
+    end.last(100) # This is number of records that is displayed fine on the chart.
   end
 end
