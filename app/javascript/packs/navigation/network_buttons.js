16be1fa5565cc0e6f215fbe2118092494a3e3649
maciejkocylapc <maciej.kocyla@polcode.net>
2021-09-27 14:43:00 +0200

    fix buttons for turbolinks

--- app/javascript/packs/navigation/network_buttons.js
+++ app/javascript/packs/navigation/network_buttons.js
@@ -1,9 +1,12 @@
 import Vue from 'vue/dist/vue.esm'
 import NetworkButtonsTemplate from './network_buttons_template'
+import TurbolinksAdapter from 'vue-turbolinks';
 
-document.addEventListener('DOMContentLoaded', () => {
+Vue.use(TurbolinksAdapter);
+
+document.addEventListener('turbolinks:load', () => {
   const chindex = new Vue({
-    el: '#network-buttons',    
+    el: '#network-buttons',
     render(createElement) {
       return createElement(NetworkButtonsTemplate)
     }
