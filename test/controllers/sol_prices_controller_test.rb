c714143af66fb13df895c56372ebf3144ef9559e
Radosław Stolarski <radoslaw.stolarski@polcode.net>
2021-09-27 13:24:16 +0200

    Fix url in test

--- test/controllers/sol_prices_controller_test.rb
+++ test/controllers/sol_prices_controller_test.rb
@@ -2,7 +2,7 @@ require "test_helper"
 
 class SolPricesControllerTest < ActionDispatch::IntegrationTest
   test "should get index" do
-    get sol_prices_index_url
+    get sol_prices_url
     assert_response :success
   end
 end
