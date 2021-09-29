d9b3999b12ce6b3b95fc00d790a0da0830c10d18
Radosław Stolarski <radoslaw.stolarski@polcode.net>
2021-09-29 14:39:35 +0200

    Display last 100 days on coin gecko tab for more readability and coherence with fth chart

--- app/controllers/sol_prices_controller.rb
+++ app/controllers/sol_prices_controller.rb
@@ -5,10 +5,10 @@ class SolPricesController < ApplicationController
     @ftx_prices = SolPrice.where(exchange: SolPrice.exchanges[:ftx])
                           .order(datetime_from_exchange: :asc)
 
-    @coin_gecko_labels = @coin_gecko_prices.pluck(:datetime_from_exchange).map do |datetime|
+    @coin_gecko_labels = @coin_gecko_prices.pluck(:datetime_from_exchange).last(100).map do |datetime|
       datetime.to_date.to_s
     end
-    @coin_gecko_data = @coin_gecko_prices.pluck(:average_price)
+    @coin_gecko_data = @coin_gecko_prices.pluck(:average_price).last(100)
 
     @ftx_data = @ftx_prices.map do |ftx_price|
       {
