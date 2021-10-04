836a8dae9c295ba987abc1310c75576c7173c928
Radosław Stolarski <radoslaw.stolarski@polcode.net>
2021-10-04 13:42:47 +0200

    177618323-historical-SOL-prices-views (#218)
    * Add basic table view for sol_prices
    
    * Add chart.js
    
    * Add chart for average price from coin gecko
    
    * Add candlestick chart for ftx data
    
    * Order by datetime_from_exchange asc
    
    * Remove tables
    
    * Fix url in test
    
    * Add note about data providers
    
    * Add footer nav link to sol prices
    
    * Add sol_price factory
    
    * Move charts to separate file
    
    * Convert to int on the backend
    
    * Display last 100 days on coin gecko tab for more readability and coherence with fth chart
    
    * Update colors
    
    * Chart style tweaks
    
    * Update color for down candle
    
    * Update color for down candle v2
    
    * Set data-turbolinks to false in footer li element
    
    * Moves sol_prices_charts outside packs folder, imports in application, check if id exists
    
    * Add yarn.lock
    
    * Revert unwanted change
    
    * Add link to data centers in footer
    
    * Revert out of scope change
    
    Co-authored-by: Klaudia Biala <klaudia.biala@polcode.net>

---
+++ app/javascript/src/sol_prices_charts.js
@@ -0,0 +1,61 @@
+
+import Chart from 'chart.js/auto';
+import { DateTime } from "luxon";
+import 'chartjs-adapter-luxon';
+import '../lib/chart-financial';
+
+document.addEventListener('turbolinks:load', () => {
+  const chartCoinGecko = document.getElementById('myChart')
+  const chartFtx = document.getElementById('myChart2')
+
+  if (chartCoinGecko == null && chartFtx == null) {
+    return null;
+  }
+
+  const ctx = chartCoinGecko.getContext('2d');
+  const ctx2 = chartFtx.getContext('2d');
+  const dataset = ctx.canvas.dataset
+  const dataset2 = ctx2.canvas.dataset
+
+  const myChart = new Chart(ctx, {
+    type: 'line',
+    data: {
+      labels: JSON.parse(dataset.labels),
+      datasets: [{
+        label: 'SOL Token Price',
+        data: JSON.parse(dataset.data),
+        fill: false,
+        tension: 0.1,
+        borderColor: dataset.color,
+        backgroundColor: dataset.color,
+        borderWidth: 1,
+      }]
+    },
+    options: {
+      plugins: {
+        legend: {
+          onClick: null
+        }
+      }
+    }
+  });
+
+  const myChart2 = new Chart(ctx2, {
+    type: 'candlestick',
+    data: {
+      datasets: [{
+        label: 'SOL Token Price',
+        data: JSON.parse(dataset2.data),
+        fill: false,
+        tension: 0.1,
+      }]
+    },
+    options: {
+      plugins: {
+        legend: {
+          onClick: null
+        }
+      }
+    }
+  });
+})
