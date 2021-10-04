ee313c60f562ad880e9d7942980fb871eba310cf
Radosław Stolarski <radoslaw.stolarski@polcode.net>
2021-10-04 17:46:15 +0200

    Update ip factory, add assertions for delinquent validators

--- test/factories/ips.rb
+++ test/factories/ips.rb
@@ -1,35 +1,44 @@
 FactoryBot.define do
   factory :ip do
     address { Faker::Internet.ip_v4_address }
-    continent_code { 'AS' }
-    continent_geoname_id { 6255147 }
-    continent_name { 'Asia' }
-    country_confidence { 99 }
-    country_geoname_id { 1814991 }
-    country_name { 'China' }
-    registered_country_iso_code { 'CN' }
-    registered_country_geoname_id { 1814991 }
-    registered_country_name { 'China' }
     traits_anonymous { true }
     traits_hosting_provider { true }
     traits_user_type { 'hosting' }
     traits_autonomous_system_number { 00000 }
     traits_isp { 'Organization' }
     traits_organization { 'Organization' }
-    location_time_zone { 'Asia/Shanghai' }
 
     trait :ip_berlin do
-      traits_autonomous_system_number { 54321 }
-      data_center_key { '54321-DE-Europe/Berlin' }
-      traits_autonomous_system_organization { 'Berlin Organisation' }
+      continent_code { 'EU' }
+      country_confidence { 99 }
+      country_geoname_id { 276 }
       country_iso_code { 'DE' }
+      continent_name { 'Europe' }
+      country_name { 'Germany' }
+      data_center_key { '54321-DE-Europe/Berlin' }
+      location_time_zone { 'Europe/CET' }
+      registered_country_iso_code { 'DE' }
+      registered_country_geoname_id { 276 }
+      registered_country_name { 'Germany' }
+      traits_autonomous_system_number { 54321 }
+      traits_autonomous_system_organization { 'Berlin Organisation' }
     end
 
     trait :ip_china do
-      traits_autonomous_system_number { 12345 }
-      data_center_key { '12345-CN-Asia/Shanghai' }
-      traits_autonomous_system_organization { 'Chinese Organisation' }
+      continent_code { 'AS' }
+      continent_geoname_id { 6255147 }
+      continent_name { 'Asia' }
+      country_confidence { 99 }
+      country_geoname_id { 1814991 }
       country_iso_code { 'CN' }
+      country_name { 'China' }
+      data_center_key { '12345-CN-Asia/Shanghai' }
+      location_time_zone { 'Asia/Shanghai' }
+      registered_country_iso_code { 'CN' }
+      registered_country_geoname_id { 1814991 }
+      registered_country_name { 'China' }
+      traits_autonomous_system_number { 12345 }
+      traits_autonomous_system_organization { 'Chinese Organisation' }
     end
   end
 end
--- test/services/sorted_data_centers_test.rb
+++ test/services/sorted_data_centers_test.rb
@@ -3,15 +3,19 @@ require 'test_helper'
 class SortedDataCenterTest < ActiveSupport::TestCase
 
   setup do
+    scores_china = []
+    scores_berlin = []
+
     3.times do
       ip = create(:ip, :ip_china)
-      create(
+      score = create(
         :validator_score_v1,
         ip_address: ip.address,
         active_stake: 100,
         network: 'testnet',
         data_center_key: ip.data_center_key
       )
+      scores_china << score
     end
     2.times do
       ip = create(
@@ -19,24 +23,32 @@ class SortedDataCenterTest < ActiveSupport::TestCase
         :ip_berlin,
         traits_autonomous_system_number: 12345
       )
-      create(
+      score = create(
         :validator_score_v1,
         ip_address: ip.address,
         active_stake: 100,
         network: 'testnet',
         data_center_key: ip.data_center_key
       )
+
+      scores_berlin << score
     end
     3.times do
       ip = create(:ip, :ip_berlin)
-      create(
+      score = create(
         :validator_score_v1,
         ip_address: ip.address,
         active_stake: 100,
         network: 'testnet',
         data_center_key: ip.data_center_key
       )
+
+      scores_berlin << score
     end
+
+    scores_china.first.update(delinquent: true)
+    scores_berlin.first.update(delinquent: true)
+    scores_berlin.last.update(delinquent: true)
   end
 
   test 'SortedDataCenters when sort_by_asn returns correct result' do
@@ -48,6 +60,10 @@ class SortedDataCenterTest < ActiveSupport::TestCase
     assert_equal 8, result[:total_population]
     assert_equal 800, result[:total_stake]
     assert_equal 2, result[:results][0][1][:data_centers].count
+    assert_equal 3, result[:total_delinquent]
+    assert_equal 2, result[:results][0][1][:delinquent_validators] # for traits_autonomous_system_number: 12345
+    assert_equal 1, result[:results][1][1][:delinquent_validators] # for traits_autonomous_system_number: 54321
+
   end
 
   test 'SortedDataCenters when sort_by_data_centers returns correct result' do
@@ -59,5 +75,9 @@ class SortedDataCenterTest < ActiveSupport::TestCase
     assert_equal 8, result[:total_population]
     assert_equal 800, result[:total_stake]
     assert_equal 3, result[:results].count
+    assert_equal 3, result[:total_delinquent]
+    assert_equal 1, result[:results][0][1][:delinquent_validators] # 12345-CN-Asia/Shanghai
+    assert_equal 1, result[:results][1][1][:delinquent_validators] # 54321-DE-Europe/CET
+    assert_equal 1, result[:results][2][1][:delinquent_validators] # 12345-DE-Europe/CET
   end
 end
