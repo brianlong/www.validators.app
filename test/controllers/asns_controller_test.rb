056ead922c5d3e145494d675eaa6efa8d211420f
Radosław Stolarski <radoslaw.stolarski@polcode.net>
2021-10-26 11:21:00 +0200

    180027663_stop_display_data_centers_with_no_validators (#244)
    * Join Ip with score to get data centers with at least one validator
    
    * Update test

--- test/controllers/asns_controller_test.rb
+++ test/controllers/asns_controller_test.rb
@@ -2,14 +2,16 @@ require 'test_helper'
 
 class AsnControllerTest < ActionDispatch::IntegrationTest
   setup do
-    create(
+    @ip = create(
       :ip, 
       traits_autonomous_system_number: 12345, 
       data_center_key: 'data_center_key'
     )
   end
 
-  test 'should get show with autonomous_system_number that exists ' do
+  test 'should get show with autonomous_system_number that exists and has at least one score' do
+    create(:validator_score_v1, ip_address: @ip.address)
+
     get asn_url(network: 'testnet', asn: 12345)
     assert_response :success
   end
