ad8100bceadcfdb0100e3a4f5a91ad97fb2b9148
Klaudia Biala <klaudia.biala@polcode.net>
2021-11-03 20:05:42 +0100

    180135617 Add jpool to homepage (#255)
    * Prepare a carousel for more stake pools
    
    * Update Jpool logo

--- app/helpers/validators_helper.rb
+++ app/helpers/validators_helper.rb
@@ -81,7 +81,9 @@ module ValidatorsHelper
     [
       ['https://lido.fi/solana', 'lido.png'],
       ['https://marinade.finance', 'marinade.png'],
-      ['https://www.socean.fi', 'socean.png']
+      ['https://www.socean.fi', 'socean.png'],
+      ['https://jpool.one/', 'jpool.png']
+      # add more stake pools here
     ].shuffle
   end
 end
