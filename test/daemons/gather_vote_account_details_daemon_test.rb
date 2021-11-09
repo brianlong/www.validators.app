4b77aa1aba601673c431378bd34655dbff9cd5da
Klaudia Biała <klaudia.biala@polcode.net>
2021-11-09 18:09:37 +0100

    Remove left over debug code

--- test/daemons/gather_vote_account_details_daemon_test.rb
+++ test/daemons/gather_vote_account_details_daemon_test.rb
@@ -15,7 +15,6 @@ class GatherVoteAccountDetailsDaemonTest < ActiveSupport::TestCase
   end
 
   test 'validator_identity and authorized_withdrawer should be updated' do
-    binding.pry
     json_data = {
       "validatorIdentity":"abcdef",
       "authorizedWithdrawer":"abcdef"
@@ -45,7 +44,6 @@ class GatherVoteAccountDetailsDaemonTest < ActiveSupport::TestCase
       ).call
 
       @v.score.reload
-      binding.pry
       assert_equal 0, @v.score.authorized_withdrawer_score
     end
   end
