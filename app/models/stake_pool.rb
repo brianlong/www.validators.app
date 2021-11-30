5cbe5f0a7f2e517ec1105d6247a68c6231ec473e
Radosław Stolarski <radoslaw.stolarski@polcode.net>
2021-11-30 15:04:16 +0100

    Add average_validators_fee float field to stake_pools

--- app/models/stake_pool.rb
+++ app/models/stake_pool.rb
@@ -2,13 +2,14 @@
 #
 # Table name: stake_pools
 #
-#  id          :bigint           not null, primary key
-#  authority   :string(191)
-#  manager_fee :float(24)
-#  name        :string(191)
-#  network     :string(191)
-#  created_at  :datetime         not null
-#  updated_at  :datetime         not null
+#  id                            :bigint           not null, primary key
+#  authority                     :string(191)
+#  average_validators_commission :float(24)
+#  manager_fee                   :float(24)
+#  name                          :string(191)
+#  network                       :string(191)
+#  created_at                    :datetime         not null
+#  updated_at                    :datetime         not null
 #
 class StakePool < ApplicationRecord
   has_many :stake_accounts
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2021_11_29_160614) do
+ActiveRecord::Schema.define(version: 2021_11_30_140342) do
 
   create_table "active_storage_attachments", charset: "utf8mb4", collation: "utf8mb4_unicode_ci", force: :cascade do |t|
     t.string "name", null: false
@@ -328,6 +328,7 @@ ActiveRecord::Schema.define(version: 2021_11_29_160614) do
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
     t.float "manager_fee"
+    t.float "average_validators_commission"
   end
 
   create_table "users", charset: "utf8mb4", collation: "utf8mb4_unicode_ci", force: :cascade do |t|
