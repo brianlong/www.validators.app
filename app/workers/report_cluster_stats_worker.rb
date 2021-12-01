ba18624e76f06ade9dbcd18615b703a970d1c769
maciejkocylapc <maciej.kocyla@polcode.net>
2021-12-01 13:31:08 +0100

    remove unnecessery comments

--- app/workers/report_cluster_stats_worker.rb
+++ app/workers/report_cluster_stats_worker.rb
@@ -1,15 +1,10 @@
 # frozen_string_literal: true
 
-# ReportClusterStatsWorker.perform_async(
-#   network: 'testnet',
-#   batch_uuid: '65c9083f-8f53-4873-a9f8-8f782e276d30'
-# )
 class ReportClusterStatsWorker
   include Sidekiq::Worker
   include ReportLogic
 
   def perform(args = {})
-    # byebug
     payload = {
       network: args["network"],
       batch_uuid: args["batch_uuid"]
