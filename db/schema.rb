0c8eef3c3a9eea9917fc830d5a7a9e69b8fd81f5
Radosław Stolarski <radoslaw.stolarski@polcode.net>
2021-12-27 14:58:59 +0100

    Add admin_warning field to validators table

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2021_12_02_090220) do
+ActiveRecord::Schema.define(version: 2021_12_27_132812) do
 
   create_table "active_storage_attachments", charset: "utf8mb4", collation: "utf8mb4_0900_ai_ci", force: :cascade do |t|
     t.string "name", null: false
@@ -490,6 +490,7 @@ ActiveRecord::Schema.define(version: 2021_12_02_090220) do
     t.string "security_report_url"
     t.boolean "is_rpc", default: false
     t.boolean "is_active", default: true
+    t.string "admin_warning"
     t.index ["network", "account"], name: "index_validators_on_network_and_account", unique: true
   end
 
