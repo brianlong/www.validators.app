5fa0d5ada74a6574209f713a082d00665ba4f05a
maciejkocylapc <maciej.kocyla@polcode.net>
2022-01-11 14:43:16 +0100

    single quotes to double quotes

--- script/fix_ip_webnx.rb
+++ script/fix_ip_webnx.rb
@@ -1,25 +1,25 @@
 # frozen_string_literal: true
 
-require File.expand_path('../config/environment', __dir__)
-require File.expand_path('./concerns/fix_ip_module', __dir__)
+require File.expand_path("../config/environment", __dir__)
+require File.expand_path("./concerns/fix_ip_module", __dir__)
 
 include FixIpModule
 
-ASO = 'WEBNX'
+ASO = "WEBNX"
 
 WEBNX_HOSTS = {
   "tier-four.demarc" => {
-    country_iso_code: 'US',
-    country_name: 'United States',
-    city_name: 'Ogden',
-    data_center_key: '18450-US-Ogden',
+    country_iso_code: "US",
+    country_name: "United States",
+    city_name: "Ogden",
+    data_center_key: "18450-US-Ogden",
     aso: ASO
   },
   "ip4.gtt.net" => {
-    country_iso_code: 'US',
-    country_name: 'United States',
-    city_name: 'Los Angeles',
-    data_center_key: '18450-US-Los Angeles',
+    country_iso_code: "US",
+    country_name: "United States",
+    city_name: "Los Angeles",
+    data_center_key: "18450-US-Los Angeles",
     aso: ASO
   }
 }
@@ -35,7 +35,7 @@ Ip.where(traits_autonomous_system_number: 18_450)
   WEBNX_HOSTS.each do |host_reg, host_data|
     next unless last_webnx_ip&.include?(host_reg)
 
-    host = ('H' + last_webnx_ip).strip.split(' ')[1].strip
+    host = ("H" + last_webnx_ip).strip.split(" ")[1].strip
     setup_ip_override(ip: ip, host_data: host_data, host: host)
   end
 end
@@ -44,4 +44,4 @@ end
 update_ip_with_overrides
 update_validator_score_with_overrides
 
-puts 'end of script'
+puts "end of script"
