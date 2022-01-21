6bb11c8e5935fbdb8d86103f78587b9204b91a60
Radosław Stolarski <radoslaw.stolarski@polcode.net>
2022-01-21 13:14:50 +0100

    180903060_tweak_displaying_0_stake_pool_values (#314)
    * Change 'N / A' to 0
    
    * Keep N/A for average_apy

--- app/javascript/packs/stake_accounts/pool_stats.js
+++ app/javascript/packs/stake_accounts/pool_stats.js
@@ -28,7 +28,7 @@ var StakePoolStats = Vue.component('StakePoolStats', {
         <div class="text-center text-muted small mb-4">
           <a href="#" @click.prevent="go_to_metrics()">See metrics explanation</a>
         </div>
-        
+
         <div class="row pl-lg-4 pl-xl-5">
           <div class="col-md-4 pl-lg-4 pl-xl-5">
             <div class="mb-2">
@@ -53,18 +53,18 @@ var StakePoolStats = Vue.component('StakePoolStats', {
               <strong class="text-purple">{{ (total_stake / 1000000000).toLocaleString('en-US', {maximumFractionDigits: 0}) }} SOL</strong>
             </div>
           </div>
-          
+
           <div class="col-md-4 pl-lg-4 pl-xl-5">
             <div class="stat-title-3 mb-2">
               <i class="fas fa-hand-holding-usd text-success mr-2"></i>Fees
-            </div> 
+            </div>
             <div>
               <span class="text-muted">Manager Fee:&nbsp;</span>
-              <strong class="text-success">{{ pool.manager_fee ? pool.manager_fee + '%' : 'N / A' }}</strong>
+              <strong class="text-success">{{ pool.manager_fee ? pool.manager_fee + '%' : 0 }}</strong>
             </div>
             <div class="mb-4">
               <span class="text-muted">Avg Commission:&nbsp;</span>
-              <strong class="text-success">{{ pool.average_validators_commission ? pool.average_validators_commission.toFixed(2) : 'N / A' }}%</strong>
+              <strong class="text-success">{{ pool.average_validators_commission ? pool.average_validators_commission.toFixed(2) : 0 }}%</strong>
             </div>
             <div class="mb-3">
               <span class="stat-title-3">
@@ -82,19 +82,19 @@ var StakePoolStats = Vue.component('StakePoolStats', {
             </div>
             <div>
               <span class="text-muted">Avg Skipped Slots:&nbsp;</span>
-              <strong class="text-success">{{ pool.average_skipped_slots ? pool.average_skipped_slots.toFixed(2) : 'N / A' }}&percnt;</strong>
+              <strong class="text-success">{{ pool.average_skipped_slots ? pool.average_skipped_slots.toFixed(2) : 0 }}&percnt;</strong>
             </div>
             <div>
               <span class="text-muted">Avg Lifetime:&nbsp;</span>
-              <strong class="text-success">{{ pool.average_lifetime ? pool.average_lifetime + ' days' : 'N / A' }}</strong>
+              <strong class="text-success">{{ pool.average_lifetime ? pool.average_lifetime + ' days' : 0 }}</strong>
             </div>
             <div>
               <span class="text-muted">Avg Uptime:&nbsp;</span>
-              <strong class="text-success">{{ pool.average_uptime ? pool.average_uptime.toFixed(1) + ' days': 'N / A' }}</strong>
+              <strong class="text-success">{{ pool.average_uptime ? pool.average_uptime.toFixed(1) + ' days': 0 }}</strong>
             </div>
             <div class="">
               <span class="text-muted">Avg Score:&nbsp;</span>
-              <strong class="text-success">{{ pool.average_score || 'N / A' }}</strong>
+              <strong class="text-success">{{ pool.average_score || 0 }}</strong>
             </div>
           </div>
         </div>
