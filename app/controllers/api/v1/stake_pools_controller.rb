5884b27cc87ae844a4c35482e6336eacadc70605
Klaudia Biala <klaudia.biala@polcode.net>
2022-01-26 13:48:18 +0100

    update old tests

--- app/controllers/api/v1/stake_pools_controller.rb
+++ app/controllers/api/v1/stake_pools_controller.rb
@@ -2,7 +2,7 @@ module Api
   module V1
     class StakePoolsController < BaseController
       def index
-        @stake_pools = StakePool.where(network: params[:network])
+        @stake_pools = StakePool.where(network: params[:network]).includes([:stake_accounts])
       end
 
       private
