fa1dc4ff7ff652edb99a4616e7c4300ce7d6963a
maciejkocylapc <maciej.kocyla@polcode.net>
2022-02-07 10:30:04 +0100

    apy helper

--- app/logic/stake_logic.rb
+++ app/logic/stake_logic.rb
@@ -3,6 +3,7 @@
 module StakeLogic
   include PipelineLogic
   include SolanaLogic #for solana_client_request
+  include ApyHelper
 
   def get_last_batch
     lambda do |p|
@@ -290,35 +291,5 @@ module StakeLogic
     rescue StandardError => e
       Pipeline.new(500, p.payload, "Error from calculate_apy_for_pools", e)
     end
-  end
-
-  private
-
-  # returns [epoch that's not finished yet, last finished epoch]
-  def set_epochs(network)
-    last_epoch = EpochWallClock.where(
-      network: network
-    ).last
-
-    previous_epoch = EpochWallClock.where(
-      network: network,
-      epoch: last_epoch.epoch - 1
-    ).last
-
-    [last_epoch, previous_epoch]
-  end
-
-  # returns reward from the account minus stake pool fee
-  def reward_with_fee(manager_fee, rewards)
-    return rewards unless manager_fee
-    return rewards - rewards * (manager_fee / 100)
-  end
-
-  # returns APY for a single account or nil
-  def calculate_apy(credits_diff, rewards, num_of_epochs)
-    credits_diff_percent = credits_diff / (rewards[:postBalance] - rewards[:amount]).to_f
-
-    apy = (((1 + credits_diff_percent) ** num_of_epochs) - 1) * 100
-    apy < 100 && apy > 0 ? apy.round(6) : nil
   end
 end
---
+++ app/logic/stake_logic/apy_helper.rb
@@ -0,0 +1,29 @@
+module StakeLogic::ApyHelper
+  # returns [epoch that's not finished yet, last finished epoch]
+  def set_epochs(network)
+    last_epoch = EpochWallClock.where(
+      network: network
+    ).last
+
+    previous_epoch = EpochWallClock.where(
+      network: network,
+      epoch: last_epoch.epoch - 1
+    ).last
+
+    [last_epoch, previous_epoch]
+  end
+
+  # returns reward from the account minus stake pool fee
+  def reward_with_fee(manager_fee, rewards)
+    return rewards unless manager_fee
+    return rewards - rewards * (manager_fee / 100)
+  end
+
+  # returns APY for a single account or nil
+  def calculate_apy(credits_diff, rewards, num_of_epochs)
+    credits_diff_percent = credits_diff / (rewards[:postBalance] - rewards[:amount]).to_f
+
+    apy = (((1 + credits_diff_percent) ** num_of_epochs) - 1) * 100
+    apy < 100 && apy > 0 ? apy.round(6) : nil
+  end
+end
