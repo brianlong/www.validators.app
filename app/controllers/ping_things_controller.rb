b62385e69fb8495229a641aa9fe79a886f9fa72f
Klaudia Biala <klaudia.biala@polcode.net>
2022-02-08 18:47:46 +0100

    Fix ping times query

--- app/controllers/ping_things_controller.rb
+++ app/controllers/ping_things_controller.rb
@@ -3,6 +3,6 @@ class PingThingsController < ApplicationController
     @ping_things = PingThing.where(network: params[:network])
                             .includes(:user)
                             .order(created_at: :desc)
-                            .last(60)
+                            .first(60)
   end
 end
