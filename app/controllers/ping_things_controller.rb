c542c1a72a7e44eb14ea8db197a030313fd0c0e6
Klaudia Biala <klaudia.biala@polcode.net>
2022-02-08 18:37:23 +0100

    Add includes to query

--- app/controllers/ping_things_controller.rb
+++ app/controllers/ping_things_controller.rb
@@ -1,6 +1,7 @@
 class PingThingsController < ApplicationController
   def index
     @ping_things = PingThing.where(network: params[:network])
+                            .includes(:user)
                             .order(created_at: :desc)
                             .last(60)
   end
