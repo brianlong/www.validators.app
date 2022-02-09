4bf7d3d62df53f5337c4ef4213ae3086fb6bbd97
maciejkocylapc <80820353+maciejkocylapc@users.noreply.github.com>
2022-02-09 14:43:40 +0100

    Update config/routes.rb
    Co-authored-by: Radosław Stolarski <radoslaw.stolarski@polcode.net>

--- config/routes.rb
+++ config/routes.rb
@@ -138,7 +138,7 @@ Rails.application.routes.draw do
       # api_v1_ping_times GET /api/v1/ping_times
       get 'ping-times/:network', to: 'api#ping_times', as: 'ping_times'
 
-      post 'ping-thing/:network/:token', to: 'ping_thing#create', as: 'ping_thing'
+      post 'ping-things/:network/:token', to: 'ping_things#create', as: 'ping_things'
     end
   end
 end
