846d55f1a8b7eae50734dd05e4002af5bd1df80a
maciejkocylapc <80820353+maciejkocylapc@users.noreply.github.com>
2022-02-09 14:43:47 +0100

    Update app/controllers/api/v1/ping_thing_controller.rb
    Co-authored-by: Radosław Stolarski <radoslaw.stolarski@polcode.net>

--- app/controllers/api/v1/ping_thing_controller.rb
+++ app/controllers/api/v1/ping_thing_controller.rb
@@ -1,6 +1,6 @@
 module Api
   module V1
-    class PingThingController < BaseController
+    class PingThingsController < BaseController
       
       def create
         api_token = request.headers["Token"]
