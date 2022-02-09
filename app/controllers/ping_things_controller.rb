5ec35756db4e96b2d0883d7357429df19867f052
maciejkocylapc <maciej.kocyla@polcode.net>
2022-02-09 17:27:57 +0100

    change number of records

--- app/controllers/ping_things_controller.rb
+++ app/controllers/ping_things_controller.rb
@@ -3,6 +3,6 @@ class PingThingsController < ApplicationController
     @ping_things = PingThing.where(network: params[:network])
                             .includes(:user)
                             .order(created_at: :desc)
-                            .first(60)
+                            .first(240)
   end
 end
