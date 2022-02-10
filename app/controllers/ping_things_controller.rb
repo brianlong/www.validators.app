2267121120e45d5e7ccfb5faacbadc7b1e2c3990
Klaudia Biala <klaudia.biala@polcode.net>
2022-02-10 16:56:54 +0100

    Remove unnecessary query

--- app/controllers/ping_things_controller.rb
+++ app/controllers/ping_things_controller.rb
@@ -6,7 +6,7 @@ class PingThingsController < ApplicationController
                             .includes(:user)
                             .order(created_at: :desc)
                             .first(240)
-    @ping_things_count = @ping_things.count
+    @ping_things_count = @ping_things.length
     @ping_things_array_for_chart = @ping_things.pluck(:response_time).reverse
   end
 end
