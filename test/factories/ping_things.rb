3b721877cfc23d0b3921061b6972b0f65c9471f7
Klaudia Biala <klaudia.biala@polcode.net>
2022-02-15 16:46:46 +0100

    Add tests

--- test/factories/ping_things.rb
+++ test/factories/ping_things.rb
@@ -2,10 +2,10 @@
 
 FactoryBot.define do
   factory :ping_thing do
-    user { nil }
+    user
     amount { "" }
-    signature { "MyString" }
+    signature { "0d7f418e4d1a3f80dc8a266cd867f766b73d9c80feea36524dfd074068bdef9221e356c192ac6ac71b71404d" }
     response_time { 1 }
-    transaction_type { "MyString" }
+    transaction_type { "transfer" }
   end
 end
