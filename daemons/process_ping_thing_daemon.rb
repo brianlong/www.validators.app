8020de4c6947fadf24a2980209524a3af051d9ce
maciejkocylapc <maciej.kocyla@polcode.net>
2022-02-21 15:28:19 +0100

    cleanup

--- daemons/process_ping_thing_daemon.rb
+++ daemons/process_ping_thing_daemon.rb
@@ -4,9 +4,14 @@ require_relative '../config/environment'
 
 class SkipAndSleep < StandardError; end
 
+# Send an interrupt with `ctrl-c` or `kill` to stop the script
+interrupted = false
+trap('INT') { interrupted = true }  unless Rails.env.test?
+
 begin
   loop do
     ProcessPingThingsService.new(records_count: 100).call
+    break if ARGV[0] == "once" || interrupted
     sleep(5)
   end
 rescue => e
--- test/daemons/process_ping_thing_daemon_test.rb
+++
@@ -1,7 +0,0 @@
-# fronze_string_literal: true
-
-require 'test_helper'
-
-class ProcessPingThingDaemonTest < ActiveSupport::TestCase
-
-end
