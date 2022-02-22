dda47c5a2e7a845e497ed9e633a0d31d2bb5d149
Radosław Stolarski <radoslaw.stolarski@polcode.net>
2022-02-22 18:53:57 +0100

    New append geo data script

---
+++ script/NEW_append_ip_geo_data.rb
@@ -0,0 +1,198 @@
+# frozen_string_literal: true
+
+# RAILS_ENV=production bundle exec ruby script/append_ip_geo_data.rb
+require File.expand_path('../config/environment', __dir__)
+require 'json'
+require 'net/http'
+require 'uri'
+require 'maxmind/geoip2'
+
+begin
+  interrupted = false
+  trap('INT') { interrupted = true }  unless Rails.env.test?
+
+  verbose = true # Rails.env == 'development'
+
+  # Setup the MaxMind client
+  client = MaxMind::GeoIP2::Client.new(
+    account_id: Rails.application.credentials.max_mind[:account_id],
+    license_key: Rails.application.credentials.max_mind[:license_key]
+  )
+
+  # SQL statement to find un-appended IPs
+  sql = "SELECT DISTINCT address
+         FROM validator_ips v
+         WHERE v.data_center_host_id IS NULL"
+
+  sql = "#{sql} LIMIT 10" if Rails.env == 'development'
+
+  ValidatorIp.connection.execute(sql).each do |missing_ip|
+    puts missing_ip[0].inspect if verbose
+    # Skip private IPs
+    next if missing_ip[0][0..2] == '10.'
+    next if missing_ip[0][0..3] == '192.'
+    next if missing_ip[0][0..3] == '127.'
+
+    record = client.insights(missing_ip[0])
+
+    data_center = DataCenter.find_or_create_by!(
+      # address: missing_ip[0],
+      continent_code: record.continent.code,
+      continent_geoname_id: record.continent.geoname_id,
+      continent_name: record.continent.name,
+      country_confidence: record.country.confidence,
+      country_iso_code: record.country.iso_code,
+      country_geoname_id: record.country.geoname_id,
+      country_name: record.country.name,
+      registered_country_iso_code: record.registered_country.iso_code,
+      registered_country_geoname_id: record.registered_country.geoname_id,
+      registered_country_name: record.registered_country.name,
+      traits_anonymous: record.traits.anonymous?,
+      traits_hosting_provider: record.traits.hosting_provider?,
+      traits_user_type: record.traits.user_type,
+      traits_autonomous_system_number: record.traits.autonomous_system_number,
+      traits_autonomous_system_organization: record.traits.autonomous_system_organization,
+      traits_isp: record.traits.isp,
+      traits_organization: record.traits.organization,
+      city_confidence: record.city.confidence,
+      city_geoname_id: record.city.geoname_id,
+      city_name: record.city.name,
+      location_average_income: record.location.average_income,
+      location_population_density: record.location.population_density,
+      location_accuracy_radius: record.location.accuracy_radius,
+      location_latitude: record.location.latitude,
+      location_longitude: record.location.longitude,
+      location_metro_code: record.location.metro_code,
+      location_time_zone: record.location.time_zone,
+      postal_confidence: record.postal.confidence,
+      postal_code: record.postal.code
+    )
+  
+    unless record.most_specific_subdivision.nil?
+      data_center.update(
+        subdivision_confidence: record.most_specific_subdivision.confidence,
+        subdivision_iso_code: record.most_specific_subdivision.iso_code,
+        subdivision_geoname_id: record.most_specific_subdivision.geoname_id,
+        subdivision_name: record.most_specific_subdivision.name
+      )
+    end
+
+    data_center_host = DataCenterHost.find_or_create_by!(
+      data_center: data_center,
+      host: nil
+    )
+
+    val_ip = ValidatorIp.find_by(address: missing_ip.first)
+    val_ip.update(
+      data_center_host: data_center_host,
+      traits_domain: record.traits.domain, 
+      traits_ip_address: record.traits.ip_address,
+      traits_network: record.traits.network
+    )
+  end
+
+  puts '' if verbose
+  # Update validator_score_v1s with the latest data
+  ValidatorScoreV1.find_each do |vs1|
+    next unless vs1.validator && vs1.validator&.ip_address
+
+    validator = vs1.validator
+    validator_ip = validator.validator_ips.order('updated_at desc').first
+
+    ip = validator_ip.address
+    puts ip if verbose
+
+    vs1.network = validator.network
+    vs1.ip_address = ip
+    unless validator_ip.data_center_host.nil?
+      vs1.data_center_key = validator_ip.data_center_host.data_center_key
+    end
+    vs1.save
+  end
+
+  # Clean up the scores table to remove data_center_host if the node moved out
+  # of Hetzner
+  ValidatorScoreV1.connection.execute(
+    "update validator_score_v1s set data_center_host = null where data_center_key not like '24940%' and data_center_host is not null;"
+  )
+
+rescue StandardError => e
+  puts "\nERROR:"
+  puts e.message
+  puts e.backtrace
+  puts ''
+end
+
+# puts ''
+# puts record.inspect
+# puts ''
+#
+# puts 'Continent:'
+# puts record.continent.code
+# puts record.continent.geoname_id
+# puts record.continent.name
+# puts ''
+#
+# puts 'Country:'
+# puts record.country.confidence
+# puts record.country.iso_code
+# puts record.country.geoname_id
+# puts record.country.name
+# puts ''
+#
+# puts 'Registered Country:'
+# puts record.registered_country.iso_code
+# puts record.registered_country.geoname_id
+# puts record.registered_country.name
+# puts ''
+#
+# # Represented Country
+# # puts record.represented_country
+#
+# puts 'Traits:'
+# puts record.traits.anonymous?
+# puts record.traits.hosting_provider?
+# puts record.traits.user_type
+# puts record.traits.autonomous_system_number
+# puts record.traits.autonomous_system_organization
+# puts record.traits.domain
+# puts record.traits.isp
+# puts record.traits.organization
+# puts record.traits.ip_address
+# puts record.traits.network
+# puts ''
+#
+# puts 'City:'
+# puts record.city.confidence
+# puts record.city.geoname_id
+# puts record.city.name
+# puts ''
+#
+# puts 'Location:'
+# puts record.location.average_income
+# puts record.location.population_density
+# puts record.location.accuracy_radius
+# puts record.location.latitude
+# puts record.location.longitude
+# puts record.location.metro_code
+# puts record.location.time_zone
+# puts ''
+#
+# puts 'Postal:'
+# puts record.postal.confidence
+# puts record.postal.code
+# puts ''
+# puts ''
+#
+# puts 'Subdivision:'
+# puts record.most_specific_subdivision.confidence
+# puts record.most_specific_subdivision.iso_code
+# puts record.most_specific_subdivision.geoname_id
+# puts record.most_specific_subdivision.name
+# puts ''
+#
+# puts 'MaxMind:'
+# puts record.maxmind.queries_remaining
+# puts ''
+
+# puts 'Updated proxy_ips/max_mind_insights.txt'
