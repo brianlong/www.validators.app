e0e5aa460d283cd614685e1b859b3261cc22dd87
Radosław Stolarski <radoslaw.stolarski@polcode.net>
2022-02-23 15:52:09 +0100

    Add tests for data_center model

--- test/factories/data_centers.rb
+++ test/factories/data_centers.rb
@@ -2,6 +2,44 @@
 
 FactoryBot.define do
   factory :data_center do
-    
+    traits_anonymous { true }
+    traits_hosting_provider { true }
+    traits_user_type { 'hosting' }
+    traits_autonomous_system_number { 00000 }
+    traits_isp { 'Organization' }
+    traits_organization { 'Organization' }
+
+    trait :berlin do
+      continent_code { 'EU' }
+      country_confidence { 99 }
+      country_geoname_id { 276 }
+      country_iso_code { 'DE' }
+      continent_name { 'Europe' }
+      country_name { 'Germany' }
+      data_center_key { '54321-DE-Europe/Berlin' }
+      location_time_zone { 'Europe/CET' }
+      registered_country_iso_code { 'DE' }
+      registered_country_geoname_id { 276 }
+      registered_country_name { 'Germany' }
+      traits_autonomous_system_number { 54321 }
+      traits_autonomous_system_organization { 'Berlin Organisation' }
+    end
+
+    trait :china do
+      continent_code { 'AS' }
+      continent_geoname_id { 6255147 }
+      continent_name { 'Asia' }
+      country_confidence { 99 }
+      country_geoname_id { 1814991 }
+      country_iso_code { 'CN' }
+      country_name { 'China' }
+      data_center_key { '12345-CN-Asia/Shanghai' }
+      location_time_zone { 'Asia/Shanghai' }
+      registered_country_iso_code { 'CN' }
+      registered_country_geoname_id { 1814991 }
+      registered_country_name { 'China' }
+      traits_autonomous_system_number { 12345 }
+      traits_autonomous_system_organization { 'Chinese Organisation' }
+    end
   end
 end
--- test/models/data_center_test.rb
+++ test/models/data_center_test.rb
@@ -3,7 +3,16 @@
 require "test_helper"
 
 class DataCenterTest < ActiveSupport::TestCase
-  # test "the truth" do
-  #   assert true
-  # end
+  def setup
+    @data_center = build(:data_center, :berlin)
+  end
+
+  test "#to_builder returns correct data" do
+    assert_equal "{\"autonomous_system_number\":54321}", @data_center.to_builder.target!
+  end
+
+  test "before_save #assign_data_center_key assigns key correctly" do
+    @data_center.save
+    assert_equal "54321-DE-Europe/CET", @data_center.data_center_key
+  end
 end
