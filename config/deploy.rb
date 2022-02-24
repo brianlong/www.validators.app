4921af895034ee85cd9a90bcafb043861e0eb729
maciejkocylapc <80820353+maciejkocylapc@users.noreply.github.com>
2022-02-24 14:22:52 +0100

    181243773 ping thing daemon (#364)
    * add daemon and service
    
    * remove ping thing worker
    
    * cleanup
    
    * fix SyslogIdentifier in process_ping_thing.service
    
    * add ping thing service to deploy.rb
    
    * require_relative -> require
    
    * remove argv
    
    * add bash -c to service
    
    * try /bin/bash
    
    * comment out ping things in deploy.rb

--- config/deploy.rb
+++ config/deploy.rb
@@ -106,6 +106,7 @@ namespace :daemons do
           execute :systemctl, '--user', :start, :gather_rpc_mainnet
           execute :systemctl, '--user', :start, :gather_rpc_testnet
           execute :systemctl, '--user', :start, :gather_vote_account_details
+          # execute :systemctl, '--user', :start, :process_ping_thing
         end
       end
     end
@@ -121,6 +122,7 @@ namespace :daemons do
           execute :systemctl, '--user', :stop, :gather_rpc_mainnet
           execute :systemctl, '--user', :stop, :gather_rpc_testnet
           execute :systemctl, '--user', :stop, :gather_vote_account_details
+          # execute :systemctl, '--user', :stop, :process_ping_thing
         end
       end
     end
@@ -136,6 +138,7 @@ namespace :daemons do
           execute :systemctl, '--user', :restart, :gather_rpc_mainnet
           execute :systemctl, '--user', :restart, :gather_rpc_testnet
           execute :systemctl, '--user', :restart, :gather_vote_account_details
+          # execute :systemctl, '--user', :restart, :process_ping_thing
         end
       end
     end
