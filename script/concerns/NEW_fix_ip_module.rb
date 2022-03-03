f1c9c5a99556194373ee0be8b84a995aedac20a0
Radosław Stolarski <radoslaw.stolarski@polcode.net>
2022-03-03 10:51:51 +0100

    Remove commented out code

--- script/concerns/NEW_fix_ip_module.rb
+++ script/concerns/NEW_fix_ip_module.rb
@@ -36,25 +36,6 @@ module FixIpModule
     vip.update(data_center_host: data_center_host, is_overridden: true)
   end
 
-  # I think this won't be needed 
-  # def update_ip_with_overrides
-  #   sql1 = "
-  #     UPDATE ips ip
-  #     INNER JOIN ip_overrides ipor
-  #     ON ip.address = ipor.address
-  #     SET
-  #     ip.traits_autonomous_system_number = ipor.traits_autonomous_system_number,
-  #     ip.country_iso_code = ipor.country_iso_code,
-  #     ip.country_name = ipor.country_name,
-  #     ip.city_name = ipor.city_name,
-  #     ip.data_center_key = ipor.data_center_key,
-  #     ip.data_center_host = ipor.data_center_host,
-  #     ip.traits_autonomous_system_organization = ipor.traits_autonomous_system_organization,
-  #     ip.updated_at = NOW();
-  #   ".squish
-  #   Ip.connection.execute(sql1)
-  # end
-
   # I think that data_center_key and host should be removed later 
   # because this information is in the data_center and data_center_host table
   def update_validator_score_with_overrides
