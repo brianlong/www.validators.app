543d2c59c24e9dbda3c5774cbe5a5359ed7f7121
maciejkocylapc <maciej.kocyla@polcode.net>
2022-03-07 12:40:27 +0100

    table for ping-thing

--- app/javascript/packs/ping_thing/index_template.vue
+++ app/javascript/packs/ping_thing/index_template.vue
@@ -19,23 +19,13 @@
       </p>
       <div class="text-danger">This feature is a work-in-progress. Please contact @ValidatorsApp on Twitter with feedback.</div>
     </div>
-    <!--
     <div class="card mb-4">
       <div class="card-content">
-        <h3 class="card-heading mb-4"><%= params[:network].capitalize %> TX Confirmation Time</h3>
-        <%= render 'scatter_chart',
-                  chart_name: "scatter_chart_response_times",
-                  vector: @ping_things_array_for_chart,
-                  line_color: SPARK_LINE_2,
-                  fill_color: SPARK_LINE_2_TRANSPARENT,
-                  y_axis_desc: 'Response Time (ms)',
-                  x_axis_desc: "Last #{@ping_things_count} Observations"
-        %>
+        <h3 class="card-heading mb-4">{{ network }} TX Confirmation Time</h3>
+        <scatter-chart :vector="ping_things" fill_color="rgba(221, 154, 229, 0.4)" line_color="rgb(221, 154, 229)"/>
       </div>
     </div>
-    <div class="">
 
-    </div>
     <div class="card">
       <div class="table-responsive-lg">
         <table class='table mb-0'>
@@ -55,32 +45,84 @@
           </thead>
 
           <tbody>
-          <% @ping_things.each.with_index do |pt, index| %>
-            <tr>
+            <tr v-for="(pt, index) in ping_things" :key="pt.id">
               <td class="align-middle">
-                <%= success_icon(pt.success).html_safe %>
-                <strong class="text-success h6"><%= number_with_delimiter(pt.response_time) %></strong>&nbsp;ms
+                <span v-html="success_icon(pt.success)"></span>
+                <strong class="text-success h6">{{ pt.response_time.toLocaleString() }}</strong>&nbsp;ms
               </td>
               <td class="align-middle small">
-                <%= pt.reported_at %><br />
-                <span class="word-break"><%= link_to shorten_key(pt.signature), link_from_signature(pt.signature), target: "_blank", class: "small"%></span>
-              </td>
-              <td class="align-middle small">
-                <% if pt.application %>
-                  <%= pt.application %><br />
-                <% end %>
-                <span class="text-muted">
-                  <%= transaction_type_icon(pt.transaction_type).html_safe %><%= pt.transaction_type %>
-                  <% if pt.commitment_level %>(<%= pt.commitment_level %>)<% end %>
+                {{ pt.reported_at }}<br />
+                <span class="word-break">
+                  <a :href="link_from_signature(pt.signature)" target="_blank" class="small">
+                    {{ pt.signature.substring(0,6) + "..." + pt.signature.substring(pt.signature.length - 4, pt.signature.length) }}
+                  </a>
                 </span>
               </td>
-              <td class="align-middle text-muted"><%= pt.user.username %></td>
+              <td class="align-middle small">
+                <div v-if="pt.application">
+                  {{ pt.application }}<br />
+                </div>
+                <span class="text-muted">
+                  <span v-html="transaction_type_icon(pt.transaction_type)"></span>
+                  {{ pt.transaction_type }}
+                  <span v-if="pt.commitment_level">
+                    ({{ pt.commitment_level }})
+                  </span>
+                </span>
+              </td>
+              <td class="align-middle text-muted">{{ pt.username }}</td>
             </tr>
-          <% end %>
           </tbody>
         </table>
       </div>
     </div>
-    -->
   </div>
 </template>
+
+<script>
+  import axios from 'axios'
+  import loadingImage from 'loading.gif'
+  import scatterChart from './scatter_chart'
+
+  axios.defaults.headers.get["Authorization"] = window.api_authorization
+
+  export default {
+    props: ['network'],
+    data () {
+      return {
+        ping_things: []
+      }
+    },
+    created () {
+      var ctx = this
+      axios.get("/api/v1/ping-thing/testnet")
+           .then(function(response){
+             ctx.ping_things = response.data
+           })
+    },
+    methods: {
+      success_icon(success) {
+        if(success){
+          return '<i class="fas fa-check-circle text-success mr-1"></i>'
+        } else {
+          return '<i class="fas fa-times-circle text-danger mr-1"></i>'
+        }
+      },
+      link_from_signature(signature){
+        return "https://solanabeach.io/transaction/" + signature
+      },
+      transaction_type_icon(tx){
+        switch(tx){
+          case 'transfer':
+            return '<i class="fas fa-exchange-alt text-success mr-1"></i>'
+          default:
+            return '<i class="fas fa-random text-success mr-1"></i>'
+        }
+      }
+    },
+    components: {
+      "scatter-chart": scatterChart
+    }
+  }
+
+</script>
