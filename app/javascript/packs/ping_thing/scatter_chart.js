67fab5d5a838186eea65b4d859ab46a753235dd9
maciejkocylapc <maciej.kocyla@polcode.net>
2022-03-07 14:01:59 +0100

    cleanup

--- app/javascript/packs/ping_thing/scatter_chart.js
+++ app/javascript/packs/ping_thing/scatter_chart.js
@@ -24,78 +24,7 @@ export default {
     }
   },
   mounted: function(){
-    var ctx = document.getElementById("ping-thing-scatter-chart").getContext('2d');
-    console.log("chart create ...")
-    this.chart = new Chart(ctx, {
-        type: 'scatter',
-
-        // The data for our dataset
-        data: {
-            datasets: [
-                {
-                    data: this.vector.map( (vector_element, index) => ({ x: index, y: vector_element['response_time'] }) ),
-                    backgroundColor: this.fill_color,
-                    borderColor: this.line_color
-                }
-            ],
-        },
-
-        // Configuration options
-        options: {
-            legend: {
-                display: false,
-                labels: {
-                    fontColor: this.dark_grey
-                },
-            },
-            scales: {
-                xAxes: [{
-                    display: true,
-                    ticks: { display: false },
-                    gridLines: { display: false },
-                    scaleLabel: {
-                        display: true,
-                        labelString: "Last " + this.vector.length + " Observations",
-                        fontColor: this.dark_grey
-                    }
-                }],
-                yAxes: [{
-                    display: true,
-                    ticks: {
-                        //min: 0,
-                        padding: 10,
-                        callback: function(value, index, values) {
-                            return value.toLocaleString('en-US')
-                        }
-                    },
-                    gridLines: {
-                        display: true,
-                        zeroLineColor: this.chart_line,
-                        color: this.chart_line
-                    },
-                    scaleLabel: {
-                        display: true,
-                        labelString: 'Response Time (ms)',
-                        fontColor: this.dark_grey,
-                        padding: 5
-                    }
-                }]
-            },
-            elements: {
-                point: {
-                    radius: 3
-                }
-            },
-            tooltips: {
-                displayColors: false,
-                callbacks: {
-                    label: function(tooltipItem, data) {
-                        return tooltipItem.yLabel.toLocaleString('en-US').concat(' ms');
-                    },
-                },
-            },
-        }
-    });
+    this.update_chart()
   },
   watch: {
       'vector': {
@@ -106,8 +35,9 @@ export default {
   },
   methods: {
     update_chart: function(){
-        this.chart.destroy()
-
+        if(this.chart){
+            this.chart.destroy()
+        }
         var ctx = document.getElementById("ping-thing-scatter-chart").getContext('2d');
         this.chart = new Chart(ctx, {
             type: 'scatter',
