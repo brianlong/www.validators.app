1eff7b2f1c95990dd41dfa689b693cadc5a6c188
maciejkocylapc <maciej.kocyla@polcode.net>
2022-03-15 09:12:03 +0100

    fix quotes

--- app/controllers/concerns/validators_controller_helper.rb
+++ app/controllers/concerns/validators_controller_helper.rb
@@ -20,14 +20,14 @@ module ValidatorsControllerHelper
     # Data from the skipped_slots_report
     unless @skipped_slots_report.nil?
       this_report = @skipped_slots_report.payload.select do |ssr|
-        ssr['account'] == validator.account
+        ssr["account"] == validator.account
       end
 
       unless this_report.first.nil?
         hash.merge!({
-          'skipped_slots' => this_report.first['skipped_slots'],
-          'skipped_slot_percent' => this_report.first['skipped_slot_percent'],
-          'ping_time' => nil # this_report.first['ping_time']
+          "skipped_slots" => this_report.first["skipped_slots"],
+          "skipped_slot_percent" => this_report.first["skipped_slot_percent"],
+          "ping_time" => nil # this_report.first["ping_time"]
         })
       end
     end
@@ -38,38 +38,38 @@ module ValidatorsControllerHelper
 
   def validator_fields
     [
-      'account',
-      'created_at',
-      'details',
-      'id',
-      'keybase_id',
-      'name',
-      'network',
-      'updated_at',
-      'www_url',
-      'avatar_url',
-      'admin_warning'
+      "account",
+      "created_at",
+      "details",
+      "id",
+      "keybase_id",
+      "name",
+      "network",
+      "updated_at",
+      "www_url",
+      "avatar_url",
+      "admin_warning"
     ].map { |e| "validators.#{e}" }
   end
 
   def validator_score_v1_fields
     [
-      'active_stake',
-      'commission',
-      'delinquent',
-      'data_center_concentration_score',
-      'data_center_key',
-      'data_center_host',
-      'published_information_score',
-      'root_distance_score',
-      'security_report_score',
-      'skipped_slot_score',
-      'software_version',
-      'software_version_score',
-      'stake_concentration_score',
-      'total_score',
-      'validator_id',
-      'vote_distance_score'
+      "active_stake",
+      "commission",
+      "delinquent",
+      "data_center_concentration_score",
+      "data_center_key",
+      "data_center_host",
+      "published_information_score",
+      "root_distance_score",
+      "security_report_score",
+      "skipped_slot_score",
+      "software_version",
+      "software_version_score",
+      "stake_concentration_score",
+      "total_score",
+      "validator_id",
+      "vote_distance_score"
     ].map { |e| "validator_score_v1s.#{e}" }
   end
 end
