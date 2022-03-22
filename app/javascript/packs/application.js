edd2ccb44df68dfa424cdce096feafe72439bfc2
Radosław Stolarski <radoslaw.stolarski@polcode.net>
2022-03-22 19:36:10 +0100

    181616433_fix_datetime_labels (#390)
    * Add momentjs
    
    * Use momentjs to format date in ping things table

--- app/javascript/packs/application.js
+++ app/javascript/packs/application.js
@@ -7,6 +7,8 @@ require("@rails/ujs").start()
 require("turbolinks").start()
 require("@rails/activestorage").start()
 require("channels")
+var moment = require('moment');
+
 import '../src/sol_prices_charts'
 // Uncomment to copy all static images under ../images to the output folder and reference
 // them with the image_pack_tag helper in views (e.g <%= image_pack_tag 'rails.png' %>)
