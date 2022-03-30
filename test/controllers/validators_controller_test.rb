82a63c90d2bce0dd330ad546e803920d5e7600db
Klaudia Biala <klaudia.biala@polcode.net>
2022-03-30 14:39:49 +0200

    Change path helper

--- test/controllers/validators_controller_test.rb
+++ test/controllers/validators_controller_test.rb
@@ -41,14 +41,7 @@ class ValidatorsControllerTest < ActionDispatch::IntegrationTest
   end
 
   test "GET validator with account param returns validator details" do
-    validator = create(
-      :validator,
-      :with_score,
-      :with_score_v2,
-      :with_data_center_through_validator_ip,
-      network: "testnet"
-    )
-    get validator_url(network: 'testnet', account: validator.account)
+    get validator_path(network: "testnet", account: @validator.account)
     assert_response :success
   end
 
