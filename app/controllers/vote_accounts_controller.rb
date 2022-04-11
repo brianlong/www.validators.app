6ef34c08d721f3816529c7bd7f65e8f3b2ab8eae
Radosław Stolarski <radoslaw.stolarski@polcode.net>
2022-04-11 13:13:11 +0200

    Use last vote_account

--- app/controllers/vote_accounts_controller.rb
+++ app/controllers/vote_accounts_controller.rb
@@ -11,7 +11,9 @@ class VoteAccountsController < ApplicationController
   def set_vote_account
     @vote_account = VoteAccount.where(
       network: params[:network],
-      account: params[:account]).first
+      account: params[:account],
+      validator_id: params[:validator_id]
+    ).last
     render file: "#{Rails.root}/public/404.html" , status: 404 \
       if @vote_account.nil?
   end
--- test/controllers/vote_accounts_controller_test.rb
+++ test/controllers/vote_accounts_controller_test.rb
@@ -4,10 +4,15 @@ require 'test_helper'
 
 # VoteAccountsControllerTest
 class VoteAccountsControllerTest < ActionDispatch::IntegrationTest
+  setup do
+    @vote_account = vote_accounts(:one)
+  end
+
   test 'should get show' do
     get vote_account_url(
       network: 'testnet',
-      account: vote_accounts(:one).account
+      account: @vote_account.account,
+      validator_id: @vote_account.validator_id
     )
     assert_response :success
   end
