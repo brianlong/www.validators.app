ae7bacf821d8a494f6df4766590156f47355e574
maciejkocylapc <maciej.kocyla@polcode.net>
2022-04-14 15:13:51 +0200

    fix path in script

--- script/one_time_scripts/back_fill_ping_thing_stats.rb
+++ script/one_time_scripts/back_fill_ping_thing_stats.rb
@@ -1,6 +1,6 @@
 # frozen_string_literal: true
 
-require File.expand_path('../config/environment', __dir__)
+require File.expand_path('../../config/environment', __dir__)
 
 begin_minutes_ago = 1440 # 24 hours
 # begin_minutes_ago = 60 # 1 hour
