36f627aa64a84e2fc7647fa28956a882beac8905
Klaudia Biała <klaudia.biala@polcode.net>
2022-05-02 14:16:29 +0200

    Add more tests

--- test/models/validator_score_v2_test.rb
+++ test/models/validator_score_v2_test.rb
@@ -53,6 +53,9 @@ class ValidatorScoreV2Test < ActiveSupport::TestCase
 
   test '#calculate_total_score correctly calculates score including proper values from v1 and v2' do
     assert_equal 10, @validator_score_v2.calculate_total_score
+    assert_equal 2, @validator_score_v2.root_distance_score
+    assert_equal 2, @validator_score_v2.vote_distance_score
+    assert_equal 2, @validator_score_v2.skipped_slot_score
 
     @validator_score_v2.update_columns(
       root_distance_score: 1,
@@ -61,6 +64,33 @@ class ValidatorScoreV2Test < ActiveSupport::TestCase
     )
 
     assert_equal 7, @validator_score_v2.calculate_total_score
+  end
+
+  test '#calculate_total_score correctly calculates total score based on cosensus_mods values in v1' do
+    validator_with_consensus_mods = create(:validator, :with_consensus_mods_true)
+    validator_score_v1 = create(
+      :validator_score_v1,
+      validator: validator_with_consensus_mods,
+      software_version: @batch.software_version,
+      published_information_score: 1,
+      security_report_score: 1,
+      software_version_score: 2,
+      stake_concentration_score: 0,
+      data_center_concentration_score: 0,
+      authorized_withdrawer_score: 0,
+      delinquent: false
+    )
+    validator_score_v2 = create(
+      :validator_score_v2,
+      validator: validator_with_consensus_mods,
+      root_distance_score: 2,
+      vote_distance_score: 2,
+      skipped_slot_score: 2
+    )
+    assert_equal -2, validator_score_v1.consensus_mods_score # reduced by 2
+    assert_equal 0, validator_score_v1.security_report_score # reduced by 1
+    assert_equal 4, validator_score_v1.calculate_total_score # reduced by (2+1)
+    assert_equal 7, validator_score_v2.calculate_total_score # reduced by (2+1)
   end
 
   test '#delinquent? returns corrent boolean value' do
