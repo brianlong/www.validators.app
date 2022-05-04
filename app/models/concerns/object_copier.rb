93bbfaa767e6e81d9fca03b3499b3585f4bbae4a
maciejkocylapc <maciej.kocyla@polcode.net>
2022-05-04 13:47:57 +0200

    add option in credentials

--- app/models/concerns/object_copier.rb
+++ app/models/concerns/object_copier.rb
@@ -7,9 +7,13 @@ module ObjectCopier
     after_create :copy_to_stage_db, unless: :skip_copy
 
     def copy_to_stage_db
-      if Rails.env.production?
+      if should_copy_records?
         ObjectCopyService.new(original: self).call
       end
+    end
+
+    def should_copy_records?
+      Rails.env.production? && [true, "true"].include?(Rails.application.credentials.copy_records_to_stage)
     end
   end
 end
