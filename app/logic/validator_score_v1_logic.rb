3b06cb095044cc3380a632b6b5d15a5227ec3919
maciejkocylapc <maciej.kocyla@polcode.net>
2022-05-04 11:31:18 +0200

    add working logger

--- app/logic/validator_score_v1_logic.rb
+++ app/logic/validator_score_v1_logic.rb
@@ -4,14 +4,16 @@
 module ValidatorScoreV1Logic
   include PipelineLogic
 
-  file_path = File.join(Rails.root, "log", "validator_score_logic.log")
-  @logger = Logger.new(file_path)
+  def logger
+    file_path = File.join(Rails.root, "log", "validator_score_logic.log")
+    @logger ||= Logger.new(file_path)
+  end
 
   # Payload starts with :network & :batch_uuid
   def set_this_batch
     lambda do |p|
       start_time = Time.now
-      @logger.info "start  set_this_batch"
+      logger.debug "---------- start set_this_batch -----------"
       return p unless p.code == 200
 
       this_batch = Batch.where(
@@ -23,7 +25,7 @@ module ValidatorScoreV1Logic
         raise "No batch: #{p.payload[:network]}, #{p.payload[:batch_uuid]}"
       end
 
-      @logger.info (Time.now - start_time).to_i
+      logger.debug (Time.now - start_time).to_i
       Pipeline.new(200, p.payload.merge(this_batch: this_batch))
     rescue StandardError => e
       Pipeline.new(500, p.payload, 'Error from set_this_batch', e)
@@ -35,7 +37,7 @@ module ValidatorScoreV1Logic
     lambda do |p|
       return p unless p.code == 200
       start_time = Time.now
-      @logger.info "start  validators_get"
+      logger.debug "start  validators_get"
 
       validators = Validator.where(network: p.payload[:network])
                             .active
@@ -52,7 +54,7 @@ module ValidatorScoreV1Logic
         Appsignal.send_error(e)
       end
 
-      @logger.info (Time.now - start_time).to_i
+      logger.debug (Time.now - start_time).to_i
       Pipeline.new(200, p.payload.merge(validators: validators))
     rescue StandardError => e
       Pipeline.new(500, p.payload, 'Error from validators_get', e)
@@ -64,7 +66,7 @@ module ValidatorScoreV1Logic
     lambda do |p|
       return p unless p.code == 200
       start_time = Time.now
-      @logger.info "start  block_vote_history_get"
+      logger.debug "start  block_vote_history_get"
 
       # Grab the highest root block & vote for this batch so we can calculate
       # the distances
@@ -144,7 +146,7 @@ module ValidatorScoreV1Logic
         skipped_vote_all_median: skipped_vote_all_median,
       )
 
-      @logger.info (Time.now - start_time).to_i
+      logger.debug (Time.now - start_time).to_i
       Pipeline.new(200, p.payload.merge(total_active_stake: total_active_stake))
     rescue StandardError => e
       Pipeline.new(500, p.payload, 'Error from block_vote_history_get', e)
@@ -155,7 +157,7 @@ module ValidatorScoreV1Logic
     lambda do |p|
       return p unless p.code == 200
       start_time = Time.now
-      @logger.info "start  assign_block_and_vote_scores"
+      logger.debug "start  assign_block_and_vote_scores"
 
       # get the average & median from the cluster history
       root_distance_all = []
@@ -232,7 +234,7 @@ module ValidatorScoreV1Logic
         Appsignal.send_error(e)
       end
 
-      @logger.info (Time.now - start_time).to_i
+      logger.debug (Time.now - start_time).to_i
       Pipeline.new(200, p.payload.merge(
                           root_distance_all: root_distance_all,
                           vote_distance_all: vote_distance_all,
@@ -252,7 +254,7 @@ module ValidatorScoreV1Logic
       return p unless p.code == 200
 
       start_time = Time.now
-      @logger.info "start  assign_block_and_vote_scores"
+      logger.debug "start  assign_block_and_vote_scores"
 
       vbh_stats = Stats::ValidatorBlockHistory.new(p.payload[:network], p.payload[:batch_uuid])
       avg_skipped_slot_pct_all = vbh_stats.average_skipped_slot_percent
@@ -280,7 +282,7 @@ module ValidatorScoreV1Logic
         Appsignal.send_error(e)
       end
 
-      @logger.info (Time.now - start_time).to_i
+      logger.debug (Time.now - start_time).to_i
       Pipeline.new(
         200,
         p.payload.merge(
@@ -298,7 +300,7 @@ module ValidatorScoreV1Logic
     lambda do |p|
       return p unless p.code == 200
       start_time = Time.now
-      @logger.info "start  assign_block_history_score"
+      logger.debug "start  assign_block_history_score"
       p.payload[:validators].each do |validator|
         skipped_slot_percent = \
           validator&.validator_score_v1&.skipped_slot_moving_average_history&.last
@@ -318,7 +320,7 @@ module ValidatorScoreV1Logic
         Appsignal.send_error(e)
       end
 
-      @logger.info (Time.now - start_time).to_i
+      logger.debug (Time.now - start_time).to_i
       Pipeline.new(200, p.payload)
     rescue StandardError => e
       Pipeline.new(500, p.payload, 'Error from assign_block_history_score', e)
@@ -329,7 +331,7 @@ module ValidatorScoreV1Logic
     lambda do |p|
       return p unless p.code == 200
       start_time = Time.now
-      @logger.info "start  assign_software_version_score"
+      logger.debug "start  assign_software_version_score"
 
       # call #to_a at the end to guarantee the query is run now, instead of inside of the loop
       last_validator_histories = ValidatorHistory.where(
@@ -374,7 +376,7 @@ module ValidatorScoreV1Logic
         validator.validator_score_v1.assign_software_version_score(current_software_version)
       end
 
-      @logger.info (Time.now - start_time).to_i
+      logger.debug (Time.now - start_time).to_i
       Pipeline.new(200, p.payload)
     rescue StandardError => e
       Pipeline.new(500, p.payload, 'Error from assign_software_version_score', e)
@@ -385,7 +387,7 @@ module ValidatorScoreV1Logic
     lambda do |p|
       return p unless p.code == 200
       start_time = Time.now
-      @logger.info "start  get_ping_times"
+      logger.debug "start  get_ping_times"
 
       p.payload[:validators].each do |validator|
         validator.validator_score_v1.ping_time_avg = \
@@ -394,7 +396,7 @@ module ValidatorScoreV1Logic
         Appsignal.send_error(e)
       end
 
-      @logger.info (Time.now - start_time).to_i
+      logger.debug (Time.now - start_time).to_i
       Pipeline.new(200, p.payload)
     rescue StandardError => e
       Pipeline.new(500, p.payload, 'Error from get_ping_times', e)
@@ -405,7 +407,7 @@ module ValidatorScoreV1Logic
     lambda do |p|
       return p unless p.code == 200
       start_time = Time.now
-      @logger.info "start  save_validators"
+      logger.debug "start  save_validators"
       ActiveRecord::Base.transaction do
         p.payload[:validators].each do |validator|
           validator.save
@@ -414,7 +416,7 @@ module ValidatorScoreV1Logic
           Appsignal.send_error(e)
         end
       end
-      @logger.info (Time.now - start_time).to_i
+      logger.debug (Time.now - start_time).to_i
 
       Pipeline.new(200, p.payload)
     rescue StandardError => e
