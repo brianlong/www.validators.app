d57327485163c8ff3578f91e46af3005d5c18907
Brian Long <brian.long@fmadata.com>
2022-05-04 18:37:32 -0600

    Update copy on the page

--- app/javascript/packs/ping_thing/ping_thing_header.vue
+++ app/javascript/packs/ping_thing/ping_thing_header.vue
@@ -2,13 +2,10 @@
   <div class="page-header">
     <h1>Ping Thing</h1>
     <p>
-      We use this Ping Thing to show recent transaction confirmation times on the Solana blockchain. The <a href='https://www.blocklogic.net' target='_blank'>Block Logic Validator</a> is sending simple transfer transactions every 15 seconds and the confirmation times appear below. You can use these pings as a gauge of cluster performance.
+      We use this Ping Thing to show recent transaction confirmation times on the Solana blockchain. The <a href='https://www.blocklogic.net' target='_blank'>Block Logic Validator</a> plus several other validators are sending simple transfer transactions every 15 seconds and the confirmation times appear below. You can use these pings as a gauge of cluster performance.
     </p>
     <p>
-      This is a very simple first iteration with many improvements on the way. Upcoming features include: REST API to download data, interactive chart with custom timeframes, statistics for a given period of time, and more.
-    </p>
-    <p>
-      App developers, you can also upload your own TX confirmation times using our <a href="/api-documentation">REST API</a>. Any Solana application or transaction type is allowed. Use this tool to show recent confirmation times to your users.
+      All data is available for download from our <a href="/api-documentation">REST API</a>. App developers, you can also upload your own TX confirmation times. Any Solana application or transaction type is allowed. Use this tool to show recent confirmation times to your users.
     </p>
     <p>
       I would like to get feedback from Solana developers on how we can make this tool more useful for your applications. Please contact me at @ValidatorsApp on Twitter. -- @brianlong
@@ -16,6 +13,5 @@
     <p>
       NOTES: We use a 60-second timeout threshold for the pings. On the chart and table below, a value of 60,000 milliseconds means that the operation failed or took longer than 60 seconds to complete.
     </p>
-    <div class="text-danger">This feature is a work-in-progress. Please contact @ValidatorsApp on Twitter with feedback.</div>
   </div>
 </template>
