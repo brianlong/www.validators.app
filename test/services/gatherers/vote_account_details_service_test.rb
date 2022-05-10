afe76cf45883b82729c37b4af01fa2185cbf7b68
maciejkocylapc <maciej.kocyla@polcode.net>
2022-05-10 16:35:50 +0200

    fix test

--- test/services/gatherers/vote_account_details_service_test.rb
+++ test/services/gatherers/vote_account_details_service_test.rb
@@ -16,38 +16,26 @@ class VoteAccountDetailsServiceTest < ActiveSupport::TestCase
 
     @val = create(:validator, network: @network)
     @score = create(:validator_score_v1, network: @network, validator: @val)
-  end
 
-  teardown do
-    VoteAccount.delete_all
-  end
-
-  test "VoteAccountDetailsService updates identity and withdrawer" do
-    va1 = create(
+    @va1 = create(
       :vote_account,
       validator: @val,
       network: @network,
       account: "acc1"
     )
+  end
+
+  test "VoteAccountDetailsService updates identity and withdrawer" do
     SolanaCliService.stub(:request, @json_response, ["vote-account", @testnet_url]) do
       Gatherers::VoteAccountDetailsService.new(network: @network, config_urls: [@testnet_url]).call
 
-      assert_equal va1.reload.validator_identity, @validator_identity
-      assert_equal va1.reload.authorized_withdrawer, @authorized_withdrawer
+      assert_equal @va1.reload.validator_identity, @validator_identity
+      assert_equal @va1.reload.authorized_withdrawer, @authorized_withdrawer
       assert_equal 0, @score.reload.authorized_withdrawer_score
     end
   end
 
   test "VoteAccountDetailsService sets correct score" do
-    va1 = create(
-      :vote_account,
-      validator: @val,
-      network: @network,
-      account: "acc1",
-      authorized_withdrawer: @authorized_withdrawer,
-      validator_identity: @validatorIdentity
-    )
-
     @json_response = {
       validatorIdentity: "equal_token",
       authorizedWithdrawer: "equal_token"
