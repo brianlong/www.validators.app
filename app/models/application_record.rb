ce7181b2e756266275bb6ac400183df5007a5232
maciejkocylapc <80820353+maciejkocylapc@users.noreply.github.com>
2022-05-17 12:01:23 +0200

    181897977 db copy feature (#431)
    * adding associations WIP
    
    * copy service
    
    * copy only for production
    
    * fix solana link
    
    * add option in credentials
    
    * remove puts
    
    * Delete staging_schema.rb
    
    * add copy info to readme
    
    Co-authored-by: Klaudia Biala <klaudia.biala@polcode.net>

--- app/models/application_record.rb
+++ app/models/application_record.rb
@@ -1,3 +1,6 @@
 class ApplicationRecord < ActiveRecord::Base
   self.abstract_class = true
+  if Rails.env.production?
+    connects_to database: { writing: :primary,  staging: :staging }
+  end
 end
