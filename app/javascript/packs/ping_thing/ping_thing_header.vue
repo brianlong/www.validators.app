92a273e48d179b52749db7b5f0cb694b23f1f51d
Klaudia Biala <klaudia.biala@polcode.net>
2022-05-18 12:41:17 +0200

    182209436 validators logos square (#442)
    * Change validators images
    
    * Update default avatar
    
    * Remove unused views
    
    * Add missing classes and change buttons size
    
    * Reduce validator name size on mobiles
    
    * Fix formatting
    
    * Change triangles to buttons on details page
    
    * Address CR comments
    
    * Use bootstrap-like names for css variables
    
    * Add space on mobile

--- app/javascript/packs/ping_thing/ping_thing_header.vue
+++ app/javascript/packs/ping_thing/ping_thing_header.vue
@@ -2,16 +2,24 @@
   <div class="page-header">
     <h1>Ping Thing</h1>
     <p>
-      We use this Ping Thing to show recent transaction confirmation times on the Solana blockchain. The <a href='https://www.blocklogic.net' target='_blank'>Block Logic Validator</a> plus several other validators are sending simple transfer transactions every 15 seconds and the confirmation times appear below. You can use these pings as a gauge of cluster performance.
+      We use this Ping Thing to show recent transaction confirmation times on the Solana blockchain.
+      The <a href='https://www.blocklogic.net' target='_blank'>Block Logic Validator</a> plus several other
+      validators are sending simple transfer transactions every 15 seconds and the confirmation times appear
+      below. You can use these pings as a gauge of cluster performance.
     </p>
     <p>
-      All data is available for download from our <a href="/api-documentation">REST API</a>. App developers, you can also upload your own TX confirmation times. Any Solana application or transaction type is allowed. Use this tool to show recent confirmation times to your users.
+      All data is available for download from our <a href="/api-documentation">REST API</a>. App developers, you can
+      also upload your own TX confirmation times. Any Solana application or transaction type is allowed. Use this
+      tool to show recent confirmation times to your users.
     </p>
     <p>
-      I would like to get feedback from Solana developers on how we can make this tool more useful for your applications. Please contact me at @ValidatorsApp on Twitter. -- @brianlong
+      I would like to get feedback from Solana developers on how we can make this tool more useful for your
+      applications. Please contact me at <a href="https://twitter.com/ValidatorsApp" target="_blank">@ValidatorsApp</a>
+      on Twitter. -- @brianlong
     </p>
     <p>
-      NOTES: We use a 60-second timeout threshold for the pings. On the chart and table below, a value of 60,000 milliseconds means that the operation failed or took longer than 60 seconds to complete.
+      NOTES: We use a 60-second timeout threshold for the pings. On the chart and table below, a value of 60,000
+      milliseconds means that the operation failed or took longer than 60 seconds to complete.
     </p>
   </div>
 </template>
