77e98aec7fe9675ec0fee52f8ef00ddbd305349b
Klaudia Biala <klaudia.biala@polcode.net>
2022-05-25 15:09:35 +0200

    Fix schema version

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2022_05_25_083707) do
+ActiveRecord::Schema.define(version: 2022_05_06_111500) do
 
   create_table "active_storage_attachments", charset: "utf8mb4", collation: "utf8mb4_0900_ai_ci", force: :cascade do |t|
     t.string "name", null: false
