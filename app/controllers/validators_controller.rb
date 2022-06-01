45dbe97909376eb6807e8d748c10df7988d62f52
maciejkocylapc <maciej.kocyla@polcode.net>
2022-06-01 12:35:22 +0200

    info page instead of flash

--- app/controllers/validators_controller.rb
+++ app/controllers/validators_controller.rb
@@ -8,15 +8,7 @@ class ValidatorsController < ApplicationController
   # GET /validators.json
   def index
     @per = 25
-    user_id = if params[:watchlist] && current_user.watched_validators.exists?
-                current_user.id
-              elsif params[:watchlist]
-                flash[:notice] = t('flash.empty_watchlist')
-                current_user.id
-              else
-                nil
-              end
-
+    user_id = params[:watchlist] ? current_user&.id : nil
     @validators = ValidatorQuery.new(user_id: user_id).call(
       network: params[:network],
       sort_order: params[:order],
