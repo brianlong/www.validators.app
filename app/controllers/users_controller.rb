577c4d5bd2a8ada04f216655b97e9737d6234b2b
maciejkocylapc <maciej.kocyla@polcode.net>
2022-06-01 11:41:14 +0200

    fix indentation

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -3,10 +3,10 @@
 class UsersController < ApplicationController
   def current_user_info
     response = if current_user
-      { api_token: current_user.api_token }
-    else
-      { error: "user not signed in" }
-    end
+                { api_token: current_user.api_token }
+              else
+                { error: "user not signed in" }
+              end
 
     render json: response.to_json
   end
