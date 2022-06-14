8674e2a073be1c00ff1ae08b68d10643bb9592f1
Klaudia Biala <klaudia.biala@polcode.net>
2022-06-14 13:46:58 +0200

    Use double quotes

--- test/controllers/you_controller_test.rb
+++ test/controllers/you_controller_test.rb
@@ -9,7 +9,7 @@ class YouControllerTest < ActionDispatch::IntegrationTest
     @user = create(:user, :confirmed)
   end
 
-  test 'index returns success for signed in user' do
+  test "index returns success for signed in user" do
     sign_in @user
 
     get user_root_url
@@ -17,21 +17,21 @@ class YouControllerTest < ActionDispatch::IntegrationTest
     assert_response :success
   end
 
-  test 'index redirects if no signed in user' do
+  test "index redirects if no signed in user" do
     get user_root_url
 
     assert_redirected_to new_user_session_path
     assert_match "You need to sign in or sign up before continuing.", flash[:alert]
   end
 
-  test 'regenerate_token redirects if no signed in user' do
+  test "regenerate_token redirects if no signed in user" do
     get user_root_url
 
     assert_redirected_to new_user_session_path
     assert_match "You need to sign in or sign up before continuing.", flash[:alert]
   end
 
-  test 'regenerate_token returns success for signed in user' do
+  test "regenerate_token returns success for signed in user" do
     sign_in @user
 
     get user_root_url
