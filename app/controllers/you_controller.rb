a5914dd47a2e7fbc9bb39037247d881212deb9f3
Klaudia Biala <klaudia.biala@polcode.net>
2022-06-14 13:43:12 +0200

    Remove duplicated code

--- app/controllers/you_controller.rb
+++ app/controllers/you_controller.rb
@@ -1,7 +1,7 @@
 # frozen_string_literal: true
 
 class YouController < ApplicationController
-  before_action :authenticate_user!, only: [:index]
+  before_action :authenticate_user!, only: [:index, :regenerate_token]
 
   def index
     @user = current_user
@@ -16,13 +16,8 @@ class YouController < ApplicationController
 
   # POST you/regenerate_token
   def regenerate_token
-    if current_user
-      current_user.regenerate_api_token
-      redirect_to user_root_url
-    else
-      flash[:warning] = t('you.only_users_allowed')
-      redirect_to :root
-    end
+    current_user.regenerate_api_token
+    redirect_to user_root_url
   end
 
   private
