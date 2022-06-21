322e2090c0bb08269435bd3b84f323265bb5b2eb
maciejkocylapc <maciej.kocyla@polcode.net>
2022-06-21 11:07:09 +0200

    WIP

--- app/javascript/packs/ping_thing/index_template.vue
+++ app/javascript/packs/ping_thing/index_template.vue
@@ -4,6 +4,26 @@
 
     <div class="card mb-4">
       <div class="card-content">
+        <h2 class="h3 card-heading">{{ network[0].toUpperCase() + network.substring(1) }} stats for last 5 minutes</h2>
+        <div class="row pl-lg-4 pl-xl-5">
+          <div class="col-md-4 pl-lg-4 pl-xl-5">
+            <span class="text-muted">Number of entries:&nbsp;</span>
+            <strong class="text-success">{{ count_last_5_minutes }}</strong>
+          </div>
+          <div class="col-md-4 pl-lg-4 pl-xl-5">
+            <span class="text-muted">Transactions P90:&nbsp;</span>
+            <strong class="text-success">{{ p90 }}</strong>
+          </div>
+          <div class="col-md-4 pl-lg-4 pl-xl-5">
+            <span class="text-muted">Transactions average:&nbsp;</span>
+            <strong class="text-success">{{ avg_last_5_minutes }}</strong>
+          </div>
+        </div>
+      </div>
+    </div>
+
+    <div class="card mb-4">
+      <div class="card-content">
         <h2 class="h3 card-heading">{{ network[0].toUpperCase() + network.substring(1) }} TX Confirmation Time Stats</h2>
         <stats-chart :network="network"/>
       </div>
@@ -53,15 +73,21 @@
         ping_things: [],
         page: 1,
         total_count: 0,
-        api_url: api_url
+        api_url: api_url,
+        p90: 0,
+        count_last_5_minutes: 0,
+        avg_last_5_minutes: 0
       }
     },
     created () {
       var ctx = this
-      axios.get(ctx.api_url, { params: { with_total_count: true } })
+      axios.get(ctx.api_url, { params: { with_stats: true } })
            .then(function(response){
              ctx.ping_things = response.data.ping_things;
              ctx.total_count = response.data.total_count;
+             ctx.p90 = response.data.p90;
+             ctx.count_last_5_minutes = response.data.count_last_5_minutes;
+             ctx.avg_last_5_minutes = response.data.avg_last_5_minutes
            })
     },
     watch: {
@@ -72,10 +98,10 @@
     methods: {
       paginate: function(){
         var ctx = this
-        axios.get(ctx.api_url, { params: { with_total_count: true, page: ctx.page } })
-             .then(response => (
+        axios.get(ctx.api_url, { params: { with_stats: true, page: ctx.page } })
+             .then(function(response) {
                ctx.ping_things = response.data.ping_things
-             ))
+             })
       },
       reset_filters: function() {
         // TODO
