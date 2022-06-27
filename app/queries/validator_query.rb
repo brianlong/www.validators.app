d4c6a71c7ee1d9b0a9edc4c36cbe7930279fb507
maciejkocylapc <maciej.kocyla@polcode.net>
2022-06-27 23:25:05 +0200

    restore order by stake

--- app/queries/validator_query.rb
+++ app/queries/validator_query.rb
@@ -65,14 +65,11 @@ class ValidatorQuery < ApplicationQuery
     when "name"
       "validators.name asc"
     when "stake"
-      main_sort = "validator_score_v1s.active_stake desc"
-      secondary_sort = @api ? "validator_score_v1s.total_score desc" : "RAND()"
-
-      [main_sort, secondary_sort].join(", ")
+      "validator_score_v1s.active_stake desc, validator_score_v1s.total_score desc"
     else # Order by score by default
       main_sort = "validator_score_v1s.total_score desc"
       secondary_sort = @api ? "validator_score_v1s.active_stake desc" : "RAND()"
-      
+
       [main_sort, secondary_sort].join(", ")
     end
   end
