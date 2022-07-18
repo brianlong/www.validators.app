1ffbf05a9e4576015a6a50f99e6bd1a7594b5054
Radosław Stolarski <radoslaw.stolarski@polcode.net>
2022-07-18 13:20:56 +0200

    Set defaults to 6.1 in application.rb, remove file from initializers

--- config/application.rb
+++ config/application.rb
@@ -12,7 +12,7 @@ Bundler.require(*Rails.groups)
 module FmaRubyOnRailsTemplate
   class Application < Rails::Application
     # Initialize configuration defaults for originally generated Rails version.
-    config.load_defaults 6.0
+    config.load_defaults 6.1
 
     config.autoloader = :classic
 
