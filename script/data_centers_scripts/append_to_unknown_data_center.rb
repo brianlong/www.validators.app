182997567b10fe9388db395252aa44023de4a849
Radosław Stolarski <radoslaw.stolarski@polcode.net>
2022-07-27 17:16:02 +0200

    Add script to append validators without validator_ip to unknown data center

--- script/data_centers_scripts/append_to_unknown_data_center.rb
+++ script/data_centers_scripts/append_to_unknown_data_center.rb
@@ -8,29 +8,27 @@ log_path = Rails.root.join('log', 'append_to_unknown_data_center.log')
 @logger.info "Script has started at #{Time.now}"
 
 begin
+  data_center_key = ARGV[0] || "0--Unknown"
+  unknown_data_center = DataCenter.find_by(data_center_key: data_center_key)
+  service = DataCenters::AppendToDataCenter.new(unknown_data_center)
+
   # Data center for validators without data_center assigned
   # most cases does not provide IP so we cannot assign them
   # because we cannot check their IP in MaxMind. 
-  unknown_data_center = DataCenter.find_by(data_center_key: 'Unknown')
-  data_center_host = unknown_data_center.data_center_hosts.find_by(host: nil)
-
   validators_active_without_data_center = Validator.active.where.missing(:data_center)
-
+  
   validators_active_without_data_center.each do |val|
-    # Probably waiting for script append_data_centers_geo_data.rb
-    # which is fired every 1 hour
-    if val.validator_ips.any?
-      next
-    else
-      validator_ip = val.validator_ips.create(
-        data_center_host: data_center_host,
-        address: nil, 
-        is_active: true
-      )
+    # We don't want to assign new validators to Unknown data center
+    # we want to wait some time 
+    if val.created_at < Date.today - 5.days
+      service.call(val)
+
+      val.reload
 
       info = <<-EOS 
-        Validator #{val.name} (##{val.id}) assigned to 
-        data center: #{unknown_data_center.data_center_key} (##{unknown_data_center.id})
+        Validator #{val.name} (##{val.id}) 
+        assigned to data center #{unknown_data_center.data_center_key} (##{unknown_data_center.id});
+        validator_ip: ##{val.validator_ip_active.id}, data_center_host: ##{val.data_center_host.id}"
       EOS
 
       @logger.info info.squish
