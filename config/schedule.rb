a7927802d5f5cf293719d191e93f5b8e42495e51
Radosław Stolarski <radoslaw.stolarski@polcode.net>
2022-07-28 15:51:53 +0200

    Run script later

--- config/schedule.rb
+++ config/schedule.rb
@@ -55,12 +55,12 @@ end
 every 1.day, roles: [:background] do
   ruby_script 'validators_update_avatar_url.rb'
   ruby_script 'remove_unconfirmed_users.rb'
-  ruby_script_data_centers 'append_to_unknown_data_center.rb'
 end
 
 every 1.day, at: '0:10am', roles: [:background] do
   ruby_script_sol_prices 'coin_gecko_gather_yesterday_prices.rb'
   ruby_script_sol_prices 'ftx_gather_yesterday_prices.rb'
+  ruby_script_data_centers 'append_to_unknown_data_center.rb'
 end
 
 every 10.minutes, roles: [:background] do
