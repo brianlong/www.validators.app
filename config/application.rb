4cd62ef85cd796523988b5c94d72877deb85645a
maciejkocylapc <80820353+maciejkocylapc@users.noreply.github.com>
2022-08-11 11:58:32 +0200

    182783470 pipeline blacklist (#523)
    * create validator_blacklist.yml
    
    * add blacklist to solana logic
    
    * add test for blacklist
    
    * minor test refactor
    
    * restore credentials file
    
    * staging -> stage
    
    Co-authored-by: Klaudia Biala <klaudia.biala@polcode.net>

--- config/application.rb
+++ config/application.rb
@@ -16,6 +16,9 @@ module FmaRubyOnRailsTemplate
     # Load all of the sub-folders in `app/`
     config.autoloader = :classic
 
+    # Load blacklist of validators from config/validator_blacklist.yml
+    config.validator_blacklist = config_for(:validator_blacklist)
+
     # Settings in config/environments/* take precedence over those specified
     # here.
     # Application configuration can go into files in config/initializers
