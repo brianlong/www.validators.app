a9eab040ac4b224ed59539c0d9e4ed4d218bc0b2
maciejkocylapc <maciej.kocyla@polcode.net>
2022-08-24 13:40:50 +0200

    fix migration

--- db/migrate/20220824102422_change_identity_to_account_in_gossip_nodes.rb
+++ db/migrate/20220824102422_change_identity_to_account_in_gossip_nodes.rb
@@ -1,7 +1,5 @@
 class ChangeIdentityToAccountInGossipNodes < ActiveRecord::Migration[6.1]
   def change
     rename_column :gossip_nodes, :identity, :account
-
-    remove_index :gossip_nodes, :ip
   end
 end
