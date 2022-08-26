45fcd0cf4da865cc57f363f550e6b98093fd70cd
Radosław Stolarski <radoslaw.stolarski@polcode.net>
2022-08-26 16:43:17 +0200

    Add index on network to asn_Stats

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2022_08_26_141204) do
+ActiveRecord::Schema.define(version: 2022_08_26_144138) do
 
   create_table "active_storage_attachments", charset: "utf8mb4", collation: "utf8mb4_0900_ai_ci", force: :cascade do |t|
     t.string "name", null: false
@@ -50,6 +50,7 @@ ActiveRecord::Schema.define(version: 2022_08_26_141204) do
     t.string "network"
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
+    t.index ["network"], name: "index_asn_stats_on_network"
   end
 
   create_table "batches", charset: "utf8mb4", collation: "utf8mb4_0900_ai_ci", force: :cascade do |t|
