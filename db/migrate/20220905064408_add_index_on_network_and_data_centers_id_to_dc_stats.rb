9134503549015b7e7428700d438c476f7bbdc69e
maciejkocylapc <maciej.kocyla@polcode.net>
2022-09-07 11:59:50 +0200

    tmp migration

--- db/migrate/20220905064408_add_index_on_network_and_data_centers_id_to_dc_stats.rb
+++ db/migrate/20220905064408_add_index_on_network_and_data_centers_id_to_dc_stats.rb
@@ -1,5 +1,5 @@
 class AddIndexOnNetworkAndDataCentersIdToDcStats < ActiveRecord::Migration[6.1]
   def change
-    add_index :data_center_stats, [:network, :data_center_id], unique: true
+    # add_index :data_center_stats, [:network, :data_center_id], unique: true
   end
 end
