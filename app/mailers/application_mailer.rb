795bb2cff206079a284a6f4f7e31506469dd7da8
maciejkocylapc <maciej.kocyla@polcode.net>
2022-09-14 15:23:09 +0200

    update parentheses

--- app/mailers/application_mailer.rb
+++ app/mailers/application_mailer.rb
@@ -1,6 +1,6 @@
 # frozen_string_literal: true
 
-class ApplicationMailer < ActionMailer::Base  
-  default from: 'customer.service@fmadata.com'
-  layout 'mailer'
+class ApplicationMailer < ActionMailer::Base
+  default from: "customer.service@fmadata.com"
+  layout "mailer"
 end
