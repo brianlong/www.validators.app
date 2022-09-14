8660e3c4b2068e362db3b8e565b1b1ab2d60e5d1
maciejkocylapc <maciej.kocyla@polcode.net>
2022-09-14 14:20:05 +0200

    add faq

--- test/mailers/previews/commission_history_mailer_preview.rb
+++ test/mailers/previews/commission_history_mailer_preview.rb
@@ -4,7 +4,11 @@ class CommissionHistoryMailerPreview < ActionMailer::Preview
   def commission_change_info
     watchlist_el = UserWatchlistElement.first
     val = watchlist_el.validator
-    
-    CommissionHistoryMailer.commission_change_info(validator: val, commission: val.commission_histories.first)
+    usr = watchlist_el.user
+    CommissionHistoryMailer.commission_change_info(
+      user: usr,
+      validator: val,
+      commission: val.commission_histories.first
+    )
   end
 end
