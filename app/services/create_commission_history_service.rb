852c82efa6bf3de4e89b22f2ed6b0d3058df7873
maciejkocylapc <maciej.kocyla@polcode.net>
2022-09-14 14:04:42 +0200

    changed variable names

--- app/services/create_commission_history_service.rb
+++ app/services/create_commission_history_service.rb
@@ -9,18 +9,18 @@ class CreateCommissionHistoryService
   # payload returns CommissionHistory object if it has been created
   # otherwise it returns false
   def call
-    c_history = create_commission
-    notify_users(c_history)
+    commission_change = create_commission
+    notify_users(commission_change)
   rescue StandardError => e
     OpenStruct.new({ success?: false, error: e })
   else
-    OpenStruct.new({ success?: true, payload: c_history })
+    OpenStruct.new({ success?: true, payload: commission_change })
   end
 
   private
 
   def create_commission
-    new_ch = @score.validator.commission_histories.create(
+    @score.validator.commission_histories.create(
       commission_before: @score.commission_before_last_save,
       commission_after: @score.commission,
       batch_uuid: last_batch.uuid,
@@ -47,12 +47,12 @@ class CreateCommissionHistoryService
     ((recent_epoch.slot_index / recent_epoch.slots_in_epoch.to_f) * 100).round(2)
   end
 
-  def notify_users(commission)
+  def notify_users(commission_change)
     @score.validator.watchers.each do |watcher|
       CommissionHistoryMailer.commission_change_info(
         user: watcher,
         validator: @score.validator,
-        commission: commission
+        commission: commission_change
       ).deliver_later
     end
   end
