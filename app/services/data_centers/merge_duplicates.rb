955f8b483c3102792af4c558a7ee3aa550fe2df3
Radosław Stolarski <radoslaw.stolarski@polcode.net>
2022-09-15 14:16:58 +0200

    Split to methods

--- app/services/data_centers/merge_duplicates.rb
+++ app/services/data_centers/merge_duplicates.rb
@@ -6,55 +6,74 @@ module DataCenters
       @logger ||= Logger.new(LOG_PATH)
     end
 
+
     def call
-      all_data_centers = DataCenter.all.group(:data_center_key).count
-      duplicated_data_center_keys = all_data_centers.select { |k,v| k if v > 1 }.keys
+      duplicated_data_center_keys = select_duplicates
 
       duplicated_data_center_keys.each do |dc|
-        data_centers = DataCenter.where(data_center_key: dc)
-
-        data_centers_with_validators_size = []
-        data_centers.each do |dc| 
-          os = OpenStruct.new(
-            data_center: dc,
-            validators_number: dc.data_center_hosts.map { |dch| dch.validators.size }.sum
-          )
-          data_centers_with_validators_size << os
-        end
-
-        sorted_data_centers = data_centers_with_validators_size.sort { |os| -os.validators_number }
+        data_centers_with_validators_number = count_validators_in_data_center(dc)
+        
+        sorted_data_centers = sort_data_centers_by_validators_number(data_centers_with_validators_number)
 
         main_dc = sorted_data_centers.shift
-        log_message("Main dc is: #{main_dc.data_center.data_center_key}, (##{main_dc.data_center.id}) with #{main_dc.data_center.data_center_hosts.map { |dch| dch.validators.size }.sum } validators.")
+        log_message("Main dc is: #{main_dc.data_center.data_center_key}, (##{main_dc.data_center.id}) with #{main_dc.validators_number} validators.")
         main_dc = main_dc.data_center
 
         sorted_data_centers.each do |entry|
           dc = entry.data_center
-          duplicated_data_center_hosts = dc.data_center_hosts
+          data_center_hosts = dc.data_center_hosts
 
-          log_message("Processing data_center: #{dc.data_center_key}, (##{dc.id}) with #{duplicated_data_center_hosts.size} data center hosts and #{duplicated_data_center_hosts.map { |dch| dch.validators.size }.sum } validators.")
+          log_message("Processing data_center: #{dc.data_center_key}, (##{dc.id}) with #{data_center_hosts.size} data center hosts and #{data_center_hosts.map { |dch| dch.validators.size }.sum } validators.")
 
-          duplicated_data_center_hosts.each do |dch|
+          data_center_hosts.each do |dch|
             log_message("Processing data_center_host: #{dch.host}, (##{dch.id}) with #{dch.validators.size} validators.")
-            existing_main_dc_dch = main_dc.data_center_hosts.find_or_initialize_by(host: dch.host)
-            dch.validator_ips.each do |vip|
-              val = vip.validator
-              
-              next unless val
+            main_dc_host = main_dc.data_center_hosts.find_or_initialize_by(host: dch.host)
 
-              log_message("Assign validator #{val.name} (##{val.id}) with ip #{vip.address} (##{vip.id}) to data center host #{existing_main_dc_dch.host} (##{existing_main_dc_dch.id}).")
-              # vip.update(data_center_host_id: existing_main_dc_dch.id)
+            dch.validator_ips.each do |vip|
+              update_validator_ip(vip, main_dc_host)
             end
           end
         end
 
         log_message("---------------", type: :info)
-
       end
-
     end
 
-    private 
+    private
+
+    def select_duplicates
+      all_data_centers = DataCenter.all.group(:data_center_key).count
+      all_data_centers.select { |k,v| k if v > 1 }.keys
+    end
+
+    def count_validators_in_data_center(dc)
+      data_centers = DataCenter.where(data_center_key: dc)
+      data_centers_with_validators_number = []
+
+      data_centers.each do |dc| 
+        os = OpenStruct.new(
+          data_center: dc,
+          validators_number: dc.data_center_hosts.map { |dch| dch.validators.size }.sum
+        )
+        data_centers_with_validators_number << os
+      end
+
+      data_centers_with_validators_number
+    end
+
+    def sort_data_centers_by_validators_number(data_centers)
+      data_centers.sort { |os| -os.validators_number }
+    end
+
+    def update_validator_ip(validator_ip, main_dc_dch)
+      val = validator_ip.validator
+              
+      return unless val
+
+      log_message("Assign validator #{val.name} (##{val.id}) with ip #{validator_ip.address} (##{validator_ip.id}) to data center host #{main_dc_dch.host} (##{main_dc_dch.id}).")
+      # vip.update(data_center_host_id: existing_main_dc_dch.id)
+    end
+
     def log_message(message, type: :info)
       # Remove unless clause to see log file from tests.
       @logger.send(type, message.squish) unless Rails.env.test?
