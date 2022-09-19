80fb2749df70b52a0f819fbfce056c935e01fd59
maciejkocylapc <maciej.kocyla@polcode.net>
2022-09-19 13:07:12 +0200

    add average_slot_latency to ping thing stats

--- app/models/ping_thing_stat.rb
+++ app/models/ping_thing_stat.rb
@@ -4,16 +4,17 @@
 #
 # Table name: ping_thing_stats
 #
-#  id             :bigint           not null, primary key
-#  interval       :integer
-#  max            :float(24)
-#  median         :float(24)
-#  min            :float(24)
-#  network        :string(191)
-#  num_of_records :integer
-#  time_from      :datetime
-#  created_at     :datetime         not null
-#  updated_at     :datetime         not null
+#  id                   :bigint           not null, primary key
+#  average_slot_latency :integer
+#  interval             :integer
+#  max                  :float(24)
+#  median               :float(24)
+#  min                  :float(24)
+#  network              :string(191)
+#  num_of_records       :integer
+#  time_from            :datetime
+#  created_at           :datetime         not null
+#  updated_at           :datetime         not null
 #
 # Indexes
 #
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2022_09_16_065710) do
+ActiveRecord::Schema.define(version: 2022_09_19_072658) do
 
   create_table "active_storage_attachments", charset: "utf8mb4", collation: "utf8mb4_0900_ai_ci", force: :cascade do |t|
     t.string "name", null: false
@@ -259,6 +259,7 @@ ActiveRecord::Schema.define(version: 2022_09_16_065710) do
     t.datetime "time_from"
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
+    t.integer "average_slot_latency"
     t.index ["network", "interval"], name: "index_ping_thing_stats_on_network_and_interval"
   end
 
