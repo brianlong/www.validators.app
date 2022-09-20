bfa061453ba74b543619d4d2c5c4410dd22b787b
maciejkocylapc <maciej.kocyla@polcode.net>
2022-09-20 14:57:32 +0200

    script check_unknown_data_centers_for_updates

--- app/services/data_centers/check_ip_info_service.rb
+++ app/services/data_centers/check_ip_info_service.rb
@@ -8,7 +8,7 @@ class DataCenters::CheckIpInfoService
   end
 
   def call
-    puts @ip.inspect
+    puts @ip
 
     # Skip private IPs
     regexp_for_private_ips = /(^127\.)|(^10\.)|(^172\.1[6-9]\.)|(^172\.2[0-9]\.)|(^172\.3[0-1]\.)|(^192\.168\.)/
