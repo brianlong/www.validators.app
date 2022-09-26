8f805cda4435ffd9df257163ac430764d446d16d
maciejkocylapc <maciej.kocyla@polcode.net>
2022-09-26 16:15:21 +0200

    muted text test

--- app/javascript/packs/ping_thing/ping_thing_table.vue
+++ app/javascript/packs/ping_thing/ping_thing_table.vue
@@ -13,8 +13,8 @@
           <span class="small text-muted">Type (Commitment Level)</span>
         </th>
         <th class="column-lg">
-          Slot Sent<br />
-          Slot Landed (Latency)
+          <span class="text-muted">Slot Sent</span><br />
+          <span class="text-muted">Slot Landed</span> (Latency)
         </th>
         <th class="column-xs">Posted&nbsp;By</th>
       </tr>
@@ -47,8 +47,8 @@
             </span>
           </td>
           <td class="small">
-            {{ pt.slot_sent }} <br />
-            {{ pt.slot_landed }} ({{ slot_latency(pt.slot_sent, pt.slot_landed) }})
+            <span class="text-muted">{{ pt.slot_sent }}</span> <br />
+            <span class="text-muted">{{ pt.slot_landed }}</span> ({{ slot_latency(pt.slot_sent, pt.slot_landed) }})
           </td>
           <td class="text-muted">{{ pt.username }}</td>
         </tr>
