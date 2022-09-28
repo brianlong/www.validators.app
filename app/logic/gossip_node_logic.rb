e4464ec283a89b87724554c2b9d45c7068d0c6c5
Radosław Stolarski <radoslaw.stolarski@polcode.net>
2022-09-28 18:02:58 +0200

    Use validator_ip_active

--- app/logic/gossip_node_logic.rb
+++ app/logic/gossip_node_logic.rb
@@ -43,7 +43,7 @@ module GossipNodeLogic
   def set_staked_flag
     lambda do |p|
       staked_validators = Validator.joins(:validator_score_v1)
-                                   .includes(:validator_ips)
+                                   .includes(:validator_ip_active)
                                    .where("validator_score_v1s.active_stake > 0")
                                    .where(network: p.payload[:network])
 
