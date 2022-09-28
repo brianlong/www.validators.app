99eee85e7093ecceec5a975c6f6b0cad7c1b3772
Radosław Stolarski <radoslaw.stolarski@polcode.net>
2022-09-28 15:26:11 +0200

    Rewrite service

--- app/services/validator_ips/set_active_ip_for_gossip_nodes.rb
+++ app/services/validator_ips/set_active_ip_for_gossip_nodes.rb
@@ -15,57 +15,65 @@ module ValidatorIps
     def call
       log_message("#{SEPARATOR}#{START_MESSAGE}, run update: #{@run_update}#{SEPARATOR}")
 
-      GossipNode.find_each do |gossip_node|
-        next if gossip_node.validator_ip_active
-
-        message = <<-EOS
-          Processing Gossip Node with address #{gossip_node.ip} (##{gossip_node.id}).
-        EOS
-
-        log_message(message)
-
-        gossip_node_validator_ip = gossip_node.validator_ip
-        validator = gossip_node_validator_ip&.validator
-        validator_ip_active = validator&.validator_ip_active
-
-        if validator 
-          message = <<-EOS
-            Validator Ip connected with validator #{validator.name} (##{validator.id}).
-          EOS
-
-          log_message(message)
-        end
-
-        if validator_ip_active.present?
-          message = <<-EOS
-            Validator connected with active ip #{validator_ip_active.address} (##{validator_ip_active.id}).
-          EOS
-
-          log_message(message)
-        end
-
-        if gossip_node_validator_ip&.id == validator_ip_active&.id
-          message = <<-EOS
-            Gossip node validator ip (##{gossip_node_validator_ip&.id}) is the same as 
-            validator's ip active (##{validator_ip_active&.id}), skipping.
-          EOS
-
-          next
-        else
-          message = <<-EOS
-            Validator Ip #{gossip_node_validator_ip.address} (##{gossip_node_validator_ip.id}) will be set to active.
-          EOS
-
-          log_message(message)
-
-          gossip_node_validator_ip.update(is_active: true) if @run_update
-        end
-      end
+      set_is_active_in_inactive_vips_without_validator_id
+      set_is_active_in_inactive_vips_with_validator_id
 
       log_message("#{SEPARATOR}#{FINISH_MESSAGE}#{SEPARATOR}")
     end
 
     private
+
+    def set_is_active_in_inactive_vips_without_validator_id
+      vips_without_val_id = ValidatorIp.joins(:gossip_node)
+                                       .where(validator_id: nil, is_active:false)
+                                       .distinct
+            
+      message = <<-EOS
+        Number of inactive validator ips connected with gossip nodes, where validator_id is nil: #{vips_without_val_id.size}.
+      EOS
+
+      log_message(message)
+
+      vips_without_val_id.update_all(is_active: true) if @run_update
+    end
+
+    def set_is_active_in_inactive_vips_with_validator_id
+      ValidatorIp.joins(:gossip_node)
+                 .where("validator_ips.is_active = false AND validator_ips.validator_id IS NOT NULL")
+                 .each do |validator_ip|
+
+        message = <<-EOS
+          Processing vip with address #{validator_ip.address} (##{validator_ip.id}).
+        EOS
+
+        log_message(message)
+
+        gossip_node = validator_ip.gossip_node
+        validator = validator_ip.validator
+        validator_ip_active = validator&.validator_ip_active
+
+        if validator_ip_active.present?
+          message = <<-EOS
+            Validator #{validator.name} (##{validator.id}) connected with active vip #{validator_ip_active.address} (##{validator_ip_active.id}).
+          EOS
+
+          log_message(message)
+        end
+
+        if validator_ip.id != validator_ip_active.id
+          message = <<-EOS
+            Validator #{validator.name} (##{validator.id}) is connected to active vip with different id (##{validator_ip_active.id})
+            than currently processed vip (##{validator_ip.id}) connected to gossip node, 
+            vip set to active and validator_id is nullified.
+          EOS
+
+          log_message(message)
+
+          validator_ip.update(validator_id: nil, is_active: true) if @run_update
+        end
+      end
+    end
+
 
     def log_message(message, type: :info)
       # Remove unless clause to see log file from tests.
