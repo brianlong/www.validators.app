51ba96a5febff58afef494f7e608c3516afc6ccd
psuchta <pawel.suchta@polcode.net>
2022-09-29 18:46:14 +0200

    Add letter opener

--- test/mailers/previews/commission_history_mailer_preview.rb
+++ test/mailers/previews/commission_history_mailer_preview.rb
@@ -2,7 +2,13 @@
 
 class CommissionHistoryMailerPreview < ActionMailer::Preview
   def commission_change_info
-    watchlist_el = UserWatchlistElement.first
+    watchlist_el = UserWatchlistElement.first_or_create(
+      validator: Validator.first,
+      user: User.last
+    )
+    commission_history = FactoryBot.create(:commission_history, validator: Validator.first)
+
+
     val = watchlist_el.validator
     usr = watchlist_el.user
     CommissionHistoryMailer.commission_change_info(
