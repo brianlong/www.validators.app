7cc7315f55087052ca86affec45de7f022dd5a0d
maciejkocylapc <maciej.kocyla@polcode.net>
2022-09-30 10:07:00 +0200

    update babel

--- app/javascript/packs/navigation/network_buttons.js
+++ app/javascript/packs/navigation/network_buttons.js
@@ -2,9 +2,6 @@ import Vue from 'vue/dist/vue.esm'
 import NetworkButtonsTemplate from './network_buttons_template'
 import TurbolinksAdapter from 'vue-turbolinks';
 
-import WalletMultiButton from 'solana-wallets-vue-2/src/components/WalletMultiButton.vue';
-// import 'solana-wallets-vue-2/styles.css'
-
 Vue.use(TurbolinksAdapter);
 
 document.addEventListener('turbolinks:load', () => {
@@ -12,7 +9,6 @@ document.addEventListener('turbolinks:load', () => {
     el: '#network-buttons',
     render(createElement) {
       return createElement(NetworkButtonsTemplate)
-    },
-    components: { WalletMultiButton }
+    }
   })
 })
--- app/javascript/packs/navigation/network_buttons_template.vue
+++ app/javascript/packs/navigation/network_buttons_template.vue
@@ -1,5 +1,6 @@
 <template>
   <div class="btn-group btn-group-xs btn-group-toggle">
+    <wallet-multi-button :wallets="wallets" auto-connect />
     <label 
       class="btn btn-secondary nav-link" 
       :class="{ active: is_mainnet() }"
@@ -16,8 +17,9 @@
 </template>
 
 <script>
-  
-  import PhantomWalletAdapter from '@solana/wallet-adapter-wallets'
+  import { WalletMultiButton } from 'solana-wallets-vue-2';
+  import 'solana-wallets-vue-2/styles.css'
+  import { PhantomWalletAdapter } from '@solana/wallet-adapter-wallets'
 
   export default {
     data(){
@@ -54,6 +56,7 @@
         window.location.href = this.url
         return true
       }
-    }
+    },
+    components: { 'wallet-multi-button': WalletMultiButton }
   }
 </script>
