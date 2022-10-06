f04f0d4dbf84be730d217e54ce938211222a26e7
kamtarnowski <kam.tarnowski@gmail.com>
2022-10-06 15:26:45 +0200

    remove redundant active stake components; remove circulating_supply_channel

--- app/javascript/packs/validators/circulating_supply.js
+++ app/javascript/packs/validators/circulating_supply.js
@@ -1,14 +1,7 @@
 import Vue from 'vue/dist/vue.esm'
 import TurbolinksAdapter from 'vue-turbolinks';
 import CirculatingSupply from './circulating_supply.vue';
-import ActionCableVue from "actioncable-vue";
 Vue.use(TurbolinksAdapter);
-Vue.use(ActionCableVue, {
-  debug: true,
-  debugLevel: "error",
-  connectionUrl: "/cable",
-  connectImmediately: true,
-});
 
 document.addEventListener('turbolinks:load', () => {
   new Vue({
