adeb67c1ebfbd47640237deeeb935abcd5b4d169
kamtarnowski <kam.tarnowski@gmail.com>
2022-10-07 14:58:22 +0200

    add clusterStatChannel

---
+++ app/channels/cluster_stat_channel.rb
@@ -0,0 +1,11 @@
+# frozen_string_literal: true
+
+class ClusterStatChannel < ApplicationCable::Channel
+  def subscribed
+    stream_from "cluster_stat_channel"
+  end
+
+  def unsubscribed
+    # Any cleanup needed when channel is unsubscribed
+  end
+end
