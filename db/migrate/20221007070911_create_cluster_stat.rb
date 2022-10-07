0eecc1308791527b029b2dbc4d7846a90c098901
kamtarnowski <kam.tarnowski@gmail.com>
2022-10-07 14:55:12 +0200

    add stakeStatsLogic module; add broadcast method to clusterStut

--- db/migrate/20221007070911_create_cluster_stat.rb
+++ db/migrate/20221007070911_create_cluster_stat.rb
@@ -2,7 +2,6 @@ class CreateClusterStat < ActiveRecord::Migration[6.1]
   def change
     create_table :cluster_stats do |t|
       t.integer :total_active_stake
-      t.integer :total_stake
       t.string :network
 
       t.timestamps
