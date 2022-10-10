3148738b13a6a2a3590e2bfa691c26aefe3f45af
maciejkocylapc <maciej.kocyla@polcode.net>
2022-10-10 12:31:47 +0200

    add change and volume

--- app/javascript/packs/sol_prices/sol_price_component_template.vue
+++ app/javascript/packs/sol_prices/sol_price_component_template.vue
@@ -1,14 +1,28 @@
 <template>
   <div class="card col-md-3 col-sm-6">
     <div class="card-content">
-      <div class="col-12 stat-title-4">
-        sol price: 
-        <strong class="text-success" v-if="price">
-          <i class="fas fa-dollar-sign px-2"></i>{{price}}
-        </strong>
-        <strong class="text-muted" v-if="!price">
-          loading ...
-        </strong>
+      <div class="row">
+        <div class="col-12 stat-title-4 mb-2">
+          sol price: 
+          <strong class="text-success" v-if="price">
+            <i class="fas fa-dollar-sign px-2"></i>{{ price }}
+          </strong>
+          <strong class="text-muted" v-if="!price">
+            loading ...
+          </strong>
+        </div>
+        <div class="col-6 small">
+          24h change:<br />
+          <span :class="change_24h > 0 ? 'text-success' : 'text-danger' ">
+            {{ change_24h }}
+          </span>
+        </div>
+        <div class="col-6 small">
+          24h volume:<br />
+          <span class="text-success">
+            {{ volume_24h }}
+          </span>
+        </div>
       </div>
     </div>
   </div>
@@ -19,7 +33,9 @@
   export default {
     data() {
       return {
-        price: null
+        price: null,
+        change_24h: null,
+        volume_24h: null
       }
     },
     methods: {
@@ -32,6 +48,8 @@
         received(data) {
           console.log(data.result)
           this.price = data.result.last.toFixed(2)
+          this.change_24h = data.result.change24h.toFixed(4)
+          this.volume_24h = data.result.volumeUsd24h.toFixed(0)
         },
         disconnected() {},
       },
