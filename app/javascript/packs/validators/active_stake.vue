1f2586f7d9c360700d5e0bfa6120a3d1f6480a09
kamtarnowski <kam.tarnowski@gmail.com>
2022-10-11 14:41:16 +0200

    add interval for cluster-stats call

--- app/javascript/packs/validators/active_stake.vue
+++ app/javascript/packs/validators/active_stake.vue
@@ -13,27 +13,34 @@
   export default {
     data () {
       return {
-        total_active_stake: 0
+        total_active_stake: null,
+        api_url: "/cluster-stats",
       };
     },
     created() {
       const ctx = this;
-      const api_url = "/cluster-stats";
 
-      axios.get(api_url, {
-        headers: {
-          'Content-Type': 'application/json',
-          Accept: 'application/json'
-        },
-      }).then(function (response) {
-        const stake = response.data.total_active_stake;
-        ctx.total_active_stake = ctx.lamports_to_sol(stake).toLocaleString('en-US');
-      })
+      this.update_total_active_stake(ctx);
+
+      setInterval(() => {
+        this.update_total_active_stake(ctx);
+      }, 300000);
     },
     methods: {
       lamports_to_sol(lamports) {
         return lamports / 1000000000;
       },
+      update_total_active_stake(ctx) {
+        axios.get(this.api_url, {
+          headers: {
+            'Content-Type': 'application/json',
+            Accept: 'application/json'
+          },
+        }).then(function (response) {
+          const stake = response.data.total_active_stake;
+          ctx.total_active_stake = ctx.lamports_to_sol(stake).toLocaleString('en-US');
+        })
+      }
     },
   }
 </script>
