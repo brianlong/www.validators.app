6434f6d1cc08a3ad4cf35a2794a04fdaf5f6aa84
Klaudia Biala <klaudia.biala@polcode.net>
2022-10-11 14:09:12 +0200

    Check is_active status for newly created node

--- test/logic/gossip_node_logic_test.rb
+++ test/logic/gossip_node_logic_test.rb
@@ -50,6 +50,7 @@ class GossipNodeLogicTest < ActiveSupport::TestCase
       refute p.payload[:current_nodes].blank?
       assert GossipNode.exists?
       assert_equal @network, GossipNode.last.network
+      assert GossipNode.last.is_active
       refute GossipNode.where(account: nil).exists?
       refute GossipNode.where(ip: nil).exists?
     end
