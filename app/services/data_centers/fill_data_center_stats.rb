35712b493991b12c76db3a1daf6fac6b0da08abc
Klaudia Biala <klaudia.biala@polcode.net>
2022-10-13 14:31:58 +0200

    Fix nodes count in data center stat

--- app/services/data_centers/fill_data_center_stats.rb
+++ app/services/data_centers/fill_data_center_stats.rb
@@ -9,8 +9,8 @@ class DataCenters::FillDataCenterStats
     DataCenter.includes(:validators, :gossip_nodes).all.each do |dc|
       validators_count = dc.validators.where(network: @network).size
       active_validators_count = dc.validators.where(network: @network).active.size
-      nodes_count = dc.gossip_nodes.where(network: @network).size
-      active_nodes_count = dc.gossip_nodes.where(network: @network).active.size
+      nodes_count = dc.gossip_nodes.where(network: @network, staked: false).size
+      active_nodes_count = dc.gossip_nodes.where(network: @network, staked: false).active.size
 
       stats = dc.data_center_stats.find_or_create_by(network: @network)
       stats.validators_count = validators_count
