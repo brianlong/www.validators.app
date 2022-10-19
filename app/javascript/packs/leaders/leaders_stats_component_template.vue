977115c53a5445b8a4cd12ec6f3e94145581bac8
Klaudia Biala <klaudia.biala@polcode.net>
2022-10-19 17:01:50 +0200

    Tweak styling a little

--- app/javascript/packs/leaders/leaders_stats_component_template.vue
+++ app/javascript/packs/leaders/leaders_stats_component_template.vue
@@ -1,31 +1,28 @@
 <template>
-  <div class="col-md-4 col-sm-6 mb-4">
+  <div class="col-sm-6 mb-4">
     <div class="card h-100">
       <div class="card-content">
-        <h2 class="h5 card-heading-left">Leader</h2>
-
-        <span class="text-muted" v-if="!current_leader">loading...</span>
-
-        <div class="leaders-info" v-if="current_leader">
-          <div class="current-leader-info center-block text-center mb-2">
-            <div class="info-avatar">
-              <img :src="create_avatar_link(current_leader)" class='img-circle-medium' />
+        <div class="mb-4">
+          <h2 class="h5 card-heading-left">Leader</h2>
+          <span class="text-muted" v-if="!current_leader">loading...</span>
+          <div class="d-flex flex-wrap gap-3" v-if="current_leader">
+            <img :src="create_avatar_link(current_leader)" class="img-circle-medium" />
+            <div class="d-flex flex-column justify-content-center">
+              <strong class="text-purple">{{ leader_name(current_leader) }}</strong>
             </div>
-            <div class="text-muted mt-1">{{ leader_name(current_leader) }}</div>
           </div>
+        </div>
 
-          <div class="lead">Next Leaders</div>
-          <div class="next-leaders-info mt-3">
-            <div class="info-avatar d-flex justify-content-between flex-wrap gap-1">
-              <span v-for="leader in next_leaders">
-                <img :src="create_avatar_link(leader)" class='img-circle-medium' />
-              </span>
-            </div>
+        <div v-if="current_leader">
+          <h2 class="h5 card-heading-left">Next Leaders</h2>
+          <div class="d-flex flex-wrap gap-3">
+            <span v-for="leader in next_leaders">
+              <img :src="create_avatar_link(leader)" class='img-circle-medium' />
+            </span>
           </div>
         </div>
       </div>
     </div>
-  </div>
   </div>
 </template>
 
