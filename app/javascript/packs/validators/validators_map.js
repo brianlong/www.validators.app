961f755c250bb1681582425aeeab4472de390022
kamtarnowski <kam.tarnowski@gmail.com>
2022-10-20 16:55:18 +0200

    add testnet for validators component

--- app/javascript/packs/validators/validators_map.js
+++ app/javascript/packs/validators/validators_map.js
@@ -2,12 +2,14 @@ import Vue from 'vue/dist/vue.esm'
 import ValidatorsMapComponentTemplate from './validators_map.vue'
 import ValidatorsMapDataCenterDetailsComponent from './validators_map_data_center_details'
 import TurbolinksAdapter from 'vue-turbolinks';
+import store from "../stores/main_store.js";
 
 Vue.use(TurbolinksAdapter);
 
 document.addEventListener('turbolinks:load', () => {
   new Vue({
     el: '#validators-map',
+    store,
     components: {
       "validators-map": ValidatorsMapComponentTemplate,
       "validators-map-data-center-details": ValidatorsMapDataCenterDetailsComponent,
--- app/javascript/packs/validators/validators_map.vue
+++ app/javascript/packs/validators/validators_map.vue
@@ -70,14 +70,11 @@
 <script>
   import debounce from 'lodash/debounce';
   import axios from 'axios';
+  import { mapGetters } from 'vuex';
+
   axios.defaults.headers.get["Authorization"] = window.api_authorization;
 
   export default {
-    props: {
-      network: {
-        default: "mainnet"
-      },
-    },
     data () {
       let api_url = "/api/v1/data-centers-with-nodes/" + this.network
       return {
@@ -97,6 +94,9 @@
     },
     watch: {
     },
+    computed: mapGetters([
+      'network'
+    ]),
     methods: {
       position_horizontal: function(longitude) {
         return 50 + (longitude / 160 * 50) + '%';
