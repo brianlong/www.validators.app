5205f34c06dbd5bf07fb27ab10e391ebbe57a9fe
maciejkocylapc <80820353+maciejkocylapc@users.noreply.github.com>
2022-10-25 14:34:44 +0200

    183568931 nodes count component (#609)
    * worker updating new data
    
    * front live stats
    
    * passing test
    
    * add missing class
    
    * count not staked gossip nodes
    
    * test for not staked nodes

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,8 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2022_10_11_131406) do
+ActiveRecord::Schema.define(version: 2022_10_24_131325) do
+
   create_table "active_storage_attachments", charset: "utf8mb4", collation: "utf8mb4_0900_ai_ci", force: :cascade do |t|
     t.string "name", null: false
     t.string "record_type", null: false
@@ -77,6 +78,9 @@ ActiveRecord::Schema.define(version: 2022_10_11_131406) do
     t.string "network"
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
+    t.integer "validator_count"
+    t.integer "nodes_count"
+    t.string "software_version"
     t.index ["network"], name: "index_cluster_stat_on_network"
   end
 
--- test/services/create_cluster_stats_service_test.rb
+++ test/services/create_cluster_stats_service_test.rb
@@ -3,17 +3,19 @@
 class CreateClusterStatsServiceTest < ActiveSupport::TestCase
   def setup
     @network = "mainnet"
-    @batch_uuid = create(:batch, network: "mainnet").uuid
+    @software_version =  "1.1.1"
+    @batch = create(:batch, network: @network, software_version: @software_version)
 
     @validator_histories = [
       create(:validator_history, batch_uuid: @batch_uuid, active_stake: 10),
       create(:validator_history, batch_uuid: @batch_uuid, active_stake: 20),
       create(:validator_history, batch_uuid: @batch_uuid, active_stake: 31),
     ]
+
   end
 
   test "#call creates new ClusterStat with correct data if there's no cluster stats for given network" do
-    CreateClusterStatsService.new(network: @network, batch_uuid: @batch_uuid).call
+    CreateClusterStatsService.new(network: @network, batch_uuid: @batch.uuid).call
 
     assert ClusterStat.exists?
     assert ValidatorHistory.all.sum(&:active_stake), ClusterStat.last.total_active_stake
@@ -21,10 +23,25 @@ class CreateClusterStatsServiceTest < ActiveSupport::TestCase
 
   test "#call updates ClusterStat for network if it exists" do
     cluster_stat = create(:cluster_stat, network: @network, total_active_stake: 0)
-    CreateClusterStatsService.new(network: @network, batch_uuid: @batch_uuid).call
+    CreateClusterStatsService.new(network: @network, batch_uuid: @batch.uuid).call
 
     assert_equal 1, ClusterStat.where(network: @network).count
     assert ValidatorHistory.all.sum(&:active_stake), cluster_stat.total_active_stake
   end
 
+  test "#call updates ClusterStat with correct values" do
+    5.times do |n|
+      create(:gossip_node, network: @network)
+      create(:validator, network: @network)
+    end
+
+    create(:gossip_node, network: @network, staked: true)
+    
+    CreateClusterStatsService.new(network: @network, batch_uuid: @batch.uuid).call
+    stat = ClusterStat.where(network: @network).last
+
+    assert_equal @software_version, stat.software_version
+    assert_equal 5, stat.validator_count
+    assert_equal 5, stat.nodes_count
+  end
 end
