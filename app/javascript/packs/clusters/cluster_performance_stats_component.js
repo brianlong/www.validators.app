cf0a698c91e9fcd7af049d41f1d32086fd98a4d4
Klaudia Biala <klaudia.biala@polcode.net>
2022-10-26 20:13:13 +0200

    Use more appriopriate name

--- app/javascript/packs/clusters/cluster_performance_stats_component.js
+++ app/javascript/packs/clusters/cluster_performance_stats_component.js
@@ -1,5 +1,5 @@
 import Vue from 'vue/dist/vue.esm';
-import DistanceStatsComponentTemplate from './distance_stats_component_template';
+import DistanceStatsComponentTemplate from './cluster_performance_stats_component_template';
 import TurbolinksAdapter from 'vue-turbolinks';
 import store from "../stores/main_store.js";
 import ActionCableVue from "actioncable-vue";
