db25c7fe700df5688c5c720dcbdb9e08e096d7dd
kamtarnowski <kam.tarnowski@gmail.com>
2022-10-26 13:11:15 +0200

    remove cluster stats attrs from report logic

--- app/logic/report_logic.rb
+++ app/logic/report_logic.rb
@@ -156,11 +156,7 @@ module ReportLogic
       top_root_distance_validators: vs_stats.top_root_distance_averages_validators || [],
       top_vote_distance_validators: vs_stats.top_vote_distance_averages_validators || [],
       top_skipped_slot_validators: vbh_stats.top_skipped_slot_percent || [],
-      skipped_votes_percent: vah_stats.skipped_votes_stats,
       skipped_votes_percent_moving_average: vah_stats.skipped_vote_moving_average_stats,
-      root_distance: vs_stats.root_distance_stats,
-      vote_distance: vs_stats.vote_distance_stats,
-      skipped_slots: vbh_stats.skipped_slot_stats,
       software_version: software_report,
       batch_uuid: batch.uuid
     }
