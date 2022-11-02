13b504e057e7bb0b6243199a2243cfdbd7b6abeb
maciejkocylapc <maciej.kocyla@polcode.net>
2022-11-02 17:21:23 +0100

    if not equal

--- script/data_centers_scripts/check_hetzner_admin_warning.rb
+++ script/data_centers_scripts/check_hetzner_admin_warning.rb
@@ -1,5 +1,5 @@
 Validator.where(admin_warning: "Hetzner Data Center").each do |val|
-  if val.data_center&.traits_autonomous_system_number == 24940
+  if val.data_center&.traits_autonomous_system_number != 24940
     val.update(admin_warning: nil)
   end
 end
