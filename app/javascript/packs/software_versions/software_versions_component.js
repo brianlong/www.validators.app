3faf0251dc44e671c7372f3b1b6892584175bbf3
maciejkocylapc <80820353+maciejkocylapc@users.noreply.github.com>
2022-11-07 12:25:56 +0100

    183607614 software versions vue (#615)
    * front daemon with software versions
    
    * front for software versions component
    
    * add loading info
    
    * working version link
    
    * modal for more sv
    
    * methods + refactor
    
    * Tweak styling
    
    * Fix indentation
    
    * minor refactor
    
    Co-authored-by: Klaudia Biala <klaudia.biala@polcode.net>

---
+++ app/javascript/packs/software_versions/software_versions_component.js
@@ -0,0 +1,23 @@
+import Vue from 'vue/dist/vue.esm'
+import SoftwareVersionsTemplate from './software_versions_template'
+import TurbolinksAdapter from 'vue-turbolinks';
+import ActionCableVue from "actioncable-vue";
+import store from "../stores/main_store.js";
+
+Vue.use(TurbolinksAdapter);
+Vue.use(ActionCableVue, {
+  debug: true,
+  debugLevel: "error",
+  connectionUrl: "/cable",
+  connectImmediately: true,
+});
+
+document.addEventListener('turbolinks:load', () => {
+  new Vue({
+    el: '#software-versions-component',
+    store,
+    render(createElement) {
+      return createElement(SoftwareVersionsTemplate)
+    }
+  })
+})
---
+++ daemons/concerns/front_software_versions_module.rb
@@ -0,0 +1,31 @@
+# frozen_string_literal: true
+
+require_relative './front_stats_constants.rb'
+
+module FrontSoftwareVersionsModule
+  include FrontStatsConstants
+  include ValidatorsHelper
+
+  def get_versions_for_networks(networks = FrontStatsConstants::NETWORKS)
+    versions_for_networks = {}
+    networks.each do |network|
+      versions_for_networks[network] = sort_software_versions(get_latest_versions_report(network))
+    end
+    versions_for_networks
+  end
+
+  def broadcast_software_versions(software_versions)
+    ActionCable.server.broadcast("software_versions_channel", software_versions)
+  end
+
+  private
+
+  def get_latest_versions_report(network)
+    @report ||= Report.where(
+      name: FrontStatsConstants::SOFTWARE_VERSION_REPORT_NAME,
+      network: network
+    ).order(created_at: :desc).first
+
+    @report&.payload
+  end
+end
