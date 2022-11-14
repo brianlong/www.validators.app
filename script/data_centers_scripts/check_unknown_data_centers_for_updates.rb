741af8ea952cc33a81c09bb45e7df28a77745e08
psuchta <pawel.suchta@polcode.net>
2022-11-14 19:48:14 +0100

    Fix check_unknown_data_centers_for_updates

--- script/data_centers_scripts/check_unknown_data_centers_for_updates.rb
+++ script/data_centers_scripts/check_unknown_data_centers_for_updates.rb
@@ -1,8 +1,12 @@
 # frozen_string_literal: true
+require_relative('../../config/environment')
 
 unknown_data_center = DataCenter.find_by(data_center_key: DataCenter::UNKNOWN_DATA_CENTER_KEY)
+return if unknown_data_center.blank?
+
 unknown_validator_ips = unknown_data_center.validator_ips
+ip_service = DataCenters::CheckIpInfoService.new
 
 unknown_validator_ips.each do |validator_ip|
-  DataCenters::CheckIpInfoService.new(ip: validator_ip.address).call
+  ip_service.call(ip: validator_ip)
 end
