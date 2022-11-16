2b98cf2f0279366ced96bf4dd8194c2464ae1406
kamtarnowski <kam.tarnowski@gmail.com>
2022-11-16 14:02:13 +0100

    add pythnnet to gather vote account details daemon

--- daemons/gather_vote_account_details_daemon.rb
+++ daemons/gather_vote_account_details_daemon.rb
@@ -6,12 +6,13 @@ class SkipAndSleep < StandardError; end
 
 begin
   loop do
-    %w[mainnet testnet].each do |network|
-      config_urls = if network == 'testnet'
-        Rails.application.credentials.solana[:testnet_urls]
-      else
-        Rails.application.credentials.solana[:mainnet_urls]
-      end
+    NETWORKS.each do |network|
+      config_urls =
+        case network
+        when "mainnet" then Rails.application.credentials.solana[:mainnet_urls]
+        when "testnet" then Rails.application.credentials.solana[:testnet_urls]
+        when "pythnet" then Rails.application.credentials.solana[:pythnet_urls]
+        end
 
       Gatherers::VoteAccountDetailsService.new(
         network: network,
