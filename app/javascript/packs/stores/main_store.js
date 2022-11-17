96759cd1196fe2d95354299b94cfd6c9fe44e787
kamtarnowski <kam.tarnowski@gmail.com>
2022-11-17 15:45:40 +0100

    update main store with pythnet

--- app/javascript/packs/stores/main_store.js
+++ app/javascript/packs/stores/main_store.js
@@ -5,20 +5,33 @@ Vue.use(Vuex)
 const state = {
   mainnet_url: "https://validato-va-34e2.mainnet.rpcpool.com",
   mainnet_beta_url: "https://api.mainnet-beta.solana.com",
-  testnet_url: "https://api.testnet.solana.com"
+  testnet_url: "https://api.testnet.solana.com",
+  pythnet_url: "https://pythnet.rpcpool.com"
 }
 const getters = {
   web3_url: function(state, getters) {
-    if(getters.network === 'testnet') {
-      return state.testnet_url
-    } else if(location.hostname !== "localhost") {
-      return state.mainnet_url
-    } else {
+    if (getters.network === 'mainnet' && location.hostname === "localhost") {
       return state.mainnet_beta_url
+    }
+
+    switch (getters.network) {
+      case 'mainnet':
+        return state.mainnet_url
+      case 'pythnet':
+        return state.pythnet_url
+      case 'testnet':
+        return state.testnet_url
     }
   },
   network() {
-    return location.href.match(/network=testnet/) ? 'testnet' : 'mainnet'
+    switch (true) {
+      case location.href.match(/network=pythnet/):
+        return 'pythnet'
+      case location.href.match(/network=testnet/):
+        return 'testnet'
+      default:
+        return 'mainnet'
+    }
   }
 }
 export default new Vuex.Store({
