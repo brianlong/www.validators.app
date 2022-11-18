717fe75b1cc1484ab8f4351bc13e03a83c95e643
kamtarnowski <kam.tarnowski@gmail.com>
2022-11-18 13:47:23 +0100

    fix tests

--- test/javascript/commission_histories/index.test.js
+++ test/javascript/commission_histories/index.test.js
@@ -1,9 +1,9 @@
 import Vue from 'vue/dist/vue.esm'
 import { shallowMount } from '@vue/test-utils'
 import IndexTemplate from '../../../app/javascript/packs/commission_histories/index_template'
-import { PaginationPlugin } from "bootstrap-vue";
 import { BPagination } from "bootstrap-vue";
-import axios from 'axios'
+import axios from 'axios';
+import store from "../../../app/javascript/packs/stores/main_store.js";
 
 jest.mock('axios');
 
@@ -21,19 +21,16 @@ var mock_commission_changes = function(){
   });
 }
 
-
 describe('index_template', ()=> {
   it('has correct params without query', ()=>{
     Vue.component('BPagination', BPagination)
     mock_commission_changes()
     const wrapper = shallowMount(IndexTemplate, {
-      propsData: {
-        network: 'testnet'
-      },
+      store,
       stubs: {BPagination: BPagination}
     })
 
-    expect(wrapper.vm.api_url).toMatch("/api/v1/commission-changes/testnet?")
+    expect(wrapper.vm.api_url).toMatch("/api/v1/commission-changes/mainnet?")
     expect(wrapper.vm.page).toBe(1)
     expect(wrapper.vm.sort_by).toMatch('created_at_desc')
     expect(wrapper.vm.account_name).toBe(undefined)
@@ -43,14 +40,14 @@ describe('index_template', ()=> {
     Vue.component('BPagination', BPagination)
     mock_commission_changes()
     const wrapper = shallowMount(IndexTemplate, {
+      store,
       propsData: {
-        query: 'abc',
-        network: 'testnet'
+        query: 'abc'
       },
       stubs: {BPagination: BPagination}
     })
 
-    expect(wrapper.vm.api_url).toMatch("/api/v1/commission-changes/testnet?query=abc&")
+    expect(wrapper.vm.api_url).toMatch("/api/v1/commission-changes/mainnet?query=abc&")
     expect(wrapper.vm.page).toBe(1)
     expect(wrapper.vm.sort_by).toMatch('created_at_desc')
     expect(wrapper.vm.account_name).toBe('abc')
