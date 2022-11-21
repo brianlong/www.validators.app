6083e8799266d41de8f45710810b51dfe4b19943
maciejkocylapc <maciej.kocyla@polcode.net>
2022-11-21 11:16:59 +0100

    network buttons with pythnet

--- app/javascript/packs/navigations/network_buttons.js
+++ app/javascript/packs/navigations/network_buttons.js
@@ -1,12 +1,14 @@
 import Vue from 'vue/dist/vue.esm'
 import NetworkButtonsTemplate from './network_buttons_template'
 import TurbolinksAdapter from 'vue-turbolinks';
+import store from "../stores/main_store.js";
 
 Vue.use(TurbolinksAdapter);
 
 document.addEventListener('turbolinks:load', () => {
   const chindex = new Vue({
     el: '#network-buttons',
+    store,
     render(createElement) {
       return createElement(NetworkButtonsTemplate)
     }
--- app/javascript/packs/navigations/network_buttons_template.vue
+++ app/javascript/packs/navigations/network_buttons_template.vue
@@ -1,47 +1,49 @@
 <template>
-  <div class="btn-group btn-group-xs btn-group-toggle">
-    <label 
-      class="btn btn-secondary nav-link" 
-      :class="{ active: is_mainnet() }"
-      @click="change_network('mainnet')">
-      <a class="">Mainnet</a>
-    </label>
-    <label 
-      class="btn btn-secondary nav-link" 
-      :class="{ active: is_testnet() }"
-      @click="change_network('testnet')">
-      <a class="">Testnet</a>
-    </label>
+  <div class="dropdown">
+    <button class="btn btn-secondary dropdown-toggle btn-block" type="button" id="dropdownMenuButton1" data-bs-toggle="dropdown" aria-expanded="false">
+      {{ network }}
+    </button>
+    <ul class="dropdown-menu" aria-labelledby="dropdownMenuButton1" data-turbolinks="false">
+      <li v-for="network_entry in $store.state.networks" :key="network_entry">
+        <a class="dropdown-item" href="#" @click.prevent="change_network(network_entry)">{{ network_entry }}</a>
+      </li>
+    </ul>
   </div>
 </template>
 
 <script>
+  import { mapGetters } from 'vuex'
+
   export default {
     data(){
       return {
-        network: '',
+        current_network: '',
         url: ''
       }
     },
     created() {
       this.url = window.location.href
       if(this.url.includes('testnet')){
-        this.network = 'testnet'
+        this.current_network = 'testnet'
       } else {
-        this.network = 'mainnet'
+        this.current_network = 'mainnet'
       }
     },
+    computed: mapGetters([
+      'network'
+    ]),
     methods: {
       is_testnet: function(){
-        return this.network == 'testnet'
+        return this.current_network == 'testnet'
       },
       is_mainnet: function(){
-        return this.network == 'mainnet'
+        return this.current_network == 'mainnet'
       },
       change_network: function(target_network){
         let splitted_url = this.url.split("/")
+        this.url = window.location.href
         if(this.url.includes('network=')){
-          this.url = this.url.replace(/network=(mainnet|testnet)/, "network=" + target_network)
+          this.url = this.url.replace(/network=(mainnet|testnet|pythnet)/, "network=" + target_network)
         } else {
           this.url = this.url + '?network=' + target_network
         }
