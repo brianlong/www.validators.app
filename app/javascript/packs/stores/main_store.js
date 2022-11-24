89f3b6c4ceee43b3bcdf5e35de7ac5ef41dcd71f
maciejkocylapc <80820353+maciejkocylapc@users.noreply.github.com>
2022-11-24 12:17:24 +0100

    add pythnet to cluster stats and link for web3 (#663)
    * add pythnet to cluster stats and link for web3
    
    * passing tests

--- app/javascript/packs/stores/main_store.js
+++ app/javascript/packs/stores/main_store.js
@@ -6,12 +6,15 @@ const state = {
   mainnet_url: "https://validato-va-34e2.mainnet.rpcpool.com",
   mainnet_beta_url: "https://api.mainnet-beta.solana.com",
   testnet_url: "https://api.testnet.solana.com",
+  pythnet_url: "https://pythnet.rpcpool.com",
   networks: ["mainnet", "testnet", "pythnet"]
 }
 const getters = {
   web3_url: function(state, getters) {
     if(getters.network === 'testnet') {
       return state.testnet_url
+    } else if(getters.network === 'pythnet'){
+      return state.pythnet_url
     } else if(location.hostname !== "localhost") {
       return state.mainnet_url
     } else {
