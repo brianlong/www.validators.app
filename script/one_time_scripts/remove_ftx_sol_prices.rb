2817213f3b42e0fb57169d1728f1dc4af8dd923e
Kamil Tarnowski <kam.tarnowski@gmail.com>
2022-12-01 23:12:17 +0100

    Use ftx index const

--- script/one_time_scripts/remove_ftx_sol_prices.rb
+++ script/one_time_scripts/remove_ftx_sol_prices.rb
@@ -6,7 +6,7 @@ logger = Logger.new(log_path)
 FTX_INDEX = SolPrice::EXCHANGES.index('ftx') || 1
 
 begin
-  SolPrice.where(exchange: 1).destroy_all
+  SolPrice.where(exchange: FTX_INDEX).destroy_all
 rescue StandardError => e
   logger.error "Error message: #{e.message}\n#{e.backtrace}"
   puts 'Error occurred, more info in log/remove_ftx_sol_prices.log'
