71b78e729df923c04ec2b97293c8e25fb4731f2c
ktarnowski <kam.tarnowski@gmail.com>
2022-12-08 15:30:37 +0100

    remove html content rendering in sol price page

--- app/controllers/sol_prices_controller.rb
+++ app/controllers/sol_prices_controller.rb
@@ -1,20 +1,6 @@
 class SolPricesController < ApplicationController
-  respond_to :html, :js
+  respond_to :html
 
-  def index
-    filter = params[:filtering].to_i
-    sol_price_count ||= SolPrice.count
-
-    coin_gecko_prices = SolPrice.where(exchange: SolPrice.exchanges[:coin_gecko])
-                                .where.not(average_price: nil)
-                                .order(datetime_from_exchange: :asc)
-                                .last(filter)
-
-    @coin_gecko_data = coin_gecko_prices.map do |coin_gecko_price|
-      {
-        x: coin_gecko_price.datetime_from_exchange.strftime("%b %d"),
-        y: coin_gecko_price.average_price.round(2)
-      }
-    end
-  end
+  def index; end
 end
+app/views/layouts/_footer.html.erb
