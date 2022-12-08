a32847e8bacb55c64a78321c735b00c907046cca
ktarnowski <kam.tarnowski@gmail.com>
2022-12-08 15:32:01 +0100

    fix chart data

--- app/javascript/packs/sol_prices/historical_sol_prices_component_template.vue
+++ app/javascript/packs/sol_prices/historical_sol_prices_component_template.vue
@@ -7,21 +7,25 @@
     <div class="card">
       <div class="card-content">
         <div class="d-inline-block mb-4 float-md-end">
-          {{ create_filter_links }}
           <div class="btn-group">
-            <div v-for="filter_by_days in filter_by_days_options">
-              <a :href="sol_prices_path(filter_by_days)"
-                 title="`Filter by ${filter_days} days`"
-                 class="btn btn-sm btn-secondary nav-link chartFilterButton `${filter_btn_class(filter_days)}">
-                {{ `{{filter_days}} days` }}
-              </a>
-            </div>
+            <button v-for="filter_by_days in filter_by_days_options"
+               v-on:click="select_filter_by_days(filter_by_days)"
+               :title="'Filter by ' + filter_by_days +' days'"
+               class="btn btn-sm btn-secondary nav-link chartFilterButton"
+               :class="{ active: is_active_filter(filter_by_days) }">
+               {{ `${filter_by_days} days` }}
+            </button>
           </div>
         </div>
 
         <div class="tab-content" id='sol-prices-tab-content'>
-          <div class="tab-pane solPriceChartTab <%= active_tab?(:coin_gecko) %>" id="coinGeckoChartTab">
-            <%= render 'coin_gecko_chart' %>
+          <div class="tab-pane solPriceChartTab active" id="coinGeckoChartTab">
+            <div id="coinGeckoChart">
+              <canvas id="coinGeckoChartContent"></canvas>
+            </div>
+            <p class='mt-3 text-center'>
+              Average price of Sol Token, powered by <a href="https://www.coingecko.com/en" target='_blank'>CoinGecko</a>.
+            </p>
           </div>
         </div>
       </div>
@@ -30,27 +34,52 @@
 </template>
 
 <script>
-  const filter_by_days_options = [7, 30, 60, 90]
+  import axios from 'axios';
+  import { mapGetters } from 'vuex';
+
+  axios.defaults.headers.get["Authorization"] = window.api_authorization;
+
+  const filter_by_days_options = [7, 30, 60, 90];
 
   export default {
     data() {
       return {
-        filter_days_options: filter_days_options
+        filter_by_days_options: filter_by_days_options,
+        days: filter_by_days_options[0]
       }
     },
+    created() {
+      this.draw_chart();
+    },
     methods: {
-      filter_btn_class(filter_by_days) {
-        return window.location.href.includes(`filtering=${filter_by_days}`) ? " active" : "";
+      is_active_filter(days) {
+        return this.days === days;
       },
-      exchange_params() {
-        return window.location.href.includes("exchange=true") ? true : false;
+      select_filter_by_days(days) {
+        this.days = days;
+
+        this.draw_chart();
       },
-      sol_prices_path(filter_by_days) {
-        return `/sol-prices?network=${this.network}&filtering=${filter_by_days}&exchange=${exchange_params}`;
+      recreate_chart_skeleton() {
+        const chartCoinGeckoContent = document.getElementById('coinGeckoChartContent');
+        const chartCoinGecko = document.getElementById('coinGeckoChart');
+
+        chartCoinGeckoContent.remove();
+        chartCoinGecko.innerHTML = "<canvas id='coinGeckoChartContent'</canvas>";
+      },
+      draw_chart() {
+        var ctx = this;
+        var url = "api/v1/historical_sol_prices";
+
+        axios.get(url, { params: { filtering: this.days } })
+          .then(response => {
+            ctx.recreate_chart_skeleton();
+            drawChart(response.data);
+          })
       }
     },
     computed: mapGetters([
       'network'
-    ]),
+    ])
   }
 </script>
