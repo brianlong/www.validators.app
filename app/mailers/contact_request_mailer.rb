20d5aab1951ad048e45ee7a2e732b8e1da697ff7
Klaudia Biala <klaudia.biala@polcode.net>
2022-12-09 11:19:11 +0100

    Add Brian to CC

--- app/mailers/contact_request_mailer.rb
+++ app/mailers/contact_request_mailer.rb
@@ -1,7 +1,7 @@
 class ContactRequestMailer < ApplicationMailer
   def new_contact_request_notice(admins)
     return nil unless admins.any?
-    
+
     admins.each do |admin|
       mail(to: admin.email, subject: 'New contact request')
     end
@@ -9,6 +9,8 @@ class ContactRequestMailer < ApplicationMailer
 
   def new_opt_out_request_notice(request_id)
     @opt_out_request = OptOutRequest.find(request_id)
-    mail(to: ADMIN_EMAIL, subject: 'New opt-out request on www.fmadata.com')
+    mail(to: ADMIN_EMAIL,
+         cc: BRIAN_EMAIL,
+         subject: 'New opt-out request on www.validators.app')
   end
 end
