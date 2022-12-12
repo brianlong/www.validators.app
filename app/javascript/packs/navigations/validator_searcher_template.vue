43ac5a25bb7050378a2c13af351bb2c5e90efc18
ktarnowski <kam.tarnowski@gmail.com>
2022-12-12 14:08:44 +0100

    fixed redirect

--- app/javascript/packs/navigations/validator_searcher_template.vue
+++ app/javascript/packs/navigations/validator_searcher_template.vue
@@ -2,15 +2,13 @@
   <div>
     <b-form @submit="onSubmit" class="flex-fill">
       <div class="form-group-row">
-        <b-input-group id="input-group-1">
-          <b-form-input id="input-1"
-                        v-model="form.query"
+        <b-input-group>
+          <b-form-input v-model="form.query"
                         placeholder="Starts with... (validator name, validator account or data center key)"
-                        :value="query"
                         required>
           </b-form-input>
           <b-input-group-append>
-            <b-button class="btn-sm btn-primary">Search</b-button>
+            <b-button variant="primary" class="btn-sm btn-radius-right">Search</b-button>
           </b-input-group-append>
         </b-input-group>
       </div>
@@ -19,27 +17,27 @@
 </template>
 
 <script>
-import { mapGetters } from 'vuex'
+  import { mapGetters } from 'vuex'
 
-export default {
-  data() {
-    return {
-      form: {
-        query: null
+  export default {
+    data() {
+      return {
+        form: {
+          query: ''
+        }
+      }
+    },
+    created() { },
+    computed: {
+      ...mapGetters([
+        'network'
+      ])
+    },
+    methods: {
+      onSubmit(event) {
+        event.preventDefault()
+        window.location.replace(`validators?q=${this.form.query}&network=${this.network}`)
       }
     }
-  },
-  created() { },
-  computed: {
-    ...mapGetters([
-      'network'
-    ])
-  },
-  methods: {
-    onSubmit(event) {
-      event.preventDefault()
-      alert(JSON.stringify(this.form))
-    }
   }
-}
 </script>
