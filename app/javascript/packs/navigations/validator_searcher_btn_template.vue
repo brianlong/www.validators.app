5ba80b6d4550bd1da837a24bdff770ebfa286e40
ktarnowski <kam.tarnowski@gmail.com>
2022-12-13 16:30:50 +0100

    add validator searcher btn components

---
+++ app/javascript/packs/navigations/validator_searcher_btn_template.vue
@@ -0,0 +1,34 @@
+<template>
+  <button class="btn btn-lg btn-secondary"
+          id="validator-searcher-btn"
+          :class="searcher_class()"
+          v-on:click="trigger_btn()">
+    <i class="fas fa-search"></i>
+  </button>
+</template>
+
+<script>
+  export default {
+    data() {
+      return {
+        is_active: false
+      }
+    },
+    methods: {
+      searcher_class() {
+        return this.is_active ? 'active' : ''
+      },
+      trigger_btn() {
+        this.is_active = !this.is_active
+
+        const validator_searcher_bar = document.getElementById("validator-searcher-bar")
+
+        if (this.is_active) {
+          validator_searcher_bar.classList.add("active")
+        } else {
+          validator_searcher_bar.classList.remove("active")
+        }
+      }
+    }
+  }
+</script>
