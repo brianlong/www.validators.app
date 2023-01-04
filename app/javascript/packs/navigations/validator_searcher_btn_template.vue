97772f20f2b7b8563602dacbc7e5b2d887ea9b27
Kamil Tarnowski <kam.tarnowski@gmail.com>
2023-01-04 20:07:36 +0100

    180722100 - provide searcher on every page (#685)
    * validator searcher component created
    
    * fixed redirect
    
    * add validator searcher btn components
    
    * improvements to validator searcher bar component
    
    * address cr issue
    
    * fix invalid redirection
    
    * Remove not necessary class
    
    * Fix button class and styling
    
    * Extract class no related to search bar
    
    * Remove not needed code and use more appropriate method names
    
    * Add classes for mobiles
    
    * Clean up styling
    
    * Fix broken styling
    
    * Increase space
    
    Co-authored-by: Klaudia Biala <klaudia.biala@polcode.net>

---
+++ app/javascript/packs/navigations/validator_searcher_btn_template.vue
@@ -0,0 +1,26 @@
+<template>
+  <button :class="'btn btn-lg btn-secondary ms-lg-3 mt-3 mt-lg-0 ' + searcher_btn_class()"
+          v-on:click="toggle_searcher()">
+    <i class="fas fa-search"></i>
+  </button>
+</template>
+
+<script>
+  export default {
+    data() {
+      return {
+        is_active: false
+      }
+    },
+
+    methods: {
+      searcher_btn_class() {
+        return this.is_active ? 'active' : ''
+      },
+      toggle_searcher() {
+        this.is_active = !this.is_active
+        document.getElementById("searcher-bar").classList.toggle("d-none")
+      }
+    }
+  }
+</script>
