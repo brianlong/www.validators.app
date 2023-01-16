e4ce41438d8c12c5baac734f7c467c79fac14f7f
ktarnowski <kam.tarnowski@gmail.com>
2023-01-16 16:02:18 +0100

    create validator score modal component; place component in index action

---
+++ app/javascript/packs/validators/validator_score_modal.js
@@ -0,0 +1,21 @@
+import Vue from "vue/dist/vue.esm"
+import TurbolinksAdapter from "vue-turbolinks"
+import store from "../stores/main_store.js"
+import ValidatorScoreModal from "./validator_score_modal.vue"
+
+Vue.use(TurbolinksAdapter)
+
+document.addEventListener("turbolinks:load", () => {
+  new Vue({
+    el: "#validator-score-modal",
+    store,
+    render(createElement) {
+      return createElement(ValidatorScoreModal, {
+        props: {
+          validator: JSON.parse(this.$el.attributes.validator.value)
+        }
+      })
+    }
+  })
+})
+
