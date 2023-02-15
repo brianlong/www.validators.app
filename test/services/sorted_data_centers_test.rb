651e6a6d413dd903232883bd35bd1da31ebb359f
maciejkocylapc <80820353+maciejkocylapc@users.noreply.github.com>
2023-02-15 12:41:06 +0100

    184376335-filter-data-centers-by-inactive (#724)
    * exclude inactive from query
    
    * reduce inactive validators for show pages

--- test/services/sorted_data_centers_test.rb
+++ test/services/sorted_data_centers_test.rb
@@ -80,9 +80,33 @@ class SortedDataCenterTest < ActiveSupport::TestCase
       scores_berlin << score
     end
 
+    2.times do
+      ip_address = Faker::Internet.ip_v4_address
+      validator = create(:validator, is_active: false)
+      data_center_host = create(:data_center_host, data_center: data_center_berlin)
+
+      validator_ip = create(
+        :validator_ip,
+        :active,
+        address: ip_address, 
+        validator: validator,
+        data_center_host: data_center_host
+      )
+
+      score = create(
+        :validator_score_v1,
+        ip_address: ip_address,
+        active_stake: 100,
+        network: "testnet",
+        validator: validator
+      )
+
+      scores_berlin << score
+    end
+
     scores_china.first.update(delinquent: true)
+    scores_china.last.update(delinquent: true)
     scores_berlin.first.update(delinquent: true)
-    scores_berlin.last.update(delinquent: true)
   end
 
   test "when sort_by_asn returns correct result" do
@@ -95,8 +119,8 @@ class SortedDataCenterTest < ActiveSupport::TestCase
     assert_equal 800, result[:total_stake]
     assert_equal 2, result[:results][0][1][:data_centers].count
     assert_equal 3, result[:total_delinquent]
-    assert_equal 2, result[:results][0][1][:delinquent_validators] # for traits_autonomous_system_number: 12345
-    assert_equal 1, result[:results][1][1][:delinquent_validators] # for traits_autonomous_system_number: 54321
+    assert_equal 1, result[:results][0][1][:delinquent_validators] # for traits_autonomous_system_number: 12345
+    assert_equal 2, result[:results][1][1][:delinquent_validators] # for traits_autonomous_system_number: 54321
 
   end
 
@@ -110,8 +134,7 @@ class SortedDataCenterTest < ActiveSupport::TestCase
     assert_equal 800, result[:total_stake]
     assert_equal 3, result[:results].count
     assert_equal 3, result[:total_delinquent]
-    assert_equal 1, result[:results][0][1][:delinquent_validators] # 12345-CN-Asia/Shanghai
-    assert_equal 1, result[:results][1][1][:delinquent_validators] # 54321-DE-Europe/CET
+    assert_equal 2, result[:results][0][1][:delinquent_validators] # 12345-CN-Asia/Shanghai
     assert_equal 1, result[:results][2][1][:delinquent_validators] # 12345-DE-Europe/CET
   end
 end
