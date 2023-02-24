44477d0ea7b11672c2fb54014b1ce87ef0dfb33f
Klaudia Biala <klaudia.biala@polcode.net>
2023-02-24 15:59:33 +0100

    Use redis for as app cache store (#736)

--- config/environments/development.rb
+++ config/environments/development.rb
@@ -26,18 +26,18 @@ Rails.application.configure do
   config.consider_all_requests_local = true
 
   # Enable/disable caching. By default caching is disabled.
-  # Run rails dev:cache to toggle caching.
+  # Run rails dev:cache to toggle caching in dev environment.
+  # caching-dev.txt file is created by rails dev:cache command.
   if Rails.root.join('tmp', 'caching-dev.txt').exist?
     config.action_controller.perform_caching = true
-    config.action_controller.enable_fragment_cache_logging = true
-
-    config.cache_store = :memory_store
-    config.public_file_server.headers = {
-      'Cache-Control' => "public, max-age=#{2.days.to_i}"
+    config.cache_store = :redis_cache_store, { url: Rails.application.credentials.dig(:redis, :url),
+                                               connect_timeout:    30,  # Defaults to 20 seconds
+                                               read_timeout:       0.5, # Defaults to 1 second
+                                               write_timeout:      0.5, # Defaults to 1 second
+                                               reconnect_attempts: 1,   # Defaults to 0
     }
   else
     config.action_controller.perform_caching = false
-
     config.cache_store = :null_store
   end
   config.action_mailer.perform_caching = false
