be981fcd27c6ac072426869ae1a2ee153aa42791
ktarnowski <kam.tarnowski@gmail.com>
2023-03-08 11:10:42 +0100

    add default timestamps to validators

--- script/update_validator_stake_pools.rb
+++ script/update_validator_stake_pools.rb
@@ -3,12 +3,10 @@
 require File.expand_path('../config/environment', __dir__)
 
 stake_pool_attrs = Validator.includes(stake_accounts: :stake_pool)
-                            .select(:id, :created_at, :updated_at)
+                            .select(:id)
                             .map do |v|
   {
     id: v.id,
-    created_at: v.created_at,
-    updated_at: v.updated_at,
     stake_pool_list: v.stake_accounts.map { |sa| sa.stake_pool_valid&.name }.compact.uniq
   }
 end
