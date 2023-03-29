a79ffa826c13ee84827a93a14d398045e1c787bf
maciejkocylapc <maciej.kocyla@polcode.net>
2023-03-29 12:44:22 +0200

    Revert "script removes users without sign in after 7 days"
    This reverts commit 020f8d4f58711229445d0c265fb15d8e246e3015.

--- test/scripts/remove_unconfirmed_users_test.rb
+++ test/scripts/remove_unconfirmed_users_test.rb
@@ -7,24 +7,14 @@ class RemoveUnconfirmedUsersTest < ActiveSupport::TestCase
     @user1 = create(:user, created_at: 6.days.ago, confirmed_at: nil, email: "user1@test.com")
     @user2 = create(:user, created_at: 8.days.ago, confirmed_at: DateTime.now, email: "user2@test.com")
     @user3 = create(:user, created_at: 8.days.ago, confirmed_at: nil, email: "user3@test.com")
-    @user4 = create(
-      :user,
-      created_at: 8.days.ago,
-      confirmed_at: DateTime.now,
-      email: "user4@test.com",
-      last_sign_in_at: nil
-    )
   end
 
-  test "script deletes unconfirmed \
-        and users who never logged in \
-        after 7 days" do
+  test "script deletes unconfirmed users after 7 days" do
     load(Rails.root.join("script", "remove_unconfirmed_users.rb"))
 
     assert_equal 2, User.count
     assert User.all.include? @user1
     assert User.all.include? @user2
     refute User.all.include? @user3
-    refute User.all.include? @user4
   end
 end
