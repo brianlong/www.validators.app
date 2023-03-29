020f8d4f58711229445d0c265fb15d8e246e3015
maciejkocylapc <maciej.kocyla@polcode.net>
2023-03-29 12:38:51 +0200

    script removes users without sign in after 7 days

--- script/remove_unconfirmed_users.rb
+++ script/remove_unconfirmed_users.rb
@@ -5,3 +5,7 @@ require File.expand_path('../config/environment', __dir__)
 User.where("created_at < ?", User::CONFIRMATION_TIME.ago)
     .where(confirmed_at: nil)
     .destroy_all
+    
+User.where("created_at < ?", User::CONFIRMATION_TIME.ago)
+    .where(last_sign_in_at: nil)
+    .destroy_all
