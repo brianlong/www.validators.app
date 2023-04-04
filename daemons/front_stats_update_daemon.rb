ac327e85100933c7e9515dd2546c8a02d6b0f78f
ktarnowski <kam.tarnowski@gmail.com>
2023-04-04 16:29:10 +0200

    expose db sol price in daemon; revert changes to front end

--- daemons/front_stats_update_daemon.rb
+++ daemons/front_stats_update_daemon.rb
@@ -19,16 +19,29 @@ def get_latest_cluster_stats
   }
 end
 
+def fetch_db_sol_price
+  prices = SolPrice.last(2)
+  previous_sol_price, sol_price = prices.first, prices.last
+  change_24h = -(previous_sol_price.average_price - sol_price.average_price)
+
+  {
+    "solana" => {
+      "usd" => sol_price.average_price,
+      "usd_24h_vol" => sol_price.volume,
+      "usd_24h_change" => change_24h
+    }
+  }
+end
+
 def update_prices
   @coin_gecko_response = begin
     CoinGeckoClient.new.price
   rescue CoinGeckoClient::CoinGeckoResponseError
-    @coin_gecko_response || {}
+    fetch_db_sol_price
   end
 end
 
 @last_cluster_stats = get_latest_cluster_stats
-update_prices
 
 loop do
   begin
@@ -39,16 +52,15 @@ loop do
       @counter = 0
       @last_cluster_stats = get_latest_cluster_stats
       @software_versions = get_versions_for_networks
-      update_prices
     end
 
-    # Data updates | each 10 seconds
-    if @counter % 2 == 0
+    # Data updates | each 30 seconds
+    if @counter % 6 == 0
       update_prices
     end
 
     # Data updates | each 5 seconds
-    parsed_response = @coin_gecko_response
+    parsed_response = @coin_gecko_response || fetch_db_sol_price
     parsed_response["cluster_stats"] = @last_cluster_stats
     
     broadcast_software_versions(@software_versions)
