dad4a8a068c5c5b09801195304996ecc61e46e7c
Klaudia Biala <klaudia.biala@polcode.net>
2023-04-07 16:06:37 +0200

    Rewrite scores modal trigger from jquery to JS

--- app/javascript/src/score_modal_trigger.js
+++ app/javascript/src/score_modal_trigger.js
@@ -1,6 +1,9 @@
 document.addEventListener("turbolinks:load", function() {
-  $('.modal-trigger').on('click', function (e) {
-    let account = $(this).data()["account"]
-    window.scores_modal.$children[0].update_account(account)
+  document.querySelectorAll(".modal-trigger").forEach(modal_trigger => {
+    modal_trigger.addEventListener("click", function(event) {
+      event.preventDefault();
+      let account = modal_trigger.dataset.account;
+      window.scores_modal.$children[0].update_account(account);
+    })
   })
 });
