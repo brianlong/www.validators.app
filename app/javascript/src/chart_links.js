f829320d30f583af16d688275c522144a99175ab
Klaudia Biala <klaudia.biala@polcode.net>
2023-04-12 20:36:55 +0200

    Tweak syntax

--- app/javascript/src/chart_links.js
+++ app/javascript/src/chart_links.js
@@ -1,23 +1,20 @@
 document.addEventListener("turbolinks:load", function() {
-  let links = document.getElementsByClassName("chart-link");
-  Array.prototype.forEach.call(links, link => {
+  document.querySelectorAll(".chart-link").forEach(link => {
     link.addEventListener("click", event => {
       event.preventDefault();
-      var i = link.dataset.iterator
-      var target = link.dataset.bsTarget+'-'+i;
-      var row = document.getElementById("row-"+i);
+      let i = link.dataset.iterator;
+      let target = link.dataset.bsTarget+'-'+i;
+      let row = document.getElementById("row-"+i);
 
       // Show selected chart, hide the rest
-      var charts = row.getElementsByClassName("column-chart");
-      Array.prototype.forEach.call(charts, chart => {
+      row.querySelectorAll(".column-chart").forEach(chart => {
         chart.classList.add("d-none");
       })
       document.getElementById(target).classList.remove("d-none");
 
       // Set active link
-      var ls = row.getElementsByClassName("chart-link");
-      Array.prototype.forEach.call(ls, l => {
-        l.classList.remove("active")
+      row.querySelectorAll(".chart-link").forEach(l => {
+        l.classList.remove("active");
       });
       link.classList.add("active");
     })
