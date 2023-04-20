aa0f2b528ead33442c625afef45ca25c38881e8c
ktarnowski <kam.tarnowski@gmail.com>
2023-04-20 13:53:09 +0200

    use db epoch duration in cluster stat service

--- app/services/create_cluster_stats_service.rb
+++ app/services/create_cluster_stats_service.rb
@@ -11,8 +11,6 @@ class CreateClusterStatsService
     nodes_total = GossipNode.where(network: @network).active.not_staked.count
     dominant_software_version = Batch.last_scored(@network).software_version
 
-    network_stat = ClusterStat.find_or_create_by(network: @network)
-
     fields_for_update = {}
     fields_for_update[:roi] = epochs_annual_roi if epochs_annual_roi > 0
     fields_for_update[:total_active_stake] = \
@@ -36,17 +34,22 @@ class CreateClusterStatsService
 
   private
 
+  def network_stat
+    @network_stat ||= ClusterStat.find_or_create_by(network: @network)
+  end
+
   def epochs_annual_roi
     if last_epochs.count > 0
-      epoch_duration = (last_epochs.first.created_at - last_epochs.second.created_at)
-      return 0 unless epoch_duration.to_f > 0
-      epoch_average_roi = last_epochs.map do |epoch|
-        if epoch.total_rewards && epoch.total_active_stake
-          (epoch.total_rewards / epoch.total_active_stake.to_f) * 100
-        end
-      end.compact&.average.to_f
+      epoch_average_roi =
+        last_epochs.map do |epoch|
+          if epoch.total_rewards && epoch.total_active_stake
+            (epoch.total_rewards / epoch.total_active_stake.to_f) * 100
+          end
+        end.compact&.average.to_f
       return 0 unless epoch_average_roi > 0
-      epochs_annual_roi = (1.year.to_i / epoch_duration.to_f) * epoch_average_roi
+
+      epochs_annual_roi =
+        (1.year.to_i / network_stat.epoch_duration) * epoch_average_roi
     end
     epochs_annual_roi ||= 0
   end
