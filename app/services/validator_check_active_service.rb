efa9f875d2ac915f1e01c7e081789a307a5a0412
maciejkocylapc <80820353+maciejkocylapc@users.noreply.github.com>
2023-05-04 13:01:07 +0200

    184866369 cleanup validator check active service (#807)
    * check if validator is not too young
    
    * refactor ValidatorCheckActiveService
    
    * remove redundant methods

--- app/services/validator_check_active_service.rb
+++ app/services/validator_check_active_service.rb
@@ -10,42 +10,22 @@ class ValidatorCheckActiveService
 
   def update_validator_activity
     Validator.includes(:vote_accounts, :validator_score_v1).find_each do |validator|
-      if should_be_destroyed?(validator)
-        validator.update(is_active: false, is_destroyed: true)
-      elsif validator.scorable?
-        update_scorable(validator)
+      # Skip if validator has no history that is older than DELINQUENT_TIME
+      next if too_young?(validator)
+
+      # mark validator as destroyed if it has no recent history
+      if validator_histories_for_validator(validator).empty?
+        validator.assign_attributes(is_active: false, is_destroyed: true)
       else
-        # Check validators that are currently not scorable in case they reactivate
-        if acceptable_stake?(validator) && !delinquent?(validator)
-          validator.update(is_active: true, is_destroyed: false)
-        end
-        
-        # Check if vote account has been created for rpc_node
-        if validator.vote_accounts.exists?
-          validator.update(is_rpc: false) if validator.is_rpc
-        end
+        validator.assign_attributes(is_destroyed: false)
+        assign_validator_flags(validator)
       end
+
+      validator.save if validator.changed?
     end
   end
 
   private
-
-  def should_be_destroyed?(validator)
-    if !validator.validator_histories.exists? || \
-      (!validator.validator_histories.where("created_at > ?", @delinquent_time.ago).exists? && \
-       validator.validator_histories.where("created_at < ?", @delinquent_time.ago).exists?)
-      return true
-    end
-
-    false
-  end
-
-  def current_epoch(network)
-    @current_epochs ||= {}
-    return @current_epochs[network] if @current_epochs[network].present?
-
-    @current_epochs[network] = EpochWallClock.where(network: network).order(created_at: :desc).first
-  end
 
   # returns true if validator has no history from previous epoch
   def too_young?(validator)
@@ -54,37 +34,32 @@ class ValidatorCheckActiveService
               .exists?
   end
 
-  def became_inactive?(validator)
-    if ValidatorHistory.where(account: validator.account).exists?
-      unless acceptable_stake?(validator) && !delinquent?(validator)
-        return true
-      end
-    end
+  def validator_histories_for_validator(validator)
+    ValidatorHistory.where(account: validator.account)
+                    .where("created_at > ?", @delinquent_time.ago)
+                    .order(created_at: :desc).to_a
+  end
 
+  def should_be_active?(validator)
+    return true if acceptable_stake?(validator) && !long_time_delinquent?(validator)
     false
   end
 
-  def is_rpc?(validator)
-    if validator.created_at < (DateTime.now - @delinquent_time) && \
-       !validator.vote_accounts.exists?
-      return true
-    end
-
-    false
+  def should_be_rpc?(validator)
+    !validator.vote_accounts.exists?
   end
 
-  def update_scorable(validator)
-    if !too_young?(validator) && became_inactive?(validator)
-      validator.update(is_active: false)
-    end
-
-    validator.update(is_rpc: true) if is_rpc?(validator)
+  def assign_validator_flags(validator)
+    validator.assign_attributes(
+      is_active: should_be_active?(validator),
+      is_rpc: should_be_rpc?(validator)
+    )
   end
 
-  def delinquent?(validator)
+  def long_time_delinquent?(validator)
     return false if !validator.delinquent?
 
-    !non_delinquent_history_exists?(validator)
+    validator_histories_for_validator(validator).select { |history| !history.delinquent? }.empty?
   end
 
   # returns true if if validator had stake gt STAKE_EXCLUDE_HEIGHT since DELINQUENT_TIME
@@ -97,11 +72,5 @@ class ValidatorCheckActiveService
                                             )
 
     with_acceptable_stake.exists?
-  end
-
-  def non_delinquent_history_exists?(validator)
-    ValidatorHistory.where(account: validator.account, delinquent: false)
-                    .where("created_at > ?", @delinquent_time.ago)
-                    .exists?
   end
 end
