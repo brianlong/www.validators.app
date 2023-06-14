0fff12f943e7ec211287d3106790e3edfc1f4ced
Kamil Tarnowski <kam.tarnowski@gmail.com>
2023-06-14 15:37:14 +0200

    185302616 - fix saving Authorized Voters change (#826)
    * update callback saving changes for authorized_voters
    
    * fix logic for case with multiple keys in authorized_voters
    
    * address case if authorized_voters is nil
    
    * add case for update with nil
    
    * add script in order to remove invalid authorized_voters histories
    
    * do not update authorize_withdrawer in script
    
    * address cr issues; add tests
    
    * apply sorting when comparing authorized_voters

--- test/models/vote_account_test.rb
+++ test/models/vote_account_test.rb
@@ -1,10 +1,6 @@
 require 'test_helper'
 
 class VoteAccountTest < ActiveSupport::TestCase
-  setup do
-    @vote_account = create(:vote_account)
-  end
-
   test "scope for_api returs vote_accounts specified fields" do
     assert_equal VoteAccount::FIELDS_FOR_API.map(&:to_s), VoteAccount.for_api.first.attributes.keys
   end
@@ -19,13 +15,133 @@ class VoteAccountTest < ActiveSupport::TestCase
     assert_equal va_histories.last.authorized_withdrawer_after, "test_withdrawer_2"
   end
 
-  test "authorized_voters" do
-    vote_account = create(:vote_account, authorized_voters: { "test_voter_key" => "test_voter_value" })
-    vote_account.update(authorized_voters: { "test_voter_key_2" => "test_voter_value_2" })
+  class AuthorizedVotersTests < ActiveSupport::TestCase
+    test "returns valid history attributes when authorization has changed" do
+      vote_account = create(:vote_account, authorized_voters: { "test_voter_key" => "test_voter_value" })
+      vote_account.update(authorized_voters: { "test_voter_key_2" => "test_voter_value_2" })
 
-    va_histories = vote_account.account_authority_histories
+      va_history = vote_account.account_authority_histories.last
 
-    assert_equal va_histories.last.authorized_voters_before, { "test_voter_key" => "test_voter_value" }
-    assert_equal va_histories.last.authorized_voters_after, { "test_voter_key_2" => "test_voter_value_2" }
+      assert_equal va_history.authorized_voters_before, { "test_voter_key" => "test_voter_value" }
+      assert_equal va_history.authorized_voters_after, { "test_voter_key_2" => "test_voter_value_2" }
+    end
+
+    test "returns valid history attributes when new authorization has been added" do
+      vote_account = create(
+        :vote_account,
+        authorized_voters: { "test_voter_key" => "test_voter_value" }
+      )
+      vote_account.update(
+        authorized_voters: {
+          "test_voter_key" => "test_voter_value",
+          "test_voter_key_2" => "test_voter_value_2"
+        }
+      )
+
+      va_history = vote_account.account_authority_histories.last
+      assert_equal va_history.authorized_voters_before, {
+        "test_voter_key" => "test_voter_value"
+      }
+      assert_equal va_history.authorized_voters_after, {
+        "test_voter_key" => "test_voter_value",
+        "test_voter_key_2" => "test_voter_value_2"
+      }
+    end
+
+    test "returns valid history attributes when authorizations have keys changed only" do
+      vote_account = create(
+        :vote_account,
+        authorized_voters: {
+          "test_voter_key" => "test_voter_value",
+          "test_voter_key_2" => "test_voter_value_2"
+        }
+      )
+      vote_account.update(
+        authorized_voters: {
+          "test_voter_key_2" => "test_voter_value",
+          "test_voter_key_3" => "test_voter_value_2"
+        }
+      )
+
+      va_history = vote_account.account_authority_histories.last
+      assert_nil va_history.authorized_voters_before
+      assert_equal va_history.authorized_voters_after, {
+        "test_voter_key" => "test_voter_value",
+        "test_voter_key_2" => "test_voter_value_2"
+      }
+    end
+
+    test "skips saving history if authorizations do not change" do
+      vote_account = create(:vote_account, authorized_voters: nil)
+      vote_account.update(authorized_voters: nil)
+
+      assert_empty vote_account.account_authority_histories
+    end
+
+    test "returns valid history attributes when authorizations have changed to nil" do
+      vote_account = create(
+        :vote_account,
+        authorized_voters: {
+          "test_voter_key" => "test_voter_value",
+          "test_voter_key_2" => "test_voter_value_2"
+        }
+      )
+      vote_account.update(authorized_voters: nil)
+
+      va_history = vote_account.account_authority_histories.last
+      assert_equal va_history.authorized_voters_before, {
+        "test_voter_key" => "test_voter_value",
+        "test_voter_key_2" => "test_voter_value_2"
+      }
+      assert_nil va_history.authorized_voters_after
+    end
+
+    test "returns valid history attributes when an authorization has been removed" do
+      vote_account = create(
+        :vote_account,
+        authorized_voters: {
+          "test_voter_key" => "test_voter_value",
+          "test_voter_key_2" => "test_voter_value_2"
+        }
+      )
+      vote_account.update(
+        authorized_voters: {
+          "test_voter_key_2" => "test_voter_value_2"
+        }
+      )
+
+      va_history = vote_account.account_authority_histories.last
+      assert_equal va_history.authorized_voters_before, {
+        "test_voter_key" => "test_voter_value",
+        "test_voter_key_2" => "test_voter_value_2"
+      }
+      assert_equal va_history.authorized_voters_after, {
+        "test_voter_key_2" => "test_voter_value_2"
+      }
+    end
+
+    test "returns valid history attributes when authorizations have been mixed up" do
+      vote_account = create(
+        :vote_account,
+        authorized_voters: {
+          "test_voter_key" => "test_voter_value",
+          "test_voter_key_2" => "test_voter_value_2"
+        }
+      )
+      vote_account.update(
+        authorized_voters: {
+          "test_voter_key_2" => "test_voter_value_2",
+          "test_voter_key" => "test_voter_value"
+        }
+      )
+
+      va_history = vote_account.account_authority_histories.last
+
+      assert_nil va_history.authorized_voters_before
+      assert_equal va_history.authorized_voters_after, {
+        "test_voter_key" => "test_voter_value",
+        "test_voter_key_2" => "test_voter_value_2"
+      }
+    end
   end
 end
