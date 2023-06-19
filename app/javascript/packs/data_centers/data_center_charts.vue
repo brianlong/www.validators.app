54c433c560cbc95fdb3b1ee743e7d76f36bbd073
Klaudia Biala <klaudia.biala@polcode.net>
2023-06-19 22:23:49 +0200

    Move DC stats to 2 cards

--- app/javascript/packs/data_centers/data_center_charts.vue
+++ app/javascript/packs/data_centers/data_center_charts.vue
@@ -1,26 +1,27 @@
 <template>
   <div class="row">
-    <div class="col-12">
+    <div class="col-md-6 mb-4">
       <div class="card h-100">
-        <div class="card-header text-center">
-          <h3 class="card-title">Data Center Stats</h3>
+        <div class="card-content">
+          <h2 class="h4 card-heading">Stats By Country</h2>
+            <div class="px-lg-3 px-xl-4">
+              <pie-chart
+                  :data_center_stats="data_centers['dc_by_country']"
+                  :chart_title="'Data Centers by Country'"
+                  v-if="data_centers['dc_by_country']"></pie-chart>
+          </div>
         </div>
-        <div class="card-body">
-          <div class="row">
-            <div class="col-md-6">
-              <pie-chart 
-                :data_center_stats="data_centers['dc_by_country']"
-                :chart_title="'Data Centers by Country'"
-                v-if="data_centers['dc_by_country']"
-              ></pie-chart>
-            </div>
-            <div class="col-md-6">
-              <pie-chart 
+      </div>
+    </div>
+    <div class="col-md-6 mb-4">
+      <div class="card h-100">
+        <div class="card-content">
+          <h2 class="h4 card-heading">Stats By Organization</h2>
+          <div class="px-lg-3 px-xl-4">
+            <pie-chart
                 :data_center_stats="data_centers['dc_by_organization']"
                 :chart_title="'Data Centers by Organization'"
-                v-if="data_centers['dc_by_organization']"
-              ></pie-chart>
-            </div>
+                v-if="data_centers['dc_by_organization']"></pie-chart>
           </div>
         </div>
       </div>
