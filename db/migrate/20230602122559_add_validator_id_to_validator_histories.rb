a969b66179232c3fd6d3c63561f6760be31cfc58
ktarnowski <kam.tarnowski@gmail.com>
2023-06-26 12:37:26 +0200

    disable lock on the migration; add disable_ddl_transaction

--- db/migrate/20230602122559_add_validator_id_to_validator_histories.rb
+++ db/migrate/20230602122559_add_validator_id_to_validator_histories.rb
@@ -1,8 +1,15 @@
 # frozen_string_literal: true
 
 class AddValidatorIdToValidatorHistories < ActiveRecord::Migration[6.1]
+  disable_ddl_transaction!
+
   def change
     add_column :validator_histories, :validator_id, :integer, null: true, foreign_key: true
-    add_index :validator_histories, :validator_id
+    execute <<~SQL
+      ALTER TABLE validator_histories
+      ADD INDEX index_validator_histories_on_validator_id(validator_id),
+      ALGORITHM=DEFAULT,
+      LOCK=NONE;
+    SQL
   end
 end
