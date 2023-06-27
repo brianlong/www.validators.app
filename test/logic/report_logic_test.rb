44b3fe5d8bfba2e863bfd0e21e98dc63faab04fd
Kamil Tarnowski <kam.tarnowski@gmail.com>
2023-06-27 09:28:04 +0200

    185455392 - Fix front_stats_update service (#830)
    * return 0 instead of nil when generate software versions
    
    * change logic to skip software versions with 0 as active stake
    
    * update test
    
    * refactor ReportLogicTest
    
    * reduce ReportLogicTest lines size
    
    * fix linter
    
    * include validators in report with 0 active_stake
    
    * fixed test description
    
    * small refactor

--- test/logic/report_logic_test.rb
+++ test/logic/report_logic_test.rb
@@ -1,7 +1,7 @@
 # frozen_string_literal: true
 
-require 'test_helper'
-require 'sidekiq/testing'
+require "test_helper"
+require "sidekiq/testing"
 
 # ReportLogicTest
 class ReportLogicTest < ActiveSupport::TestCase
@@ -9,99 +9,94 @@ class ReportLogicTest < ActiveSupport::TestCase
   include SolanaLogic
 
   def setup
-    # Create our initial payload with the input values
-    @initial_payload = {
-      # config_urls: Rails.application.credentials.solana[:testnet_urls],
-      config_urls: [
-        'https://api.testnet.solana.com',
-        'http://165.227.100.142:8899',
-        'http://127.0.0.1:8899'
-      ],
-      network: 'testnet'
-    }
+    @network = "mainnet"
+    @batch = create(:batch, @network)
   end
 
-  test "report_software_versions creates correct report and ignores empty and 'unknown' software_versions" do
-    network = 'mainnet'
-    batch = create(:batch, :mainnet)
+  def generate_report
+    payload = { network: @network, batch_uuid: @batch.uuid }
+    Pipeline.new(200, payload).then(&report_software_versions)
+  end
 
-    batch_uuid = batch.uuid
-
-    software_versions = ['1.6.8', '1.7.1', '1.7.2']
-    validator_lists = []
-
+  test "report_software_versions creates correct report with software versions included" do
+    software_versions = ["1.6.8", "1.7.1", "1.7.2"]
     software_versions.each do |sw|
-      list = create_list(:validator, 5, network: network)
-      list.each do |val|
-        create(
-          :validator_score_v1, 
-          validator: val, 
-          software_version: sw,
-          network: network
-        )
-      end
-
-      validator_lists << list
-    end
-
-    # Setup score with empty software version, should be ignored in the report
-    val = create(:validator)
-    create(
-      :validator_score_v1, 
-      validator: val, 
-      software_version: '',
-      network: network
-    )
-
-    # Setup score with 'unknown' software version, should be ignored in the report
-    val_unknown = create(:validator)
-    create(
-      :validator_score_v1, 
-      validator: val, 
-      software_version: 'unknown',
-      network: network
-    )
-
-    # Setup score with malformed software version, should be ignored in the report
-    val_malformed = create(:validator)
-    create(
-      :validator_score_v1, 
-      validator: val, 
-      software_version: '1.3.9 e45f1df5',
-      network: network
-    )
-
-    validator_lists.each do |list|
-      list.each do |val|
-        va = create(:vote_account, validator: val)
-        create(:vote_account_history, vote_account: va, batch_uuid: batch_uuid, network: network)
+      list = create_list(:validator, 5, network: @network)
+      list.each do |validator|
+        create(:validator_score_v1, validator: validator, software_version: sw)
       end
     end
 
-    va_unknown = create(:vote_account, validator: val_unknown)
-    create(:vote_account_history, vote_account: va_unknown, batch_uuid: batch_uuid, network: network)
-  
-    va_malformed = create(:vote_account, validator: val_malformed)
-    create(:vote_account_history, vote_account: va_malformed, batch_uuid: batch_uuid, network: network)
+    generate_report
+    report = Report.find_by(name: "report_software_versions", batch_uuid: @batch.uuid)
+    expected_result = { "count" => 5, "stake_percent" => 33.33 }
 
-    payload = {
-      network: network,
-      batch_uuid: batch_uuid
-    }
-
-    _p = Pipeline.new(200, payload)
-                .then(&report_software_versions)
-
-    report = Report.find_by(name: 'report_software_versions', batch_uuid: batch_uuid)     
-    
-    expected_result = {"count"=>5, "stake_percent"=>33.33}
-
-    assert_equal network, report.network
-    assert_equal batch_uuid, report.batch_uuid
+    assert_equal @network, report.network
+    assert_equal @batch.uuid, report.batch_uuid
     assert_equal 3, report.payload.size # empty and unknown software_versions are ignored
 
     software_versions.each do |sw|
       assert_equal report.payload.find { |v| v[sw] }[sw], expected_result
     end
+  end
+
+  test "report_software_versions creates a report when active_stake sum is zero" do
+    validator = create(:validator, network: @network)
+    create(
+      :validator_score_v1,
+      software_version: "1.5.7",
+      validator: validator,
+      active_stake: 0
+    )
+
+    generate_report
+    report = Report.find_by(name: "report_software_versions", batch_uuid: @batch.uuid)
+
+    assert_equal 1, report.payload.size
+    assert report.payload.first.keys, ["1.5.7"]
+  end
+
+  test "does not create a report if active_stake sum is nil" do
+    validator = create(:validator, network: @network)
+    create(:validator_score_v1,
+      software_version: "1.5.7",
+      validator: validator,
+      active_stake: nil
+    )
+
+    generate_report
+    report = Report.find_by(name: "report_software_versions", batch_uuid: @batch.uuid)
+
+    assert_nil report
+  end
+
+  test "does not create a report if software version is malformed" do
+    validator = create(:validator, network: @network)
+    create(:validator_score_v1, validator: validator, software_version: "1.3.9 e45f1df5")
+
+    generate_report
+    report = Report.find_by(name: "report_software_versions", batch_uuid: @batch.uuid)
+
+    assert_nil report
+  end
+
+  test "does not create a report if software version is unknown" do
+    validator = create(:validator, network: @network)
+    create(:validator_score_v1, validator: validator, software_version: "unknown")
+
+    generate_report
+    report = Report.find_by(name: "report_software_versions", batch_uuid: @batch.uuid)
+
+    assert_nil report
+  end
+
+  test "does not create a report if software version is empty" do
+    validator = create(:validator, network: @network)
+    create(:validator_score_v1, validator: validator, software_version: "")
+
+    generate_report
+    report = Report.find_by(name: "report_software_versions", batch_uuid: @batch.uuid)
+
+    assert_nil report
   end
 end
