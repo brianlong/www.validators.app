0818758ba672d4035635facfce33c39e5c18b309
ktarnowski <kam.tarnowski@gmail.com>
2023-06-28 12:41:45 +0200

    adjust the ping thing header

--- app/javascript/packs/ping_things/ping_thing_header.vue
+++ app/javascript/packs/ping_things/ping_thing_header.vue
@@ -13,8 +13,9 @@
       tool to&nbsp;show recent confirmation times to&nbsp;your users. See GitHub for our reference <a href='https://github.com/Block-Logic/ping-thing-client' target='_blank'>Ping Thing Client</a>.
     </p>
     <p>
-      If you appreciate this website, please delegate to&nbsp;<a href="https://www.blocklogic.net/" target="_blank">Block Logic</a>.
-      Your delegations keep this website running!
+      If you enjoy The Ping Thing, please delegate to&nbsp;<a href="https://www.blocklogic.net/" target="_blank">Block Logic</a>
+      or toss some SOL into The Ping Thing tip jar:&nbsp;ping6gwBZx1ccMMFyLgkVSupUmujYrFidEXuNRPq989.<br />
+      Your contributions keep this website running!
     </p>
   </section>
 </template>
