9b164e952dbefd5971c0c9b59662535681cca6d5
Klaudia Biala <klaudia.biala@polcode.net>
2023-07-10 20:23:54 +0200

    Tweak ping thing banner

--- app/javascript/packs/ping_things/ping_thing_header.vue
+++ app/javascript/packs/ping_things/ping_thing_header.vue
@@ -1,12 +1,20 @@
 <template>
   <section class="page-header">
     <h1>Ping Thing</h1>
+
+    <div class="alert alert-info mt-4" role="alert">
+      If you enjoy The Ping Thing, please delegate to <a href='https://www.blocklogic.net/'>Block Logic</a> or toss some
+      SOL into The Ping Thing tip jar: <strong class='word-break'>ping6gwBZx1ccMMFyLgkVSupUmujYrFidEXuNRPq989</strong>.
+      Your&nbsp;contributions keep this website running!"
+    </div>
+
     <p>
       We use this Ping Thing to&nbsp;show recent transaction confirmation times on the&nbsp;Solana blockchain.
       The&nbsp;<a href='https://www.blocklogic.net' target='_blank'>Block Logic Validator</a> plus several other
       validators are sending simple transfer transactions every 15&nbsp;seconds and the&nbsp;confirmation times appear
       below. You can use these pings as a&nbsp;gauge of&nbsp;cluster performance.
     </p>
+
     <p>
       All data is available for download from our <a href="/api-documentation">REST&nbsp;API</a>. App developers, you can
       also upload your own TX&nbsp;confirmation times. Any Solana application or transaction type is allowed. Use this
