35692b2ec1b72b6335829b829c74f291f815ed70
ktarnowski <kam.tarnowski@gmail.com>
2023-08-18 12:51:50 +0200

    revert validator name changes

--- app/javascript/packs/validators/validator_details.vue
+++ app/javascript/packs/validators/validator_details.vue
@@ -9,6 +9,7 @@
         <img src="https://keybase.io/images/no-photo/placeholder-avatar-180-x-180@2x.png" class="img-circle-medium" v-else>
 
         <h1 class="word-break" v-if="is_loading_validator">loading...</h1>
+        <h1 class="word-break" v-else>{{ name_or_account(validator) }}</h1>
       </div>
 
       <div class="d-flex justify-content-between flex-wrap gap-3">
