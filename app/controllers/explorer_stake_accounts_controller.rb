1ab22309762fc59fc2cd95e1c2105e3f940e1303
maciejkocylapc <maciej.kocyla@polcode.net>
2023-10-11 13:24:04 +0200

    avoid displaying all stake accounts

--- app/controllers/explorer_stake_accounts_controller.rb
+++ app/controllers/explorer_stake_accounts_controller.rb
@@ -4,10 +4,13 @@ class ExplorerStakeAccountsController < ApplicationController
   include StakeAccountsControllerHelper
 
   def index
-    if index_params[:staker] || index_params[:withdrawer] || index_params[:vote_account] || index_params[:stake_pubkey]
-      @explorer_stake_accounts, @stake_accounts = get_explorer_stake_accounts(params: index_params)
-      @explorer_stake_accounts = @explorer_stake_accounts[:explorer_stake_accounts]
-    end
+    if index_params[:staker].present? || \
+      index_params[:withdrawer].present? || \
+      index_params[:vote_account].present? || \
+      index_params[:stake_pubkey].present?
+     @explorer_stake_accounts, @stake_accounts = get_explorer_stake_accounts(params: index_params)
+     @explorer_stake_accounts = @explorer_stake_accounts[:explorer_stake_accounts]
+   end
   end
 
   private
