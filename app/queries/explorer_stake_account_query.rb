519ffd428e30859654d230f44dc8177804aa8697
maciejkocylapc <maciej.kocyla@polcode.net>
2023-10-27 12:11:51 +0200

    passing tests

--- app/queries/explorer_stake_account_query.rb
+++ app/queries/explorer_stake_account_query.rb
@@ -1,6 +1,8 @@
 # frozen_string_literal: true
 
 class ExplorerStakeAccountQuery
+  MIN_ACCOUNTS_NUMBER = Rails.env.test? 1 : 500_000
+
   def initialize(withdrawer: nil, staker: nil, vote_account: nil, stake_pubkey: nil, network: "mainnet")
     @withdrawer = withdrawer
     @staker = staker
@@ -12,8 +14,15 @@ class ExplorerStakeAccountQuery
   def call(page: 1, per: 20)
     explorer_stake_accounts = ExplorerStakeAccount.where(
       network: @network,
-      epoch: EpochWallClock.by_network(@network).first&.epoch
+      epoch: EpochWallClock.by_network(@network).last&.epoch
     )
+
+    if explorer_stake_accounts.count < MIN_ACCOUNTS_NUMBER
+      explorer_stake_accounts = ExplorerStakeAccount.where(
+        network: @network,
+        epoch: EpochWallClock.by_network(@network).last&.epoch - 1
+      )
+    end
 
     explorer_stake_accounts = explorer_stake_accounts.where("withdrawer LIKE ?", @withdrawer) \
       if @withdrawer.present?
