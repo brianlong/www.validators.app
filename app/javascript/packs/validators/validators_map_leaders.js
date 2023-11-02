18fa744f40d5007c2a1776ad5e8983c3b9ecb048
Klaudia Biala <klaudia.biala@polcode.net>
2023-11-02 17:29:56 +0100

    Fix network on leaders map

--- app/javascript/packs/validators/validators_map_leaders.js
+++ app/javascript/packs/validators/validators_map_leaders.js
@@ -25,12 +25,12 @@ var ValidatorsMapLeaders = Vue.component('ValidatorsMapLeaders', {
       <div class="small" v-if="!current_leader">loading...</div>
 
       <div class="d-flex flex-wrap gap-3 mb-3" v-if="current_leader">
-        <a :href="validator_url(current_leader.account, current_leader.network)"
+        <a :href="validator_url(current_leader.account, $parent.network)"
            title="Go to validator details" target="_blank">
           <img :src="avatar_url(current_leader)" class="img-circle-small" />
         </a>
         <div class="d-flex flex-column justify-content-center">
-          <a :href="validator_url(current_leader.account, current_leader.network)"
+          <a :href="validator_url(current_leader.account, $parent.network)"
              title="Go to validator details"
              class="fw-bold" target="_blank">
             {{ shortened_validator_name(current_leader.name, current_leader.account) }}
@@ -42,7 +42,7 @@ var ValidatorsMapLeaders = Vue.component('ValidatorsMapLeaders', {
         <div class="small text-muted mb-2">Next Leaders</div>
         <div class="d-flex flex-wrap gap-3">
           <a v-for="leader in next_leaders"
-             :href="validator_url(leader.account, leader.network)"
+             :href="validator_url(leader.account, $parent.network)"
              title="Go to validator details" target="_blank">
             <img :src="avatar_url(leader)" class='img-circle-small' />
           </a>
