3833986c7c1a1338741ad60f0f936626f015daa0
maciejkocylapc <maciej.kocyla@polcode.net>
2023-11-08 12:00:00 +0100

    get ip from x_forwarded_for

--- app/controllers/concerns/ip_geo_country_helper.rb
+++ app/controllers/concerns/ip_geo_country_helper.rb
@@ -3,7 +3,8 @@
 module IpGeoCountryHelper
   def set_geo_country
     db = MaxMindDB.new("./GeoLite2-Country.mmdb")
-    db.lookup(request.remote_ip).country.iso_code
+    client_ip =  request.env["X_FORWARDED_FOR"] || request.env["HTTP_X_FORWARDED_FOR"] || request.remote_ip
+    db.lookup(client_ip).country.iso_code
     # db.lookup("102.129.157." + rand(1..255).to_s).country.iso_code # => "GB"
   end
 end
