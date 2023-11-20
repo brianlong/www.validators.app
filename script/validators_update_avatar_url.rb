ca47c17cd1e5da7122879e11d145ba9d8c165806
Klaudia Biala <klaudia.biala@polcode.net>
2023-11-20 11:21:24 +0100

    Tweak script

--- script/validators_update_avatar_url.rb
+++ script/validators_update_avatar_url.rb
@@ -1,6 +1,8 @@
 # frozen_string_literal: true
 
 # RAILS_ENV=production bundle exec ruby script/validators_get_avatar_url.rb
+# This script refreshes existing keybase avatars, doesn't override non-keybase avatars
+
 require File.expand_path('../config/environment', __dir__)
 
 include KeybaseLogic
@@ -8,11 +10,17 @@ include KeybaseLogic
 interrupted = false
 trap('INT') { interrupted = true }  unless Rails.env.test?
 
+KEYBASE_URL_PATTERN = /^https:\/\/s3.amazonaws.com\/keybase_processed_uploads/
+
 NETWORKS.each do |network|
-  Validator.where(network: network).where.not(keybase_id: '').where.not(avatar_url: nil).find_each do |validator|
-    validator_url = get_validator_avatar(validator.keybase_id)
-    if(validator_url != validator.avatar_url)
-      validator.update(avatar_url: validator_url)
+  Validator.active.where(network: network)
+           .where.not(keybase_id: '')
+           .where.not(avatar_url: nil)
+           .find_each do |validator|
+    next unless validator.avatar_url.match? KEYBASE_URL_PATTERN
+    new_validator_url = get_validator_avatar(validator.keybase_id)
+    if(new_validator_url != validator.avatar_url)
+      validator.update(avatar_url: new_validator_url)
     end
     sleep(1)
   end
