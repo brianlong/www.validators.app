9054b994125f460e3114661b124af5e61b2bbf59
Klaudia Biala <klaudia.biala@polcode.net>
2023-11-20 13:08:48 +0100

    Update tests

--- script/validators_update_avatar_url.rb
+++ script/validators_update_avatar_url.rb
@@ -10,14 +10,14 @@ include KeybaseLogic
 interrupted = false
 trap('INT') { interrupted = true }  unless Rails.env.test?
 
-KEYBASE_URL_PATTERN = /^https:\/\/s3.amazonaws.com\/keybase_processed_uploads/
+keybase_url_pattern = /^https:\/\/s3.amazonaws.com\/keybase_processed_uploads/
 
 NETWORKS.each do |network|
   Validator.active.where(network: network)
            .where.not(keybase_id: '')
            .where.not(avatar_url: nil)
            .find_each do |validator|
-    next unless validator.avatar_url.match? KEYBASE_URL_PATTERN
+    next unless validator.avatar_url.match? keybase_url_pattern
     new_validator_url = get_validator_avatar(validator.keybase_id)
     if(new_validator_url != validator.avatar_url)
       validator.update(avatar_url: new_validator_url)
