3089c5592f5e06db7a4aed4a913d3db07dd1974f
Klaudia Biala <klaudia.biala@polcode.net>
2023-11-21 11:05:08 +0100

    186502853 bring back logos refresh (#883)
    * Re-schedule keybase refresh script
    
    * Tweak script
    
    * Update tests
    
    * Tweak tests order
    
    * Address CR comments

--- script/validators_update_avatar_url.rb
+++ script/validators_update_avatar_url.rb
@@ -1,6 +1,8 @@
 # frozen_string_literal: true
 
 # RAILS_ENV=production bundle exec ruby script/validators_get_avatar_url.rb
+# This script refreshes existing keybase avatars, doesn't override non-keybase avatars
+
 require File.expand_path('../config/environment', __dir__)
 
 include KeybaseLogic
@@ -8,11 +10,16 @@ include KeybaseLogic
 interrupted = false
 trap('INT') { interrupted = true }  unless Rails.env.test?
 
+keybase_url_pattern = '^https:\/\/s3.amazonaws.com\/keybase_processed_uploads/'.freeze
+
 NETWORKS.each do |network|
-  Validator.where(network: network).where.not(keybase_id: '').where.not(avatar_url: nil).find_each do |validator|
-    validator_url = get_validator_avatar(validator.keybase_id)
-    if(validator_url != validator.avatar_url)
-      validator.update(avatar_url: validator_url)
+  Validator.active.where(network: network)
+           .where.not(keybase_id: '')
+           .where("avatar_url REGEXP ?", keybase_url_pattern)
+           .find_each do |validator|
+    new_validator_url = get_validator_avatar(validator.keybase_id)
+    if(new_validator_url != validator.avatar_url)
+      validator.update(avatar_url: new_validator_url)
     end
     sleep(1)
   end
