91342aa92fbcc795ab1f4d296120998ec98635fa
Klaudia Biala <klaudia.biala@polcode.net>
2023-12-07 12:47:37 +0100

    Add digitalocean credentials for stage

--- config/environments/stage.rb
+++ config/environments/stage.rb
@@ -3,12 +3,12 @@ require Rails.root.join('config/environments/production')
 
 Rails.application.routes.default_url_options = {
   host: 'stage.validators.app',
-  protocol: 'https' 
+  protocol: 'https'
 }
 
 Rails.application.configure do
   config.action_mailer.default_url_options = { host: 'stage.validators.app' }
 
   config.action_cable.url = 'wss://stage.validators.app/cable'
-  config.active_storage.service = :amazon
+  config.active_storage.service = :digitalocean
 end
