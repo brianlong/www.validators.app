41c68d49547023de55cdfa263a51d426cc8867bd
Klaudia Biala <klaudia.biala@polcode.net>
2024-01-02 16:02:22 +0100

    Remove comment

--- config/environments/stage.rb
+++ config/environments/stage.rb
@@ -10,9 +10,8 @@ Rails.application.configure do
   config.action_mailer.default_url_options = { host: 'stage.validators.app' }
 
   config.action_cable.url = 'wss://stage.validators.app/cable'
+
   config.active_storage.service = :digitalocean
 
-  # Disable serving static files from the `/public` folder by default since
-  # Apache or NGINX already handles this.
   config.public_file_server.enabled = ENV['RAILS_SERVE_STATIC_FILES'].present?
 end
