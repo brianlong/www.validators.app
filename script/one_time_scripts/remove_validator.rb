6ad6d21b8a5b4218d36f9fa9f25c877d4a85069d
ktarnowski <kam.tarnowski@gmail.com>
2024-01-20 00:11:03 +0100

    add rescue when transaction fails

--- script/one_time_scripts/remove_validator.rb
+++ script/one_time_scripts/remove_validator.rb
@@ -11,39 +11,43 @@ raise "validator not found (#{account})" unless validator
 
 puts "found validator with id: #{validator.id}"
 
-validator.validator_histories.delete_all
-puts "validator_histories deleted"
+ActiveRecord::Base.transaction do
+  validator.validator_histories.delete_all
+  puts "validator_histories deleted"
 
-validator.validator_block_histories.delete_all
-puts "validator_block_histories deleted"
+  validator.validator_block_histories.delete_all
+  puts "validator_block_histories deleted"
 
-validator.vote_accounts.each do |va|
-  va.account_authority_histories.map(&:delete)
-  va.vote_account_histories.map(&:delete)
+  validator.vote_accounts.each do |va|
+    va.account_authority_histories.map(&:delete)
+    va.vote_account_histories.map(&:delete)
+  end
+  puts "account_authority_histories deleted"
+  puts "vote_account_histories deleted"
+
+  validator.vote_accounts.delete_all
+  puts "vote_accounts deleted"
+
+  validator.commission_histories.delete_all
+  puts "commission_histories deleted"
+
+  validator.stake_account_histories.delete_all
+  puts "stake_account_histories deleted"
+
+  validator.stake_accounts.delete_all
+  puts "stake_accounts deleted"
+
+  validator.validator_ips.delete_all
+  puts "ips deleted"
+
+  validator.validator_score_v1&.delete
+  puts "score deleted"
+
+  validator.user_watchlist_elements.delete_all
+  puts "user_watchlist_elements deleted"
+
+  validator.delete
+  puts "validator deleted"
+rescue StandardError => e
+  puts e.message
 end
-puts "account_authority_histories deleted"
-puts "vote_account_histories deleted"
-
-validator.vote_accounts.delete_all
-puts "vote_accounts deleted"
-
-validator.commission_histories.delete_all
-puts "commission_histories deleted"
-
-validator.stake_account_histories.delete_all
-puts "stake_account_histories deleted"
-
-validator.stake_accounts.delete_all
-puts "stake_accounts deleted"
-
-validator.validator_ips.delete_all
-puts "ips deleted"
-
-validator.validator_score_v1&.delete
-puts "score deleted"
-
-validator.user_watchlist_elements.delete_all
-puts "user_watchlist_elements deleted"
-
-validator.delete
-puts "validator deleted"
