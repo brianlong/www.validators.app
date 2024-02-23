291cf436d7bba6bc53ba28560d1c0a3122f52e3a
ktarnowski <kam.tarnowski@gmail.com>
2024-02-23 10:56:14 +0100

    change application name

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -60,7 +60,7 @@ Rails.application.configure do
 
   # Use a real queuing backend for Active Job (and separate queues per environment).
   # config.active_job.queue_adapter     = :resque
-  # config.active_job.queue_name_prefix = "fma_ruby_on_rails_template_production"
+  # config.active_job.queue_name_prefix = "validators_app_production"
 
   config.action_mailer.perform_caching = false
 
