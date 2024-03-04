de570612df268db3b0a0c64dfafe923f857f8762
Klaudia Biala <klaudia.biala@polcode.net>
2024-03-04 15:35:17 +0100

    Add views to vote account pages

--- app/controllers/vote_accounts_controller.rb
+++ app/controllers/vote_accounts_controller.rb
@@ -19,8 +19,15 @@ class VoteAccountsController < ApplicationController
     @explorer_stake_accounts_total = @explorer_stake_accounts[:total_count]
     @explorer_stake_accounts = @explorer_stake_accounts[:explorer_stake_accounts]
 
-    @stake_histories = []
-    # @stake_histories = @vote_account.vote_account_stake_histories.order(epoch: :asc).limit(40)
+    @stake_histories = @vote_account.vote_account_stake_histories.order(epoch: :asc).limit(40)
+    @data_for_charts = {
+      active_stake: @stake_histories.map { |st| helpers.lamports_to_sol(st.active_stake.to_i).round(2) },
+      delegated_stake: @stake_histories.map { |st| helpers.lamports_to_sol(st.delegated_stake.to_i).round(2) },
+      epochs: @stake_histories.pluck(:epoch),
+      average_stake: @stake_histories.map { |st| helpers.lamports_to_sol(st.average_active_stake.to_i).round(2) },
+      deactivating_stake: @stake_histories.map { |st| helpers.lamports_to_sol(st.deactivating_stake.to_i).round(2) },
+      stake_accounts_count: @stake_histories.pluck(:delegating_stake_accounts_count),
+    }
   end
 
   private
