fe946095cb253309e320aa442b631950458e589c
Klaudia Biala <klaudia.biala@polcode.net>
2024-03-06 17:22:42 +0100

    Sort user stats by latency

--- app/controllers/api/v1/ping_thing_user_stats_controller.rb
+++ app/controllers/api/v1/ping_thing_user_stats_controller.rb
@@ -7,7 +7,7 @@ module Api
         last_5_mins = PingThingUserStat.where(
           network: stats_params[:network],
           interval: 5
-        ).group_by(&:username)
+        ).order(:average_slot_latency).group_by(&:username)
         last_60_mins = PingThingUserStat.where(
           network: stats_params[:network],
           interval: 60
