f5300f36be15e0978e13b1f7a07f10fbbbf2a4bf
Klaudia Biala <klaudia.biala@polcode.net>
2024-03-06 15:21:51 +0100

    187089574 add stake stats to views (#973)
    * Stats on stake explorer page
    
    * Tweak display
    
    * Move data for chart to controller
    
    * Replace average with delegated stake on large chart
    
    * Add small charts
    
    * Add ticks in short format
    
    * Tweak ticks for average stake chart
    
    * Add views to vote account pages
    
    * Unify vote and stage accounts headings
    
    * Make bars boulder
    
    * Move duplicated code to helper
    
    * Fix controller error

--- app/controllers/vote_accounts_controller.rb
+++ app/controllers/vote_accounts_controller.rb
@@ -2,6 +2,7 @@
 
 class VoteAccountsController < ApplicationController
   include StakeAccountsControllerHelper
+  include ExplorerStakeAccountsControllerHelper
 
   before_action :set_validator, only: %i[show]
   before_action :set_vote_account, only: %i[show]
@@ -19,8 +20,8 @@ class VoteAccountsController < ApplicationController
     @explorer_stake_accounts_total = @explorer_stake_accounts[:total_count]
     @explorer_stake_accounts = @explorer_stake_accounts[:explorer_stake_accounts]
 
-    @stake_histories = []
-    # @stake_histories = @vote_account.vote_account_stake_histories.order(epoch: :asc).limit(40)
+    @stake_histories = @vote_account.vote_account_stake_histories.order(epoch: :asc).last(40)
+    @data_for_charts = prepare_data_for_charts(@stake_histories)
   end
 
   private
