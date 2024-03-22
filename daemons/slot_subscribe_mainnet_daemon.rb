7caa8f6b875b5b4eae3dc76181f9c1874e67eeca
maciejkocylapc <maciej.kocyla@polcode.net>
2024-03-22 11:41:59 +0100

    slot subscribe service

---
+++ daemons/slot_subscribe_mainnet_daemon.rb
@@ -0,0 +1,21 @@
+# frozen_string_literal: true
+
+require_relative "../config/environment"
+
+SLEEP_TIME = 1 #seconds
+
+loop do
+  begin
+    Blockchain::SlotSubscribeService.new(
+      network: "mainnet",
+      rpc_url: Rails.application.credentials.solana[:mainnet_urls][0]
+    ).call
+  rescue => e
+    puts e
+    puts e.backtrace
+
+    # restart ws connection after SLEEP_TIME
+    sleep(SLEEP_TIME)
+    next
+  end
+end
