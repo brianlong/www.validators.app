650d21cb55147c34464846e7a4bab9cc90a4deec
maciejkocylapc <maciej.kocyla@polcode.net>
2024-03-22 11:55:25 +0100

    create daemons services

--- daemons/blockchain/slot_subscribe_pythnet_daemon.rb
+++ daemons/blockchain/slot_subscribe_pythnet_daemon.rb
@@ -1,14 +1,14 @@
 # frozen_string_literal: true
 
-require_relative "../config/environment"
+require_relative "../../config/environment"
 
 SLEEP_TIME = 1 #seconds
 
 loop do
   begin
     Blockchain::SlotSubscribeService.new(
-      network: "mainnet",
-      rpc_url: Rails.application.credentials.solana[:mainnet_urls][0]
+      network: "pythnet",
+      rpc_url: Rails.application.credentials.solana[:pythnet_urls][0]
     ).call
   rescue => e
     puts e
