1115a5ef06f381edd5f4cefbbfbd669a3fbd057a
maciejkocylapc <maciej.kocyla@polcode.net>
2024-03-22 15:28:34 +0100

    add modules for api/v1 controllers

--- test/controllers/api/v1/api_controller_test.rb
+++ test/controllers/api/v1/api_controller_test.rb
@@ -5,67 +5,71 @@ require "rack/cors"
 require "rack/test"
 
 # ApiControllerTest
-class ApiControllerTest < ActionDispatch::IntegrationTest
+module Api
+  module V1
+    class ApiControllerTest < ActionDispatch::IntegrationTest
 
-  include ResponseHelper
-
-  def setup
-    @user_params = {
-      username: "test",
-      email: "test@test.com",
-      password: "password"
-    }
-    @user = User.create(@user_params)
-  end
-
-  test "When reaching the api \
-        from origin listed in whitelist \
-        should return 200" do
-    get api_v1_ping_url, headers: {
-      "Origin" => "http://example.com" # this origin is in whitelist
-    }
-
-    assert_response 200
-    json = response_to_json(@response.body)
-    assert_equal "pong", json["answer"]
-  end
-
-  test "When reaching the api \
-        from foreign origin \
-        should return Unauthorized" do
-    get api_v1_ping_url, headers: {
-      "Origin" => "http://foreign.com"
-    }
-
-    assert_response 401
-    json = response_to_json(@response.body)
-    assert_equal "Unauthorized", json["error"]
-  end
-
-  test "When reaching the api \
-  from foreign origin but with correct token \
-  should return 200" do
-    get api_v1_ping_url, headers: {
-      "Token" => @user.api_token,
-      "Origin" => "http://foreign.com"
-    }
-
-    assert_response 200
-    json = response_to_json(@response.body)
-    assert_equal "pong", json["answer"]
-  end
-
-  test "GET api_v1_ping without token should get error" do
-    get api_v1_ping_url
-    assert_response 401
-    expected_response = { "error" => "Unauthorized" }
-    assert_equal expected_response, response_to_json(@response.body)
-  end
-
-  test "GET api_v1_ping with token should succeed" do
-    get api_v1_ping_url, headers: { "Token" => @user.api_token }
-    assert_response 200
-    json = response_to_json(@response.body)
-    assert_equal "pong", json["answer"]
+      include ResponseHelper
+    
+      def setup
+        @user_params = {
+          username: "test",
+          email: "test@test.com",
+          password: "password"
+        }
+        @user = User.create(@user_params)
+      end
+    
+      test "When reaching the api \
+            from origin listed in whitelist \
+            should return 200" do
+        get api_v1_ping_url, headers: {
+          "Origin" => "http://example.com" # this origin is in whitelist
+        }
+    
+        assert_response 200
+        json = response_to_json(@response.body)
+        assert_equal "pong", json["answer"]
+      end
+    
+      test "When reaching the api \
+            from foreign origin \
+            should return Unauthorized" do
+        get api_v1_ping_url, headers: {
+          "Origin" => "http://foreign.com"
+        }
+    
+        assert_response 401
+        json = response_to_json(@response.body)
+        assert_equal "Unauthorized", json["error"]
+      end
+    
+      test "When reaching the api \
+      from foreign origin but with correct token \
+      should return 200" do
+        get api_v1_ping_url, headers: {
+          "Token" => @user.api_token,
+          "Origin" => "http://foreign.com"
+        }
+    
+        assert_response 200
+        json = response_to_json(@response.body)
+        assert_equal "pong", json["answer"]
+      end
+    
+      test "GET api_v1_ping without token should get error" do
+        get api_v1_ping_url
+        assert_response 401
+        expected_response = { "error" => "Unauthorized" }
+        assert_equal expected_response, response_to_json(@response.body)
+      end
+    
+      test "GET api_v1_ping with token should succeed" do
+        get api_v1_ping_url, headers: { "Token" => @user.api_token }
+        assert_response 200
+        json = response_to_json(@response.body)
+        assert_equal "pong", json["answer"]
+      end
+    end    
   end
 end
--- test/controllers/api/v1/gossip_nodes_controller_test.rb
+++ test/controllers/api/v1/gossip_nodes_controller_test.rb
@@ -2,56 +2,60 @@
 
 require "test_helper"
 
-class GossipNodesControllerTest < ActionDispatch::IntegrationTest
-  include ResponseHelper
-
-  def setup
-    @network = "testnet"
-
-    @user = create(:user)
-    @headers = { "Token" => @user.api_token }
-    @gossip_nodes = create_list(:gossip_node, 60)
-  end
-
-  test "GET api_v1_gossip_nodes without token returns error" do
-    get api_v1_gossip_nodes_path(network: @network)
-    assert_response 401
-    expected_response = { "error" => "Unauthorized" }
-
-    assert_equal expected_response, response_to_json(@response.body)
-  end
-
-  test "GET api_v1_gossip_nodes with token returns 200" do
-    get api_v1_gossip_nodes_path(network: @network), headers: @headers
-
-    assert_response 200
-    assert_equal 60, response_to_json(@response.body).size
-  end
-
-  test "GET api_v1_gossip_nodes returns only active nodes" do
-    inactive_node = create(:gossip_node, :inactive)
-    get api_v1_gossip_nodes_path(network: @network), headers: @headers
-
-    assert_response 200
-    api_nodes = response_to_json(@response.body)
-    refute api_nodes.include?(inactive_node)
-  end
-
-  test "GET api_v1_gossip_nodes with token as csv returns 200" do
-    path = api_v1_gossip_nodes_path(network: @network) + ".csv"
-    get path, headers: @headers
-
-    assert_response :success
-    assert_equal "text/csv", response.content_type
-    csv = CSV.parse response.body # Let raise if invalid CSV
-    assert csv
-    assert_equal csv.size, 61
-
-    headers = (
-      GossipNode::API_FIELDS +
-      GossipNode::API_VALIDATOR_FIELDS +
-      GossipNode::API_DATA_CENTER_FIELDS
-    ).map(&:to_s)
-    assert_equal csv.first, headers
+module Api
+  module V1
+    class GossipNodesControllerTest < ActionDispatch::IntegrationTest
+      include ResponseHelper
+    
+      setup do
+        @network = "testnet"
+    
+        @user = create(:user)
+        @headers = { "Token" => @user.api_token }
+        @gossip_nodes = create_list(:gossip_node, 60)
+      end
+    
+      test "GET api_v1_gossip_nodes without token returns error" do
+        get api_v1_gossip_nodes_path(network: @network)
+        assert_response 401
+        expected_response = { "error" => "Unauthorized" }
+    
+        assert_equal expected_response, response_to_json(@response.body)
+      end
+    
+      test "GET api_v1_gossip_nodes with token returns 200" do
+        get api_v1_gossip_nodes_path(network: @network), headers: @headers
+    
+        assert_response 200
+        assert_equal 60, response_to_json(@response.body).size
+      end
+    
+      test "GET api_v1_gossip_nodes returns only active nodes" do
+        inactive_node = create(:gossip_node, :inactive)
+        get api_v1_gossip_nodes_path(network: @network), headers: @headers
+    
+        assert_response 200
+        api_nodes = response_to_json(@response.body)
+        refute api_nodes.include?(inactive_node)
+      end
+    
+      test "GET api_v1_gossip_nodes with token as csv returns 200" do
+        path = api_v1_gossip_nodes_path(network: @network) + ".csv"
+        get path, headers: @headers
+    
+        assert_response :success
+        assert_equal "text/csv", response.content_type
+        csv = CSV.parse response.body # Let raise if invalid CSV
+        assert csv
+        assert_equal csv.size, 61
+    
+        headers = (
+          GossipNode::API_FIELDS +
+          GossipNode::API_VALIDATOR_FIELDS +
+          GossipNode::API_DATA_CENTER_FIELDS
+        ).map(&:to_s)
+        assert_equal csv.first, headers
+      end
+    end
   end
 end
