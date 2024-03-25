5c897eebf0896971bd7c231b447b1e5b8f7d4bfd
Klaudia Biala <klaudia.biala@polcode.net>
2024-03-25 18:07:56 +0100

    Address rubocop comments

--- test/factories/validator_block_history_stats.rb
+++ test/factories/validator_block_history_stats.rb
@@ -2,6 +2,14 @@
 
 FactoryBot.define do
   factory :validator_block_history_stat do
-
+    batch_uuid { SecureRandom.uuid }
+    epoch { 1 }
+    start_slot { 1 }
+    end_slot { 10 }
+    total_slots { 10 }
+    total_blocks_produced { 10 }
+    total_slots_skipped { 0 }
+    network { "testnet" }
+    skipped_slot_percent_moving_average { 0.0 }
   end
 end
