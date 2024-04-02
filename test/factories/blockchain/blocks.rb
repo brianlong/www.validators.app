4a21021cbecf7503d37c156310b0cd3e496312a4
maciejkocylapc <maciej.kocyla@polcode.net>
2024-04-02 12:57:14 +0200

    tests for GetBlockService

--- test/factories/blockchain/blocks.rb
+++ test/factories/blockchain/blocks.rb
@@ -1,8 +1,9 @@
 FactoryBot.define do
   factory :blockchain_block, class: 'Blockchain::Block' do
-    height { "" }
-    block_time { "" }
-    hash { "MyString" }
-    parent_slot { "" }
+    height { "123" }
+    block_time { 12 }
+    blockhash { "block_hash_123" }
+    parent_slot { 12345 }
+    slot_number { 12346 }
   end
 end
--- test/services/blockchain/get_leader_schedule_service_test.rb
+++ test/services/blockchain/get_leader_schedule_service_test.rb
@@ -2,45 +2,47 @@
 
 require "test_helper"
 
-class GetLeaderScheduleServiceTest < ActiveSupport::TestCase
-  include VcrHelper
+module Blockchain
+  class GetLeaderScheduleServiceTest < ActiveSupport::TestCase
+    include VcrHelper
 
-  setup do
-    @epoch = 586
-    @network = "mainnet"
-    @mainnet_url = "https://api.mainnet-beta.solana.com"
-    @json_data = {
-      "epoch": 586,
-      "leaderScheduleEntries": [
-        {"slot": "123", "leader": "validator_account_1"},
-        {"slot": "124", "leader": "validator_account_2"},
-        {"slot": "125", "leader": "validator_account_3"},
-        {"slot": "126", "leader": "validator_account_4"}
-      ]
-    }.to_json
-  end
-
-  test "#call downloads and saves current leader slots" do
-    SolanaCliService.stub(:request, @json_data, ["leader-schedule --epoch #{@epoch}", @mainnet_url]) do
-      Blockchain::GetLeaderScheduleService.new(@network, @epoch, [@mainnet_url]).call
-
-      assert_equal 4, Blockchain::Slot.count
-      assert_equal "validator_account_4", Blockchain::Slot.last.leader
-      assert_equal 126, Blockchain::Slot.last.slot_number
-      assert_equal @network, Blockchain::Slot.last.network
-      assert_equal @epoch, Blockchain::Slot.last.epoch
-      assert_equal "initialized", Blockchain::Slot.last.status
+    setup do
+      @epoch = 586
+      @network = "mainnet"
+      @mainnet_url = "https://api.mainnet-beta.solana.com"
+      @json_data = {
+        "epoch": 586,
+        "leaderScheduleEntries": [
+          {"slot": "123", "leader": "validator_account_1"},
+          {"slot": "124", "leader": "validator_account_2"},
+          {"slot": "125", "leader": "validator_account_3"},
+          {"slot": "126", "leader": "validator_account_4"}
+        ]
+      }.to_json
     end
-  end
 
-  test "#call does not create new slots when there already are some slots for this epoch" do
-    existing_slot = create(:blockchain_slot)
+    test "#call downloads and saves current leader slots" do
+      SolanaCliService.stub(:request, @json_data, ["leader-schedule --epoch #{@epoch}", @mainnet_url]) do
+        Blockchain::GetLeaderScheduleService.new(@network, @epoch, [@mainnet_url]).call
 
-    SolanaCliService.stub(:request, @json_data, ["leader-schedule --epoch #{@epoch}", @mainnet_url]) do
-      Blockchain::GetLeaderScheduleService.new(@network, @epoch, [@mainnet_url]).call
+        assert_equal 4, Blockchain::Slot.count
+        assert_equal "validator_account_4", Blockchain::Slot.last.leader
+        assert_equal 126, Blockchain::Slot.last.slot_number
+        assert_equal @network, Blockchain::Slot.last.network
+        assert_equal @epoch, Blockchain::Slot.last.epoch
+        assert_equal "initialized", Blockchain::Slot.last.status
+      end
+    end
 
-      assert_equal 1, Blockchain::Slot.count
-      assert_equal existing_slot, Blockchain::Slot.last
+    test "#call does not create new slots when there already are some slots for this epoch" do
+      existing_slot = create(:blockchain_slot)
+
+      SolanaCliService.stub(:request, @json_data, ["leader-schedule --epoch #{@epoch}", @mainnet_url]) do
+        Blockchain::GetLeaderScheduleService.new(@network, @epoch, [@mainnet_url]).call
+
+        assert_equal 1, Blockchain::Slot.count
+        assert_equal existing_slot, Blockchain::Slot.last
+      end
     end
   end
 end
