5078e13098134f7fa643dad24be5989df9286653
maciejkocylapc <maciej.kocyla@polcode.net>
2024-04-10 11:43:40 +0200

    divide archives into separate files

--- dev/ping_things_history_to_csv.rb
+++ dev/ping_things_history_to_csv.rb
@@ -5,9 +5,10 @@ require File.expand_path('../config/environment', __dir__)
 require "csv"
 
 columns = %w[user_id amount signature response_time transaction_type network commitment_level success application reported_at slot_sent slot_landed]
-file = "#{Rails.root}/tmp/ping_thing.csv"
+file_recent = "#{Rails.root}/tmp/ping_thing_recent.csv"
+file_archive_root = "#{Rails.root}/tmp/ping_thing_archive"
 
-CSV.open(file, "w") do |csv|
+CSV.open(file_recent, "w") do |csv|
   csv << columns
 
   total = PingThing.count
@@ -15,10 +16,17 @@ CSV.open(file, "w") do |csv|
     print "\r#{index + 1} / #{total}"
     csv << columns.map { |col| pt.send(col) }
   end
+end
 
-  total = PingThing.count
-  PingThingArchive.all.find_each.with_index do |pta, index|
-    print "\r#{index + 1} / #{total}"
-    csv << columns.map { |col| pta.send(col) }
+total_batches = PingThingArchive.count / 500_000
+PingThingArchive.find_in_batches(batch_size: 500_000).with_index do |pta_batch, batch_index|
+  file_archive = "#{file_archive_root}_#{batch_index}.csv"
+  print "\r#{index + 1} / #{total}"
+
+  CSV.open(file_archive, "w") do |csv|
+    csv << columns
+    pta_batch.each do |pta|
+      csv << columns.map { |col| pta.send(col) }
+    end
   end
 end
