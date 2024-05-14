1a7700746551204454c4a820b72b1a9bd47a5cf7
maciejkocylapc <80820353+maciejkocylapc@users.noreply.github.com>
2024-05-14 13:51:22 +0200

    Update script/archives/archive_blockchain.rb
    Co-authored-by: Kamil Tarnowski <kam.tarnowski@gmail.com>

--- script/archives/archive_blockchain.rb
+++ script/archives/archive_blockchain.rb
@@ -5,7 +5,7 @@ require File.expand_path('../../config/environment', __dir__)
 EPOCHS_KEPT = 6
 
 NETWORKS.each do |network|
-  current_epoch = EpochWallClock.where(network: network).order(epoch: :desc)&.first
+  current_epoch = EpochWallClock.where(network: network).order(epoch: :desc).first
   next unless current_epoch
   Blockchain::Slot.where(network: network)
                   .where("epoch < ?", current_epoch.epoch - EPOCHS_KEPT)
