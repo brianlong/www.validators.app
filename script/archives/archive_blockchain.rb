49c2597d6d8bef688e8f3fde36e1c26a27210310
Maciej Kocyła <maciej.kocyla@polcode.net>
2024-05-28 10:50:15 +0200

    add time check

--- script/archives/archive_blockchain.rb
+++ script/archives/archive_blockchain.rb
@@ -12,7 +12,8 @@ NETWORKS.each do |network|
 
   ((target_epoch - EPOCHS_BACK)...target_epoch).each do |epoch_to_clear|
     if Blockchain::Slot.where(network: network, epoch: epoch_to_clear).exists?
-      Parallel.each(Blockchain::Slot.where(network: network, epoch: epoch_to_clear).find_in_batches(batch_size: 50), in_threads: 3) do |batch|
+      Parallel.each(Blockchain::Slot.where(network: network, epoch: epoch_to_clear).find_in_batches(batch_size: 50), in_threads: 2) do |batch|
+        start_time = Time.now
         slot_numbers = batch.map(&:slot_number)
 
         block_batch = Blockchain::Block.where(network: network, slot_number: slot_numbers).to_a
@@ -23,6 +24,7 @@ NETWORKS.each do |network|
         Blockchain::Transaction.archive_batch(transaction_batch, destroy_after_archive: true) unless transaction_batch.empty?
         Blockchain::Block.archive_batch(block_batch, destroy_after_archive: true) unless block_batch.empty?
         Blockchain::Slot.archive_batch(batch, destroy_after_archive: true) unless batch.empty?
+        puts "Archived thread #{Parallel.worker_number} in #{Time.now - start_time} seconds"
       end
     end
   end
