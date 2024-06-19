8d77019109f1b57321313a1dce2c6dc052510d1a
Maciej Kocyła <maciej.kocyla@polcode.net>
2024-06-19 09:40:48 +0200

    add tests

--- test/models/blockchain/block_test.rb
+++ test/models/blockchain/block_test.rb
@@ -3,7 +3,23 @@
 require "test_helper"
 
 class Blockchain::BlockTest < ActiveSupport::TestCase
-  # test "the truth" do
-  #   assert true
-  # end
+  test "#network returns correct child class" do
+    assert_equal Blockchain::Block.network("mainnet").to_s, "Blockchain::MainnetBlock"
+    assert_equal Blockchain::Block.network("testnet").to_s, "Blockchain::TestnetBlock"
+    assert_equal Blockchain::Block.network("pythnet").to_s, "Blockchain::PythnetBlock"
+  end
+
+  test "#network raises error for invalid network" do
+    assert_raises ArgumentError do
+      Blockchain::Block.network("invalid")
+    end
+  end
+
+  test "#count returns correct count" do
+    create(:mainnet_block)
+    create_list(:testnet_block, 2)
+    create_list(:pythnet_block, 3)
+
+    assert_equal Blockchain::Block.count, 6
+  end
 end
--- test/models/blockchain/slot_test.rb
+++ test/models/blockchain/slot_test.rb
@@ -3,7 +3,23 @@
 require "test_helper"
 
 class Blockchain::SlotTest < ActiveSupport::TestCase
-  # test "the truth" do
-  #   assert true
-  # end
+  test "#network returns correct child class" do
+    assert_equal Blockchain::Slot.network("mainnet").to_s, "Blockchain::MainnetSlot"
+    assert_equal Blockchain::Slot.network("testnet").to_s, "Blockchain::TestnetSlot"
+    assert_equal Blockchain::Slot.network("pythnet").to_s, "Blockchain::PythnetSlot"
+  end
+
+  test "#network raises error for invalid network" do
+    assert_raises ArgumentError do
+      Blockchain::Slot.network("invalid")
+    end
+  end
+
+  test "#count returns correct count" do
+    create(:mainnet_slot)
+    create_list(:testnet_slot, 2)
+    create_list(:pythnet_slot, 3)
+
+    assert_equal Blockchain::Slot.count, 6
+  end
 end
