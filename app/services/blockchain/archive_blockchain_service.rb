c27fea2113e23890bc6fe9752a1d6b080c2fd9d8
Maciej Kocyła <maciej.kocyla@polcode.net>
2024-07-03 11:09:33 +0200

    archive daemon v0.2

---
+++ app/services/blockchain/archive_blockchain_service.rb
@@ -0,0 +1,71 @@
+# frozen_string_literal: true
+
+module Blockchain
+  class ArchiveBlockchainService
+    def initialize(archive: true, network: "mainnet", epoch: nil, processes: 3)
+      @archive = archive
+      @network = network
+      @processes = processes
+      @epoch_to_clear = epoch
+      @current_epoch = EpochWallClock.where(network: @network).order(epoch: :desc).first
+      @logger = Logger.new("log/archive_blockchain_#{@network}.log")
+    end
+
+    def call
+      if Blockchain::Slot.network(@network).where(epoch: @epoch_to_clear).exists?
+        begin
+          Parallel.each(
+            Blockchain::Slot.network(@network).where(epoch: epoch_to_clear).find_in_batches(batch_size: 100),
+            in_processes: @processes
+          ) do |batch|
+
+            start_time = Time.now
+  
+            slot_numbers = batch.map(&:slot_number)
+            block_batch = Blockchain::Block.network(@network).where(slot_number: slot_numbers).to_a
+            transaction_batch = Blockchain::Transaction.network(@network).where(block_id: block_batch.map(&:id)).to_a
+  
+            if transaction_batch.any?
+              Blockchain::Transaction.network(@network).archive_batch(transaction_batch, archive: false, destroy_after_archive: false)
+              group_ids(transaction_batch.map(&:id)).each do |ids|
+                Blockchain::Transaction.network(@network).where("id BETWEEN ? AND ?", ids.first, ids.last).delete_all
+              end
+            end
+            
+            Blockchain::Block.network(@network).archive_batch(block_batch, archive: @archive, destroy_after_archive: true) unless block_batch.empty?
+            Blockchain::Slot.network(@network).archive_batch(batch, archive: @archive, destroy_after_archive: true) unless batch.empty?
+
+            @logger.info <<-STRING.squish
+              Archived #{block_batch.count} blocks,
+              #{batch.count} slots,
+              #{transaction_batch.count} transactions 
+              for epoch #{epoch_to_clear} (#{@network}) \
+              in thread #{Parallel.worker_number} in #{Time.now - start_time} seconds"
+            STRING
+          end
+        rescue Parallel::DeadWorker
+          @logger.error "DeadWorker error, retrying in 5 seconds"
+          sleep 5
+          retry
+        end
+      end
+    end
+
+    def group_ids ids
+      arrays = []
+      ids.each do |id|
+        if arrays.empty?
+          arrays << [id]
+        else
+          if id == arrays.last.last + 1
+            arrays.last << id
+          else
+            arrays << [id]
+          end
+        end
+      end
+      arrays
+    end
+
+  end
+end
--- daemons/blockchain/archive_mainnet_daemon.rb
+++ daemons/blockchain/archive_mainnet_daemon.rb
@@ -5,59 +5,23 @@ require File.expand_path('../../config/environment', __dir__)
 EPOCHS_KEPT = 1
 EPOCHS_BACK = 10
 NETWORK = "mainnet"
+SLEEP_TIME = 60 * 10
 
 ARCHIVE = true
 
-def group_ids ids
-  arrays = []
-  ids.each do |id|
-    if arrays.empty?
-      arrays << [id]
-    else
-      if id == arrays.last.last + 1
-        arrays.last << id
-      else
-        arrays << [id]
-      end
-    end
-  end
-  arrays
-end
+
 
 loop do
   current_epoch = EpochWallClock.where(network: NETWORK).order(epoch: :desc).first
-  next unless current_epoch
+  unless current_epoch
+    sleep SLEEP_TIME
+    next
+  end
+
   target_epoch = current_epoch&.epoch - EPOCHS_KEPT
 
   ((target_epoch - EPOCHS_BACK)...target_epoch).each do |epoch_to_clear|
-    if Blockchain::Slot.network(NETWORK).where(epoch: epoch_to_clear).exists?
-      begin
-        Parallel.each(Blockchain::Slot.network(NETWORK).where(epoch: epoch_to_clear).find_in_batches(batch_size: 100), in_processes: 3) do |batch|
-          start_time = Time.now
-
-          slot_numbers = batch.map(&:slot_number)
-          block_batch = Blockchain::Block.network(NETWORK).where(slot_number: slot_numbers).to_a
-          transaction_batch = Blockchain::Transaction.network(NETWORK).where(block_id: block_batch.map(&:id)).to_a
-
-          if transaction_batch.any?
-            Blockchain::Transaction.network(NETWORK).archive_batch(transaction_batch, archive: ARCHIVE, destroy_after_archive: false)
-            group_ids(transaction_batch.map(&:id)).each do |ids|
-              Blockchain::Transaction.network(NETWORK).where("id BETWEEN ? AND ?", ids.first, ids.last).delete_all
-            end
-          end
-          
-          Blockchain::Block.network(NETWORK).archive_batch(block_batch, archive: ARCHIVE, destroy_after_archive: true) unless block_batch.empty?
-          Blockchain::Slot.network(NETWORK).archive_batch(batch, archive: ARCHIVE, destroy_after_archive: true) unless batch.empty?
-          puts "Archived #{block_batch.count} blocks, #{batch.count} slots, #{transaction_batch.count} transactions for epoch #{epoch_to_clear} (#{NETWORK}) \
-          in thread #{Parallel.worker_number} in #{Time.now - start_time} seconds"
-        end
-      rescue Parallel::DeadWorker
-        puts "DeadWorker error, retrying in 5 seconds"
-        sleep 5
-        retry
-      end
-    end
+    Blockchain::ArchiveBlockchainService.new(archive: ARCHIVE, network: NETWORK, epoch: epoch_to_clear).call
   end
-  puts "Sleeping for 60 seconds"
-  sleep 60
+  sleep SLEEP_TIME
 end
