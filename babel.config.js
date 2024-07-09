3289a6f9572cbe727c5afdbb99cd4ba6010f4e64
Klaudia Biala <klaudia.biala@polcode.net>
2024-07-09 21:55:35 +0200

    Upgrade webpacker

--- babel.config.js
+++ babel.config.js
@@ -39,10 +39,8 @@ module.exports = function(api) {
     plugins: [
       'babel-plugin-macros',
       '@babel/plugin-syntax-dynamic-import',
-      '@babel/plugin-proposal-private-methods',
       isTestEnv && 'babel-plugin-dynamic-import-node',
       '@babel/plugin-transform-destructuring',
-      '@babel/plugin-transform-for-of',
       [
         '@babel/plugin-proposal-class-properties',
         {
