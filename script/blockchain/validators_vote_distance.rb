a3124add9c5477aa57933623b8fa5ffd4da26e34
Maciej Kocyła <maciej.kocyla@polcode.net>
2024-10-16 13:20:03 +0200

    WIP

--- script/blockchain/validators_vote_distance.rb
+++ script/blockchain/validators_vote_distance.rb
@@ -3,10 +3,7 @@ Blockchain::MainnetSlot.where(epoch: 684).group_by(&:leader).map{ |k, v| {k => v
 start_slot = Blockchain::MainnetSlot.where(epoch: 685).first.slot_number
 end_slot = start_slot + 43199
 
-leaders_data = Blockchain::MainnetBlock.joins("JOIN blockchain_mainnet_slots ON blockchain_mainnet_slots.slot_number = blockchain_mainnet_blocks.slot_number")
-    .select("blockchain_mainnet_slots.epoch AS epoch, blockchain_mainnet_slots.leader AS leader, blockchain_mainnet_blocks.slot_number AS slot_number")
-    .where("blockchain_mainnet_slots.epoch = ?", 684)
-    .group_by(&:leader)
+leaders_data = Blockchain::MainnetBlock.joins("JOIN blockchain_mainnet_slots ON blockchain_mainnet_slots.slot_number = blockchain_mainnet_blocks.slot_number").select("blockchain_mainnet_slots.epoch AS epoch, blockchain_mainnet_slots.leader AS leader, blockchain_mainnet_blocks.slot_number AS slot_number").where("blockchain_mainnet_slots.epoch = ?", 684).group_by(&:leader)
 
 leaders_results = leaders_data.map do |leader, blocks|
     previous_slot = nil
