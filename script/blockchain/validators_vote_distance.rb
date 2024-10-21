f114bb65c80039a327e446f5b2760d3eaae790c5
Maciej Kocyła <maciej.kocyla@polcode.net>
2024-10-21 09:02:06 +0200

    prepare csv file

--- script/blockchain/validators_vote_distance.rb
+++ script/blockchain/validators_vote_distance.rb
@@ -1,21 +1,16 @@
-Blockchain::MainnetSlot.where(epoch: 684).group_by(&:leader).map{ |k, v| {k => v.map(&:slot_number)}}
 
-start_slot = Blockchain::MainnetSlot.where(epoch: 685).first.slot_number
-end_slot = start_slot + 43199
-
-leaders_data = Blockchain::MainnetBlock.joins("JOIN blockchain_mainnet_slots ON blockchain_mainnet_slots.slot_number = blockchain_mainnet_blocks.slot_number").select("blockchain_mainnet_slots.epoch AS epoch, blockchain_mainnet_slots.leader AS leader, blockchain_mainnet_blocks.slot_number AS slot_number").where("blockchain_mainnet_slots.epoch = ?", 684).group_by(&:leader)
-
-leaders_results = leaders_data.map do |leader, blocks|
-    previous_slot = nil
-    current_slot = nil
-    highest_diff = 0
-    blocks.each do |block|
-        previous_slot = current_slot
-        current_slot = block.slot_number
-        diff = current_slot - previous_slot if previous_slot
-        highest_diff = diff if diff && diff > highest_diff
+validators_votes = {}
+total_batches = Blockchain::MainnetTransaction.where(epoch: 684).count / 5000
+Blockchain::MainnetTransaction.where(epoch: 684).find_in_batches(batch_size: 5000).with_index do |tx_batch, batch_index|
+    puts "Processing batch #{batch_index + 1} / #{total_batches + 1}"
+    tx_batch.each do |tx|
+        validators_votes[tx.account_key_1] = validators_votes[tx.account_key_1] ? validators_votes[tx.account_key_1] + 1 : 1
     end
-    {leader => highest_diff}
 end
 
-puts leaders_results
+CSV.open("#{Rails.root}/tmp/validator_votes.csv", "w") do |csv|
+    csv << %w[validator_key votes]
+    validators_votes.sort_by { |k, v| v }.reverse.to_h.each do |k, v|
+        csv << [k, v]
+    end
+end
