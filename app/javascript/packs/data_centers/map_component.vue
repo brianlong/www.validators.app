f660b3fdce00887f4e1690204a3919c9b08ff442
Maciej Kocyła <maciej.kocyla@polcode.net>
2024-11-27 12:23:21 +0100

    heatmap WIP

--- app/javascript/packs/data_centers/map_component.vue
+++ app/javascript/packs/data_centers/map_component.vue
@@ -8,7 +8,7 @@
 
   axios.defaults.headers.get["Authorization"] = window.api_authorization;
 
-  (g=>{var h,a,k,p="The Google Maps JavaScript API",c="google",l="importLibrary",q="__ib__",m=document,b=window;b=b[c]||(b[c]={});var d=b.maps||(b.maps={}),r=new Set,e=new URLSearchParams,u=()=>h||(h=new Promise(async(f,n)=>{await (a=m.createElement("script"));e.set("libraries",[...r]+"");for(k in g)e.set(k.replace(/[A-Z]/g,t=>"_"+t[0].toLowerCase()),g[k]);e.set("callback",c+".maps."+q);a.src=`https://maps.${c}apis.com/maps/api/js?`+e;d[q]=f;a.onerror=()=>h=n(Error(p+" could not load."));a.nonce=m.querySelector("script[nonce]")?.nonce||"";m.head.append(a)}));d[l]?console.warn(p+" only loads once. Ignoring:",g):d[l]=(f,...n)=>r.add(f)&&u().then(()=>d[l](f,...n))})({
+  (g=>{var h,a,k,p="The Google Maps JavaScript API",c="google",l="importLibrary",q="__ib__",m=document,b=window;b=b[c]||(b[c]={});var d=b.maps||(b.maps={}),r=new Set,e=new URLSearchParams,u=()=>h||(h=new Promise(async(f,n)=>{await (a=m.createElement("script"));e.set("libraries",[...r]+"visualization");for(k in g)e.set(k.replace(/[A-Z]/g,t=>"_"+t[0].toLowerCase()),g[k]);e.set("callback",c+".maps."+q);a.src=`https://maps.${c}apis.com/maps/api/js?`+e;d[q]=f;a.onerror=()=>h=n(Error(p+" could not load."));a.nonce=m.querySelector("script[nonce]")?.nonce||"";m.head.append(a)}));d[l]?console.warn(p+" only loads once. Ignoring:",g):d[l]=(f,...n)=>r.add(f)&&u().then(()=>d[l](f,...n))})({
     key: "AIzaSyDgZS-lkCg_dKPRU-oRTSM6ZY0mXwiznEk",
     v: "weekly",
   });
@@ -17,6 +17,7 @@
     data() {
       return {
         data_centers: [],
+        heat_points: [],
         map: null
       }
     },
@@ -32,14 +33,13 @@
 
     methods: {
       initMap: async function() {
-        // The location of Uluru
-        const position = { lat: -25.344, lng: 131.031 };
+        const position = { lat: 49.9936, lng: 19.9835 };
 
         const { Map } = await google.maps.importLibrary("maps")
 
         // The map, centered at Uluru
         this.map = new Map(document.getElementById("map"), {
-            zoom: 4,
+            zoom: 6,
             center: position,
             mapId: "DEMO_MAP_ID",
         });
@@ -53,6 +53,7 @@
                  this.data_centers = response.data['data_centers'];
                  this.data_centers.forEach(data_center => {
                     let position = { lat: parseFloat(data_center.location_latitude), lng: parseFloat(data_center.location_longitude) };
+                    this.heat_points.push(new google.maps.LatLng(position['lat'], position['lng']));
                     let marker = new google.maps.Marker({
                         position: position,
                         map: this.map,
@@ -66,6 +67,10 @@
                         infoWindow.open(marker.getMap(), marker);
                     });
                 });
+          const heatmap = new google.maps.visualization.HeatmapLayer({
+            data: this.heat_points,
+            map: this.map,
+          });
           });
         }
     }
