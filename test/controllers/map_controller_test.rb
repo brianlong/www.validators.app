0e9e65922e2407e3c54f6cdecc71628a5dd553e0
Klaudia Biala <klaudia.biala@polcode.net>
2024-12-18 15:16:24 +0100

    Fix test

--- test/controllers/map_controller_test.rb
+++ test/controllers/map_controller_test.rb
@@ -2,7 +2,7 @@ require "test_helper"
 
 class MapControllerTest < ActionDispatch::IntegrationTest
   test "should get index" do
-    get map_index_url
+    get map_url
     assert_response :success
   end
 end
