cc7747d85c0481b9e0d5a7db2640306d96c28f31
Maciej Kocyła <maciej.kocyla@polcode.net>
2025-02-04 11:00:11 +0100

    uncomment migrations

--- db/migrate/20250204084112_create_ping_thing_fee_stats.rb
+++ db/migrate/20250204084112_create_ping_thing_fee_stats.rb
@@ -1,19 +1,19 @@
 class CreatePingThingFeeStats < ActiveRecord::Migration[6.1]
   def change
-    # create_table :ping_thing_fee_stats do |t|
-    #   t.string :network
-    #   t.integer :priority_fee_percentile
-    #   t.float :priority_fee_micro_lamports_average
-    #   t.string :pinger_region
-    #   t.float :average_time
-    #   t.float :median_time
-    #   t.float :p90_time
-    #   t.float :min_time
-    #   t.float :min_slot_latency
-    #   t.float :median_slot_latency
-    #   t.float :p90_slot_latency
+    create_table :ping_thing_fee_stats do |t|
+      t.string :network
+      t.integer :priority_fee_percentile
+      t.float :priority_fee_micro_lamports_average
+      t.string :pinger_region
+      t.float :average_time
+      t.float :median_time
+      t.float :p90_time
+      t.float :min_time
+      t.float :min_slot_latency
+      t.float :median_slot_latency
+      t.float :p90_slot_latency
 
-    #   t.timestamps
-    # end
+      t.timestamps
+    end
   end
 end
