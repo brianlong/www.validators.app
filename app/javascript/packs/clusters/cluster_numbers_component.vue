b2ece3abd017376b7bb096b64c042b3a63db4758
Klaudia Biala <klaudia.biala@polcode.net>
2025-06-02 20:15:37 +0200

    Tweak displaying cluster software versions

--- app/javascript/packs/clusters/cluster_numbers_component.vue
+++ app/javascript/packs/clusters/cluster_numbers_component.vue
@@ -3,11 +3,18 @@
     <div class="card h-100">
       <div class="card-content">
         <h2 class="h5 card-heading-left">Cluster</h2>
-        <div class="text-muted" v-if="!software_version">loading...</div>
+        <div class="text-muted" v-if="!software_versions">loading...</div>
 
-        <div class="mb-2" v-if="software_version">
-          <span class="text-muted me-1">Software Version:</span>
-          <strong class="text-success">{{ software_version }}</strong>
+        <div class="mb-2" v-if="software_versions">
+          <div class="text-muted me-1">Software Versions:</div>
+          <div class="d-inline-block" v-if="software_versions['agave']">
+            <strong class="text-success">{{ software_versions['agave'] }}</strong>&nbsp;
+            <img :src="agave_icon" class="img-xxs" title="Agave" alt="A" style="margin-top: -3px;">
+          </div>
+          <div class="d-inline-block" v-if="software_versions['firedancer']">
+            <strong class="ms-3 text-success">{{ software_versions['firedancer'] }}</strong>
+            <img :src="firedancer_icon" class="img-xs" title="Firedancer" alt="F" style="margin-top: -3px; margin-left: 2px">
+          </div>
         </div>
         <div v-if="validators_count">
           <span class="text-muted me-1">Validators:</span>
@@ -24,6 +31,8 @@
 
 <script>
   import { mapGetters } from 'vuex'
+  import agaveIcon from 'agave.svg'
+  import firedancerIcon from 'firedancer.svg'
 
   export default {
     data() {
@@ -31,7 +40,9 @@
         connection: null,
         validators_count: null,
         nodes_count: null,
-        software_version: null
+        software_versions: null,
+        agave_icon: agaveIcon,
+        firedancer_icon: firedancerIcon
       }
     },
 
@@ -53,7 +64,7 @@
         received(data) {
           this.validators_count = data.cluster_stats[this.network].validator_count
           this.nodes_count = data.cluster_stats[this.network].nodes_count
-          this.software_version = data.cluster_stats[this.network].software_version
+          this.software_versions = data.cluster_stats[this.network].software_versions
         },
         disconnected() {},
       },
