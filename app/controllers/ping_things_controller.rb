5f263803286587fdb89bfa792edcd1c1eb446268
Klaudia Biala <klaudia.biala@polcode.net>
2025-06-04 17:57:23 +0200

    Whitelist network in routes and js

--- app/controllers/ping_things_controller.rb
+++ app/controllers/ping_things_controller.rb
@@ -1,6 +1,9 @@
 # frozen_string_literal: true
 
 class PingThingsController < ApplicationController
+  skip_before_action :set_network
+  before_action :set_network_for_ping_thing
+
   def index
     @ping_things = PingThing.where(network: params[:network])
                             .includes(:user)
@@ -8,5 +11,11 @@ class PingThingsController < ApplicationController
                             .first(240)
     @ping_things_count = @ping_things.length
     @ping_things_array_for_chart = @ping_things.pluck(:response_time).reverse
+  end
+
+  def set_network_for_ping_thing
+    return if NETWORKS_FOR_PING_THING.include?(params[:network])
+
+    params[:network] = "mainnet"
   end
 end
