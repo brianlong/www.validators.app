86115e9b97f982218b62c9c0e5cb7185fbc2e265
Klaudia Biala <klaudia.biala@polcode.net>
2025-06-25 13:17:43 +0200

    Update deploy script

--- config/deploy.rb
+++ config/deploy.rb
@@ -4,40 +4,27 @@
 lock '~> 3.16.0'
 
 set :application, 'validators.app'
+
 set :repo_url, 'git@github.com:brianlong/www.validators.app.git'
+
 set :user, 'deploy'
 
 # Default branch is :master
 ask :branch, `git rev-parse --abbrev-ref HEAD`.chomp
 
 # Default deploy_to directory is /var/www/my_app_name
-# set :deploy_to, "/var/www/my_app_name"
 set :deploy_to, "/home/deploy/#{fetch(:application)}"
 
 set :migration_role, :db
+
 set :templated_config_files, []
-
-# Default value for :format is :airbrussh.
-# set :format, :airbrussh
-
-# You can configure the Airbrussh format using :format_options.
-# These are the defaults.
-# set :format_options, command_output: true, log_file: "log/capistrano.log", color: :auto, truncate: :auto
-
-# Default value for :pty is false
-# set :pty, true
-set :pty, false
 
 set :sitemap_roles, :web
 
 # Default value for :linked_files is []
-append :linked_files, 'config/database.yml'
-
-append :linked_files, 'config/appsignal.yml'
-append :linked_files, 'config/sidekiq.yml'
-append :linked_files, 'config/sidekiq_blockchain.yml'
-append :linked_files, 'config/cluster.yml'
-append :linked_files, '.env'
+set :linked_files, fetch(:linked_files, []).push(
+  'config/database.yml', 'config/appsignal.yml', 'config/sidekiq.yml', 'config/sidekiq_blockchain.yml', 'config/cluster.yml', '.env'
+)
 
 # Default value for linked_dirs is []
 append :linked_dirs, 'log', 'tmp/pids', 'tmp/cache', 'tmp/sockets', 'public/system'
@@ -49,17 +36,8 @@ set :default_env, {
 }
 
 set :passenger_environment_variables, { path: '/usr/sbin/passenger-status:$PATH' }
-# set :passenger_restart_with_touch, true
+
 set :passenger_roles, :web
-
-# Default value for default_env is {}
-# set :default_env, { path: "/opt/ruby/bin:$PATH" }
-
-# Default value for local_user is ENV['USER']
-# set :local_user, -> { `git config user.name`.chomp }
-
-# Default value for keep_releases is 5
-# set :keep_releases, 5
 
 # Uncomment the following to require manually verifying the host key before first deploy.
 # set :ssh_options, verify_host_key: :secure
