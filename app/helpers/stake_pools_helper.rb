4ab88a78f0c37519f92cf6e6fa375ab16bf49afe
maciejkocylapc <80820353+maciejkocylapc@users.noreply.github.com>
2025-07-01 16:46:17 +0200

    update/add new stake pools (#1098)
    * update/add new stake pools
    
    * Update stake pool logos
    
    * handle deleted stake pools
    
    * fix validators table view
    
    ---------
    
    Co-authored-by: Klaudia Biala <klaudia.biala@polcode.net>

--- app/helpers/stake_pools_helper.rb
+++ app/helpers/stake_pools_helper.rb
@@ -3,13 +3,16 @@
 module StakePoolsHelper
   def stake_pool_logos(stake_pools)
     stake_pools.map do |stake_pool|
-      {image: MAINNET_STAKE_POOLS[stake_pool.downcase.to_sym][:small_logo], name: stake_pool}
-    end
+      MAINNET_STAKE_POOLS[stake_pool.downcase.to_sym] ? {
+        image: MAINNET_STAKE_POOLS[stake_pool.downcase.to_sym][:small_logo],
+        name: stake_pool
+      } : nil
+    end.compact
   end
 
   def shuffle_logos
     MAINNET_STAKE_POOLS.map do |pool_key, pool_data|
-      [pool_data[:url], pool_data[:large_logo]]
-    end.shuffle
+      pool_data[:deleted] ? nil : [pool_data[:url], pool_data[:large_logo]]
+    end.compact.shuffle
   end
 end
