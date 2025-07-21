be2f2892046c2d9efa99dd8f28a1b77eb5a3bb45
Maciej Kocyła <maciej.kocyla@polcode.net>
2025-07-21 08:29:15 +0200

    verify_security_report_script

---
+++ script/verify_security_report.rb
@@ -0,0 +1,25 @@
+# frozen_string_literal: true
+
+require File.expand_path('../config/environment', __dir__)
+require 'net/http'
+require 'uri'
+require 'logger'
+
+logger = Logger.new('log/verify_security_report.log')
+
+Validator.where.not(security_report_url: [nil, '']).find_each do |validator|
+  url = validator.security_report_url
+  begin
+    uri = URI.parse(url)
+    response = Net::HTTP.get_response(uri)
+    puts response.code.to_i
+    if response.code.to_i == 404
+      logger.info("REMOVED: #{validator.account} | #{url}")
+      validator.update(security_report_url: nil)
+    else
+      logger.info("OK for #{validator.account} (#{url})")
+    end
+  rescue => e
+    logger.error("Error for #{validator.account} (#{url}): #{e.class} #{e.message}")
+  end
+end
