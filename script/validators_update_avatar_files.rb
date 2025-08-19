1dede5cd63cfe528b376f2b3637d813eac08e05c
maciejkocylapc <80820353+maciejkocylapc@users.noreply.github.com>
2025-08-19 14:28:38 +0200

    sc-166727-investigate-large-number-of-errors-related-to-avatars (#1112)
    * add rescue for web errors
    
    * rescue other occurrences
    
    * Update rubocop rules
    
    ---------
    
    Co-authored-by: Klaudia Biala <klaudia.biala@polcode.net>

--- script/validators_update_avatar_files.rb
+++ script/validators_update_avatar_files.rb
@@ -7,6 +7,8 @@ NETWORKS.each do |network|
   Validator.where(network: network, is_active: true).where.not(avatar_url: nil).each do |validator|
     begin
       UpdateImageFileService.new(validator, :avatar).call
+    rescue OpenURI::HTTPError, OpenSSL::SSL::SSLError, Net::OpenTimeout, SocketError => e
+      next
     rescue => e
       Appsignal.send_error(e)
       next
