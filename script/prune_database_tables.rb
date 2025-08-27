b71dc666cd45b509252d8fb41bb8c34baebe127f
Maciej Kocyła <maciej.kocyla@polcode.net>
2025-08-27 09:36:10 +0200

    delete audited by batch + change schedule

--- script/prune_database_tables.rb
+++ script/prune_database_tables.rb
@@ -5,9 +5,6 @@ require File.expand_path('../config/environment', __dir__)
 
 sixty_days_ago = (Date.today - 60.days).to_s(:db)
 
-verbose = true
-puts sixty_days_ago if verbose
-
 %w[
   batches
   epoch_histories
@@ -22,38 +19,35 @@ puts sixty_days_ago if verbose
   explorer_stake_account_history_stats
 ].each do |table|
   sql = "DELETE FROM #{table} WHERE created_at < '#{sixty_days_ago}';"
-  puts sql if verbose
   ActiveRecord::Base.connection.execute(sql)
 end
 
 # Remove old validators that are not active after 60 days
-puts "Removing old inactive validators" if verbose
-ValidatorScoreV1.where("active_stake = 0 and created_at < '#{sixty_days_ago}'")
-                .each do |score|
-                  puts "#{score.validator.account} (#{score.network})" \
-                    if verbose
-                  if score.validator && score.validator.vote_account_histories.count > 0
-                    puts "  Skipping due to non-empty vote_account_histories"
-                    next
+NETWORKS.each do |network|
+  ValidatorScoreV1.where("active_stake = 0 and created_at < '#{sixty_days_ago}' and network = '#{network}'")
+                  .each do |score|
+                    if score.validator && score.validator.vote_account_histories.count > 0
+                      puts "  Skipping due to non-empty vote_account_histories"
+                      next
+                    end
+                    # score.validator.vote_account_histories.destroy_all
+                    # score.validator.vote_accounts
+                    score.validator&.destroy
                   end
-                  # score.validator.vote_account_histories.destroy_all
-                  # score.validator.vote_accounts
-                  score.validator&.destroy
-                end
 
-puts "Removing old inactive validators with NULL stake" if verbose
-ValidatorScoreV1.where("active_stake IS NULL and created_at < '#{sixty_days_ago}'")
-                .each do |score|
-                  if score.validator && score.validator.vote_account_histories.count > 0
-                    puts "  Skipping due to non-empty vote_account_histories"
-                    next
+  ValidatorScoreV1.where("active_stake IS NULL and created_at < '#{sixty_days_ago}' and network = '#{network}'")
+                  .each do |score|
+                    if score.validator && score.validator.vote_account_histories.count > 0
+                      puts "  Skipping due to non-empty vote_account_histories"
+                      next
+                    end
+                    # score.validator.vote_account_histories.destroy_all
+                    # score.validator.vote_accounts
+                    score.validator&.destroy
                   end
-                  # score.validator.vote_account_histories.destroy_all
-                  # score.validator.vote_accounts
-                  puts "destroying #{score.validator.account} (#{score.network})" \
-                    if verbose && score.validator
-                  score.validator&.destroy
-                end
-puts "Removing old inactive Audited records" if verbose
+end
+
 # remove old audit records (from explorer stake accounts)
-Audited::Audit.where("created_at < ?", sixty_days_ago).destroy_all
+Audited::Audit.where("created_at < ?", sixty_days_ago).in_batches(of: 1000) do |batch|
+  batch.delete_all
+end
