9234ccfbd6bc9770fee44f4e8f56d1ba8d6677cd
Klaudia Biala <klaudia.biala@polcode.net>
2025-09-10 02:05:00 +0200

    Match validators by identity too in addition to ip address

--- script/update_doublezero_validators.rb
+++ script/update_doublezero_validators.rb
@@ -10,13 +10,15 @@ file_path = if Rails.env.test?
             end
 
 doublezero_ips = []
+doublezero_identities = []
 doublezero_validators_ids = []
 
 ip_regexp = /(?:[0-9]{1,3}\.){3}[0-9]{1,3}/
+identity_regexp = /SolanaValidator: \(.*\)/
 
 logger.info("Starting script...")
 
-unless Rails.env.test?
+unless Rails.env.test? || Rails.env.development?
   # Remove old file if exists and create a new one
   File.delete(file_path) if File.exist?(file_path)
   fork { exec("doublezero user list > #{file_path}") }
@@ -34,13 +36,16 @@ begin
   file.each do |line|
     ip_address = line.scan(ip_regexp)&.first
     doublezero_ips << ip_address if ip_address
+
+    identity = line.scan(identity_regexp)&.first&.gsub('SolanaValidator: (', '')&.gsub(')', '')
+    doublezero_identities << identity if identity
   end
   file.close
 
-  logger.info("Found #{doublezero_ips.uniq.size} IPs")
-  logger.info("Looking for validators")
+  logger.info("Found #{doublezero_ips.uniq.size} IPs and #{doublezero_identities.uniq.size} identities in doublezero file")
+  logger.info("Looking for validators in database")
 
-  doublezero_ips.uniq.each do |ip_address|
+  doublezero_ips.each do |ip_address|
     ValidatorIp.where(is_active: true, address: ip_address).find_each do |validator_ip|
       next unless validator_ip.validator_id.present?
 
@@ -48,10 +53,18 @@ begin
     end
   end
 
+  doublezero_identities.each do |identity|
+    validator = Validator.find_by(account: identity)
+    doublezero_validators_ids << validator.id if validator
+  end
+
+  doublezero_validators_ids.uniq!
+
   Validator.where.not(id: doublezero_validators_ids).update_all(is_dz: false)
   Validator.where(id: doublezero_validators_ids).update_all(is_dz: true)
 
-  logger.info("DoubleZero validators (#{doublezero_validators_ids.size}) updated successfully: #{doublezero_validators_ids}")
+  logger.info("#{doublezero_validators_ids.size} validators updated successfully: #{doublezero_validators_ids}")
+  logger.info('End of script.')
 rescue StandardError => e
   logger.error("Failed to update DoubleZero validators: #{e}")
   Appsignal.send_error(e)
--- test/scripts/update_doublezero_validators_test.rb
+++ test/scripts/update_doublezero_validators_test.rb
@@ -5,8 +5,11 @@ require "test_helper"
 class UpdateDoublezeroValidatorsTest < ActiveSupport::TestCase
   setup do
     @network = "mainnet"
+
     @validator = create(:validator, network: @network)
-    create(:validator_ip, validator: @validator, is_active: true, address: '185.167.205.3')
+    create(:validator_ip, validator: @validator, is_active: true, address: '3.65.206.72')
+
+    @validator1 = create(:validator, network: @network, account: 'NLMSHTjmSiRxGJPs3uaqtsFBC2dTGYwK41U18Nmw5kH')
 
     @validator2 = create(:validator, network: @network)
     create(:validator_ip, validator: @validator, is_active: true, address: '111.111.111.111')
@@ -20,6 +23,7 @@ class UpdateDoublezeroValidatorsTest < ActiveSupport::TestCase
       load(Rails.root.join("script", "update_doublezero_validators.rb"))
 
       assert @validator.reload.is_dz
+      assert @validator1.reload.is_dz
       refute @validator2.reload.is_dz
       refute @validator3.reload.is_dz
     end
