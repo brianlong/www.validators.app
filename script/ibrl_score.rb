9aa4860225c16fa06fa0991bfddeaed1b61793f4
Maciej Kocyła <maciej.kocyla@polcode.net>
2026-01-23 10:35:09 +0100

    rescue 0 ibrl scores + add retry times

--- script/ibrl_score.rb
+++ script/ibrl_score.rb
@@ -4,20 +4,25 @@ require File.expand_path('../config/environment', __dir__)
 require 'uri'
 require 'net/http'
 
+RETRY_MAX = 5
+SLEEP_TIME = 10
+
+retry_count = 0
 network = ARGV[0] || 'mainnet'
 
-uri = URI("https://explorer.bam.dev/api/v1/ibrl_validators")
-response = Net::HTTP.get_response(uri)
+begin
+  uri = URI("https://explorer.bam.dev/api/v1/ibrl_validators")
+  response = Net::HTTP.get_response(uri)
 
-if response.is_a?(Net::HTTPSuccess)
+  raise "Failed to fetch data from API. Status: #{response.code}" unless response.is_a?(Net::HTTPSuccess)
+
   json_data = JSON.parse(response.body)
   
-  if json_data['data'].nil?
-    puts "No data returned from API"
-    exit
-  end
+  exit if json_data['data'].nil?
 
-  identities = json_data['data'].map { |d| d['identity'] }
+  identities = json_data['data'].reject { |d| d['ibrl_score'] == 0 }.map { |d| d['identity'] }
+
+  raise "No validators with non-zero ibrl_score found" if identities.empty?
 
   validators = Validator.where(account: identities, network: network)
                         .includes(:validator_score_v1)
@@ -37,7 +42,10 @@ if response.is_a?(Net::HTTPSuccess)
       validator.validator_score_v1.update_column(:ibrl_score, ibrl_score)
     end
   end
-
-else
-  puts "Failed to fetch data from API. Status: #{response.code}"
+rescue => e
+  retry_count += 1
+  if retry_count < RETRY_MAX
+    sleep(SLEEP_TIME)
+    retry
+  end
 end
