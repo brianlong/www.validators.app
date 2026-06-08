dfc622d090b7967713c6c1b6c740a66af2867bf7
Maciej Kocyła <maciej.kocyla@polcode.net>
2026-06-08 08:24:05 +0200

    skipped_vote_percent v2.5

--- app/models/vote_account_history.rb
+++ app/models/vote_account_history.rb
@@ -54,7 +54,7 @@ class VoteAccountHistory < ApplicationRecord
       if network == "pythnet"
         max_credits = slot_index_current
       elsif network == "alpenglow-community"
-        max_credits = slot_index_current.to_f * 256 + (slot_index_current - 1) * 256
+        max_credits = slot_index_current.to_f * 1024 + (slot_index_current - 1) * 1024
         return max_credits > 0 ? (max_credits - credits_current.to_i) / max_credits : nil
       else
         max_credits = slot_index_current * 8 + (slot_index_current - 1) * 8
